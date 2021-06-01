c233c7cbb8fb55797580f28f33b104e38e28e356
fgosuen <fgosuendacosta@gmail.com>
2021-06-01 15:25:53 -0300

    Correção Requisito 8

--- script.js
+++ script.js
@@ -2,36 +2,41 @@
 const elementButtonCriarTarefa = document.getElementById('criar-tarefa');
 const elementOl = document.getElementById('lista-tarefas');
 let elementLi = document.getElementsByClassName('tarefa');
+const elementTarefa = document.getElementById('texto-tarefa');
 
+const buttonApagaTudo = document.querySelectorAll('selected')
 
 // Eventos
-elementButtonCriarTarefa.addEventListener('click', adicionaLista)
+elementButtonCriarTarefa.addEventListener('click', criaTarefa)
 
 //Funções
-function adicionaLista() {
-  const elementTarefa = document.getElementById('texto-tarefa');
-  let tarefa = elementTarefa.value;
-  const elementLiAdd = document.createElement('li');
-  elementLiAdd.className = 'tarefa'
-  elementLiAdd.innerHTML = tarefa;
-  //* elementLiAdd.addEventListener('click', function () {*/
-  /*  elementLiAdd.style.backgroundColor = 'rgb(128,128,128)'}) */
-  elementLiAdd.addEventListener('click', alteraBackground);
-  elementOl.appendChild(elementLiAdd);
-  elementTarefa.value = '';
+function criaTarefa() {
+  const item = document.createElement('li');
+  item.className = 'tarefa'
+  item.innerHTML = elementTarefa.value;
+  elementOl.appendChild(item);
+  elementTarefa.value = null;
+
+  item.addEventListener('dblclick', function () {
+    completaTarefa(item); /* completedItem */
+  });
+  
+  item.addEventListener('click', function () {
+    selecionaTarefa(item); /* selectedItem */
+  });
 }
 
-function alteraBackground() {
-  const elementLiTarefas = document.getElementsByClassName('tarefa');
-  for (let index = 0; index < elementLiTarefas.length; index += 1) {
-    elementLiTarefas[index].addEventListener('click', function () {
-      elementLiTarefas[index].className = 'tarefa selected';
-    })
-    const elementLiTarefasSelected = document.getElementsByClassName('tarefa selected');
-    for (let index2 = 0; index2 < elementLiTarefasSelected.length; index2 += 1) {
-      if (elementLiTarefas[index].className !== elementLiTarefasSelected[index2].className) {
-        elementLiTarefasSelected[index2].className = 'tarefa';
-      }
-    }
+function selecionaTarefa(item) {
+  if (document.getElementsByClassName('selected')[0] != null) {
+    document.getElementsByClassName('selected')[0].classList.remove('selected');
+  }
+  item.classList.add('selected');
+}
+
+function completaTarefa(item){
+  if (item.classList.contains('completed')) {
+    item.classList.remove('completed');
+  } else {
+    item.classList.add('completed');
   }
 }
