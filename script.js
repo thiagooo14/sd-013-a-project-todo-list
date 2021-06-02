09b04b97067249a2d10574a00f29197ee929330b
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-06-02 16:36:45 -0300

    Resolvi os 6 desafios falta usar lint

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 var main = document.querySelector('main');
 
 inputCreator();
-listCreator ();
+listOlCreator ();
 
-console.log(main);
 function inputCreator(){
+  // Cria o botão input Text abaixo
   var inputText = document.createElement("INPUT");
   inputText.setAttribute("type", "text");
   inputText.setAttribute("id", "texto-tarefa");
   main.appendChild(inputText);
+  //// Cria o botão Adicionar embaixo
+  var inputSub = document.createElement("button");
+  inputSub.setAttribute("id", "criar-tarefa");
+  inputSub.innerText = "ADICIONAR";
+  textFunc(inputSub);
 }
 
-function listCreator() {
-  var list = document.createElement("ol");
-  list.setAttribute("id", 'lista-tarefas');
-  main.appendChild(list);
+// Cria a OL onde vai receber a lista das tarefas
+function listOlCreator() {
+  var listOl = document.createElement("ol");
+  listOl.setAttribute("id", 'lista-tarefas');
+  main.appendChild(listOl);
 }
+
+function textFunc(inputSub){
+  main.appendChild(inputSub);
+
+  inputSub.addEventListener('click', () => {
+  const li = document.createElement('li');
+  const criarTarefa = document.getElementById('lista-tarefas');
+  const texto = document.createTextNode(document.getElementById('texto-tarefa').value)
+  li.appendChild(texto);
+
+  criarTarefa.appendChild(li);
+
+  const limpador = document.getElementById('texto-tarefa').value = '';
+   } );
+}
+
+
 
 
 
