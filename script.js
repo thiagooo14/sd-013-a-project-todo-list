622ccc91352a296574dd5e31804580a2d48500b7
Lucassauro <lucassauro.lt@gmail.com>
2021-06-02 18:46:20 -0300

    Corrige requisito 5. Adiciona requisitos 6 e 7. Requisitos 5, 6 e 7 completos.

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-document.getElementById("criar-tarefa").onclick = function cria() {
-  console.log("click");
-  let text = document.getElementById('text-tarefa').value;
-  let lista = document.getElementById('lista-tarefas');
-  let li = document.createElement("li");
-  let input = document.createTextNode(text);
-
-  li.appendChild(input)
-  lista.appendChild(li)
-
-  document.getElementById('text-tarefa').value = null;
-
-}
-
 window.onload = function () {
-  document.getElementById('text-tarefa').focus();
-  document.addEventListener("keydown", function (cria) {
-    if (e.code === "Enter") {
-      cria();
-    }
-  })
+  document.getElementById('texto-tarefa').focus();
+
+  document.getElementById("criar-tarefa").onclick = function cria() {
+    console.log("click");
+    let text = document.getElementById('texto-tarefa').value;
+    let lista = document.getElementById('lista-tarefas');
+    let li = document.createElement("li");
+    let input = document.createTextNode(text);
+    li.appendChild(input)
+    lista.appendChild(li)
+    document.getElementById('texto-tarefa').value = null;
+  
+  }
 }
