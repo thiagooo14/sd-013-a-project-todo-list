1ba937080f1c2c6b78f4d39248b91c3f7d72ef66
VictorDizne <victoralbertocostadiniz@gmail.com>
2021-06-02 15:48:01 -0300

    Completa o requisito 7 e 8 do projeto.

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+function selectItem() {
+  const itensLista = document.getElementsByTagName('li');
+  for (let i = 0; i < itensLista.length; i += 1) {
+    itensLista[i].addEventListener('click', () => {
+      const currentlySelected = document.getElementById('selected');
+      if (currentlySelected !== null) {
+        currentlySelected.removeAttribute('id');
+        itensLista[i].id = 'selected';
+      } else {
+        itensLista[i].id = 'selected';
+      }
+    });
+  }
+}
 function createListItem() {
-  let button = document.querySelector('#criar-tarefa');
-  button.addEventListener('click', function() {
-    let input = document.querySelector('#texto-tarefa').value;
-    let lista = document.querySelector('#lista-tarefas');
-    let item = document.createElement('li');
-    item.innerText = input
+  const button = document.querySelector('#criar-tarefa');
+  button.addEventListener('click', () => {
+    const input = document.querySelector('#texto-tarefa').value;
+    const lista = document.querySelector('#lista-tarefas');
+    const item = document.createElement('li');
+    item.innerText = input;
     lista.appendChild(item);
     document.querySelector('#texto-tarefa').value = '';
+    selectItem();
   });
 }
 createListItem();
