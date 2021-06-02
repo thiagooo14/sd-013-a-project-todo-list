b8c9f4048becfd714974beb6e5f433903e5739c0
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-06-02 17:13:08 -0300

    adiciona funcao de selecionar tarefas

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function () {
   addingTask();
 }
 
+// Cria função para criação de tarefas.
 function addingTask() {
   let taskText = document.getElementById('texto-tarefa');
   let btnCreate = document.getElementById('criar-tarefa');
@@ -9,15 +10,42 @@ function addingTask() {
 
   btnCreate.addEventListener('click', function () {
     let task = document.createElement('li');
+    task.classList.add('task');
     task.innerText = taskText.value
     list.appendChild(task);
     taskText.value = null;
   })
 }
 
-function () {
-
-  document.addEventListener('click' function () {
-    if
-})
+// Cria função de remover classes.
+function removeClass(element, eClass) {
+  element = document.querySelectorAll(element);
+  for (let iterator of element) {
+    if (iterator.classList.contains(eClass)) {
+      iterator.classList.remove(eClass);
+    }
+  }
 }
+
+// Cria função de selecionar tarefas.
+function taskSelected() {
+
+  document.addEventListener('click', function (event) {
+    let task = event.target;
+    if (task.classList.contains('task')) {
+
+
+
+      removeClass('li', 'selected');
+
+
+      if (task.classList.contains('selected')) {
+        task.classList.remove('selected');
+      } else {
+        task.classList.add('selected');
+      }
+    }
+  })
+}
+
+taskSelected();
