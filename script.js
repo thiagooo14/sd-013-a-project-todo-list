ee8c0ae2c326079250fa830a604ccf60a7c0d2f7
Erick Santos <erickosantos.dev@gmail.com>
2021-06-02 13:02:50 -0300

    9 - Clicar duas vezes em um item, faz com que ele seja riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 const olList = document.getElementById('lista-tarefas');
 const createItemBtn = document.getElementById('criar-tarefa');
 const inputTask = document.getElementById('texto-tarefa');
-const items = document.getElementsByTagName('li');
+const items = document.getElementsByClassName('task');
 
 // Função para deixar de ser selecionada
 function notSelectItems() {
   for (let i = 0; i < items.length; i += 1) {
-    items[i].classList = '';
+    items[i].style.backgroundColor = '';
   }
 }
 // Função para selecionar a tarefa
 function selectItem() {
   for (let i = 0; i < items.length; i += 1) {
-    items[i].addEventListener('click', (event) => {
+    items[i].addEventListener('click', (e) => {
       notSelectItems();
-      const ev = event;
-      ev.target.classList = 'selected';
+      e.target.style.backgroundColor = 'rgb(128, 128, 128)';
     });
   }
 }
+
+// Função para completar tarefa
+olList.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+});
 
 // Função para adicionar tarefa na lista
 function createItem() {
@@ -26,6 +34,7 @@ function createItem() {
     const item = document.createElement('li');
     olList.appendChild(item);
     item.innerText = inputTask.value;
+    item.classList = "task"
     inputTask.value = '';
     selectItem();
   });
