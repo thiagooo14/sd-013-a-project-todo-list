94babb4fdb371aefb28c060f07899041a7e57e37
Wesley de Melo Pedroso <wesleyxd@hotmail.com>
2021-06-02 16:33:43 -0300

    7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const orderList = document.querySelector('#lista-tarefas');
 
+// Muda cor de fundo do item da lista selecionado
+function changeBgItemList() {
+  const lengthTaskList = orderList.children;
+  for (let index = 0; index < lengthTaskList.length; index += 1) {
+    lengthTaskList[index].addEventListener('click', () => {
+      lengthTaskList[index].style.backgroundColor = 'rgb(128,128,128)';
+    });
+  }
+}
+
 // Cria uma nova tarefa
 function newTask() {
   const btnAdd = document.querySelector('#criar-tarefa');
@@ -9,6 +19,7 @@ function newTask() {
     createTask.innerText = inputValue.value;
     orderList.appendChild(createTask);
     inputValue.value = '';
+    changeBgItemList();
   });
 }
 newTask();
