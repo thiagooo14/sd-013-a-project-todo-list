d55996bd6a13644bde7ab561e50ab998e1fb48d9
Felipe Dias <felipeferreiradias1@gmail.com>
2021-06-02 19:20:58 -0300

    adiciona requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 window.onload = function () {
 
-let addTaskButton = document.getElementById('criar-tarefa')
-
-
-addTaskButton.addEventListener('click', addTask);
-
-function addTask(evento) {
-  let textValue = document.getElementById('texto-tarefa').value;
-	if (textValue != "") {
-		let line = document.createElement('li')
-		document.getElementById('lista-tarefas').appendChild(line);
-		line.innerText = textValue;
-		document.getElementById('texto-tarefa').value = "";
-	} else {
-		alert ('Insira uma tarefa!');
+	let addTaskButton = document.getElementById('criar-tarefa')
+	let listaOrd = document.getElementById('lista-tarefas'); 
+		
+	addTaskButton.addEventListener('click', addTask);
+	
+	function addTask() {
+	  let textValue = document.getElementById('texto-tarefa').value;
+		if (textValue != "") {
+			let line = document.createElement('li')
+			document.getElementById('lista-tarefas').appendChild(line);
+			line.innerText = textValue;
+			line.className = 'line';
+			document.getElementById('texto-tarefa').value = "";
+		} else {
+			alert ('Insira uma tarefa!');
+		};
 	};
-};
 
+	listaOrd.addEventListener('click', gray);
 
-
-
-
-
-
-
-
-
-
+	function gray (evento) {
+		let colorChoose = document.querySelector('.selected');
+      if (colorChoose == null) {
+        evento.target.classList.add('selected');
+      } else {
+        colorChoose.classList.remove('selected');
+        evento.target.classList.add('selected');
+      }
+  }
+	
+  
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
