126a6c6ed523e6ed188c1f5fe1295e66fac18380
LuizFJP <luizfjportela@gmail.com>
2021-06-02 16:14:05 -0300

    requirement 05 and 06

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 window.onload = () =>{
-  
+  let buttonAddTask = document.getElementById("criar-tarefa");
+  let inputText = document.getElementById('texto-tarefa');
+  let list = document.getElementById('lista-tarefas');
+
+  buttonAddTask.addEventListener('click', addTask);
+
+  function addTask(text){
+    text = inputText;
+    let li = document.createElement('li');
+    li.innerText = inputText.value;
+    list.appendChild(li);
+    inputText.value = '';
+}
+
 }
