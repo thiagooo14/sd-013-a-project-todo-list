3416d7b017277690fb5e19aa9c85f0fcf83ba2d6
Jéssica Queiroz <gnr0034@gmail.com>
2021-06-02 18:05:50 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 let buttomNewTask = document.getElementById('criar-tarefa');
-console.log(buttomNewTask);
+
+buttomNewTask.addEventListener('click', () => {
+  let newTask = document.getElementById('texto-tarefa');
+  let texto = document.getElementById('texto-tarefa').value;
+  let li = document.createElement('li');
+  let ol = document.querySelector('ol');
+  li.innerHTML = texto;
+  ol.appendChild(li);
+  newTask.value = '';  
+})
+
+
+
