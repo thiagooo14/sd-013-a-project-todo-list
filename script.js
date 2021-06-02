3c475ab806c892606e094d595a02d34adcc059d1
Thiago <thiagoc.carboneri@gmail.com>
2021-06-02 16:49:08 -0300

    resolução até item, item 5 incompleto

---
+++ script.js
@@ -0,0 +1,31 @@
+
+let getButton = document.getElementById('criar-tarefa');
+let input = document.getElementById('texto-tarefa');
+let listaOrdenada = document.getElementById('lista-tarefas');
+function addItemList() {
+    getButton.addEventListener('click', function () {
+        tarefa = document.createElement('li');
+        listaOrdenada.appendChild(tarefa);
+        let texto = document.getElementById('texto-tarefa').value;
+        tarefa.innerText = texto;
+    })
+}
+addItemList();
+
+function eraseText() {
+    getButton.addEventListener('click', function () {
+        let texto = document.getElementById('texto-tarefa').value;
+        if (texto.value !== "") {
+            texto.value = '';
+        }
+    })
+}
+eraseText();
+
+function changeColorList() {
+    let listas = document.querySelector('ol');
+    listas.addEventListener('click', function (event) {
+            event.target.style.backgroundColor = 'rgb(128,128,128)';
+    })
+}
+changeColorList();
