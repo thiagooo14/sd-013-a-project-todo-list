00f6d49ee76624ce46a50df619f1240a0df2abad
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-06-02 18:46:25 -0300

    resolve 10

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let list = document.getElementById('lista-tarefas');
-let buttao = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
+const buttao = document.getElementById('criar-tarefa');
 
 buttao.addEventListener('click', function addTask() {
-  let newTask = document.getElementById('texto-tarefa').value;
-  let newLi = document.createElement('li');
+  const newTask = document.getElementById('texto-tarefa').value;
+  const newLi = document.createElement('li');
   newLi.innerHTML = newTask;
   list.appendChild(newLi);
   newLi.classList.add('item');
-  document.getElementById('texto-tarefa').value = "";
+  document.getElementById('texto-tarefa').value = '';
 });
 
 buttao.addEventListener('click', function addTask() {
   let newTask = document.getElementById('texto-tarefa').value;
   newTask = '';
-})
+});
 
-let itemObj = document.getElementsByClassName('item');
+const itemObj = document.getElementsByClassName('item');
 
 list.addEventListener('click', function changeColor(clickEvent) {
-  let listObj = clickEvent.target;
-  let varia = document.getElementsByClassName('grayTask')[0]
+  const listObj = clickEvent.target;
+  const varia = document.getElementsByClassName('grayTask')[0];
 
     itemObj[0].classList.remove('grayTask');
 
@@ -45,9 +45,5 @@ list.addEventListener('dblclick', function taskCompleted(dblclickEvent) {
 let bClear = document.getElementById("apaga-tudo");
 
 bClear.addEventListener('click', function clear() {
-  for(i = 0; i < document.getElementsByClassName('item').length; i += 1){
-    let removeItem =  document.getElementsByClassName('item')[i]
-    let lista = document.getElementById('lista-tarefas')
-    lista.removeChild(removeItem);
-  }
-})
+  list.innerHTML = ''
+});
