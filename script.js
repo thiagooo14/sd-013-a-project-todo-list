33d1b04e103c118eec9626fe30f10620e701dc88
Gustavo-Jezini <Gustavojezini@gmail.com>
2021-06-02 16:39:22 -0300

    resolve 9 (?)

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-let listO = document.getElementById('lista-tarefas');
+let list = document.getElementById('lista-tarefas');
 let buttao = document.getElementById('criar-tarefa');
 
 buttao.addEventListener('click', function addTask() {
   let newTask = document.getElementById('texto-tarefa').value;
   let newLi = document.createElement('li');
   newLi.innerHTML = newTask;
-  listO.appendChild(newLi);
+  list.appendChild(newLi);
+  newLi.classList.add('item');
+  list.clear();
+});
+let itemObj = document.getElementsByClassName('item');
+list.addEventListener('click', function changeColor(clickEvent) {
+  let listObj = clickEvent.target;
+  if(document.getElementsByClassName('grayTask').length !== 0)
+  for(i = 0; i <= itemObj.length; i += 1){
+    itemObj[i].classList.remove('grayTask');listObj.classList.add('grayTask');
+  }
+
+  listObj.classList.add('grayTask');
+  // let grayItem = document.getElementsByClassName('grayTask')[0];
+
+
+  // grayItem.style.backgroundColor = 'rgb(128,128,128)'
 });
 
-listO.addEventListener('click', function changeColor(clickEvent) {
-  clickEvent.target.style.backgroundColor = 'rgb(128,128,128)'
-})
+list.addEventListener('dblclick', function taskCompleted(dblclickEvent) {
+  if(dblclickEvent.target.classList == 'item grayTask'){
+  dblclickEvent.target.classList.add('completed');
+  } else {
+    dblclickEvent.target.classList.remove('completed')
+  }
+});
