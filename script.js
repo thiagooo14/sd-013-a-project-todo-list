bf81e28f4031d42121f8f47014fb7d5bec7d5d33
turtore <brunosap@gmail.com>
2021-06-02 17:21:04 -0300

    adicionado até o 6

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+let botaoApaga = document.querySelector("#apaga-tudo");
 
 let botaoCriar = document.querySelector("#criar-tarefa");
 
+let listaArray = document.querySelectorAll('li');
+for (let i = 0; i < listaArray.length ; i += 1 ) {
+    listaArray[i].addEventListener("dblclick", riscaElemento)
+    listaArray[i].addEventListener("click", mudaCorFundo)
+}
+
+function mudaCorFundo() {
+    window.alert('mudacor');
+}
+function riscaElemento() {
+    window.alert('riscaelemento');
+}
 
 function addTarefa(){
     let campoTexto = document.querySelector("#texto-tarefa").value;
-    window.alert(campoTexto);
+    let lugarNaLista = document.querySelector("#lista-tarefas");
+    let li = document.createElement('li');
+    li.innerHTML = campoTexto;
+    lugarNaLista.appendChild(li)
+    document.querySelector("#texto-tarefa").value = null
 }
 
+function apagaTudo() {
+    window.location.reload();
+}
+
+botaoApaga.addEventListener("click",apagaTudo);
 botaoCriar.addEventListener("click", addTarefa);
