45e4f79f90a18934c3e9fcd9f0be6892006a6be1
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-06-02 17:04:56 -0300

    Correcao lint

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const olList = document.getElementById('lista-tarefas');
 const createItemBtn = document.getElementById('criar-tarefa');
 const inputTask = document.getElementById('texto-tarefa');
 
-
 function createItemEvent() {
   createItemBtn.addEventListener('click', () => {
     const item = document.createElement('li');
@@ -13,9 +12,3 @@ function createItemEvent() {
 }
 
 createItemEvent();
-
-
-// for (let index = 0; index < 1; index += 1);
-//   const criarTarefa = document.querySelector('#criar-tarefa');
-//   const newtask = document.createElement('li');
-//   criarTarefa.appendChild(newtask);
