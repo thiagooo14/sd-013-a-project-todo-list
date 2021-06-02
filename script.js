1b57541d53ea839ef24db2893166f514d414dbd5
Jean V  Pacheco <jeanvitorp@gmail.com>
2021-06-02 17:21:26 -0300

    Adiciona ate o requisito 6

--- script.js
+++ script.js
@@ -10,7 +10,24 @@ document.body.appendChild(inputField);
 
 
 // Cria o botão
-var addTask = document.createElement('button');
-addTask.id = 'criar-tarefa';
-addTask.innerText = 'Adicionar';
-document.body.appendChild(addTask);
+var addButton = document.createElement('button');
+addButton.id = 'criar-tarefa';
+addButton.innerText = 'Adicionar';
+document.body.appendChild(addButton);
+
+// Adiciona as tarefas à lista
+function addTask (){
+  addButton.addEventListener('click', function (){
+    //pega o valor do campo input
+    let newTask = inputField.value;
+    let newElement = document.createElement('li');
+    //coloca o valor do campo input numa nova li e adiciona à lista
+    newElement.innerText = newTask;
+    let wholeList = document.querySelector('#lista-tarefas');
+    wholeList.appendChild(newElement);
+    //zera o campo
+    inputField.value = '';
+  })
+};
+
+addTask();
