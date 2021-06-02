c35c95fe929db0431521786e0e56fe58cd33b3cb
Jean V  Pacheco <jeanvitorp@gmail.com>
2021-06-02 16:43:59 -0300

    Adiciona até o requisito 4 e mexe no CSS

---
+++ script.js
@@ -0,0 +1,7 @@
+//Chama o conteúdo
+var content = document.querySelector('#content');
+console.log(content);
+// Cria o input
+var inputField = document.createElement('input');
+inputField.setAttribute('type', 'text');
+document.body.appendChild(inputField);
