e9ae7c3e46701389ca5e2fdabfbf007ad73e063b
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-06-02 19:14:32 -0300

    Adiciona requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const olList = document.getElementById('lista-tarefas');
-const createItemBtn = document.getElementById('criar-tarefa');
-const inputTask = document.getElementById('texto-tarefa');
+let createTaskButton = document.querySelector('#criar-tarefa');
+let taskList = document.querySelector('#lista-tarefas');
 
-function createItemEvent() {
-  createItemBtn.addEventListener('click', () => {
-    const item = document.createElement('li');
-    olList.appendChild(item);
-    item.innerText = inputTask.value;
-    inputTask.value = '';
-  });
+function createTask() {
+  let taskTextInput = document.querySelector('#texto-tarefa').value;
+  let newTask = document.createElement('li');
+  newTask.className = 'item-list';
+  newTask.innerHTML = taskTextInput;
+  taskList.appendChild(newTask);
+  document.querySelector('#texto-tarefa').value = '';
+  focusColorList();
 }
+createTaskButton.addEventListener('click', createTask);
 
-createItemEvent();
+function focusColorList() {
+  let taskListItems = document.querySelectorAll('.item-list');
+  for (let i = 0; i < taskListItems.length; i += 1) {
+    taskListItems[i].addEventListener('click', changeColorFocus);
+  }
+}
+focusColorList();
+
+function changeColorFocus(event) {
+  let actualFocus = document.querySelector('.selected');
+  if (actualFocus) {
+    actualFocus.className = 'item-list';
+  }
+  event.target.className = 'item-list selected';
+}
