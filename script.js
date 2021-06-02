ac7bc81445645da44681af1efe932cb9a7fd9160
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-06-02 16:05:21 -0300

    pause

---
+++ script.js
@@ -0,0 +1,16 @@
+let buttonCreateTask = document.querySelector('#criar-tarefa');
+
+function addTask (event) {
+  let taskText = document.querySelector('#texto-tarefa');
+  let ol = document.querySelector('#lista-tarefas');
+  let taskItem = document.createElement('li');
+  ol.appendChild(taskItem);
+  taskI
+
+
+  // let taskText = document.getElementById('texto-tarefa').innerHTML = txt;
+
+  event.target. = cssStyle;
+}
+
+buttonCreateTask.addEventListener('click', addTask);
