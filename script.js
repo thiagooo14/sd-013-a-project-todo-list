efb74bbd3a1a0628838a4e94c0603b11fda64743
Rafael Janovicci <rjanovicci@hotmail.com>
2021-06-02 18:18:50 -0300

    requisito 5

---
+++ script.js
@@ -0,0 +1,12 @@
+const lista = document.querySelector('#lista-tarefas');
+const botaoCriarTarefa = document.querySelector('#criar-tarefa');
+const inputTarefa = document.querySelector('#texto-tarefa');
+
+function adicionaTarefa() {
+  botaoCriarTarefa.addEventListener('click', function(){
+    const item = document.createElement('li');
+    item.className = 'item';
+    lista.appendChild(item).innerText = inputTarefa.value;
+  });
+}
+adicionaTarefa();
