3d3baa927ca11204ec5e90e237d02b7ac9314eed
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-06-02 16:38:22 -0300

    Adiciona requisitos 8 e 9

--- script.js
+++ script.js
@@ -2,20 +2,29 @@ const listaTarefas = document.querySelector('#lista-tarefas');
 const input = document.querySelector('#texto-tarefa');
 const botaoAdicionar = document.querySelector('#criar-tarefa');
 
+function completarTarefa(event) {
+  if (event.target.className === 'completed') {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
+
+function trocaCorTarefa(event) {
+  const unicacor = document.querySelectorAll('li');
+  for (let i = 0; i < unicacor.length; i += 1) {
+    unicacor[i].style.backgroundColor = '';
+  }
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
+}
 
 function criarTarefa() {
-
   const li = document.createElement('li');
   li.innerText = input.value;
-  li.className = 'tarefa'
   input.value = null;
-  li.addEventListener('click',trocaCorTarefa);
+  li.addEventListener('click', trocaCorTarefa);
+  li.addEventListener('dblclick', completarTarefa);
   listaTarefas.appendChild(li);
-
-}
-function trocaCorTarefa(event) {
-  event.target.style.backgroundColor = 'rgb(128,128,128)';
-  console.log(listaTarefas);
 }
 
 botaoAdicionar.addEventListener('click', criarTarefa);
