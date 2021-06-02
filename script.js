818084d47f9439c3161033a741ea21f87853dbf7
Erick Santos <erickosantos.dev@gmail.com>
2021-06-01 23:48:13 -0300

    8 - Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const createItemBtn = document.getElementById('criar-tarefa');
 const inputTask = document.getElementById('texto-tarefa');
 const items = document.getElementsByTagName('li');
 
+// Função para deixar de ser selecionada
+function notSelectItems() {
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].classList = '';
+  }
+}
+// Função para selecionar a tarefa
+function selectItem() {
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].addEventListener('click', (event) => {
+      notSelectItems();
+      const ev = event;
+      ev.target.classList = 'selected';
+    });
+  }
+}
+
 // Função para adicionar tarefa na lista
 function createItem() {
   createItemBtn.addEventListener('click', () => {
@@ -14,12 +31,3 @@ function createItem() {
   });
 }
 createItem();
-
-// Função para selecionar a tarefa
-function selectItem() {
-  for (let i = 0; i < items.length; i++) {
-    items[i].addEventListener('click', (event) => {
-      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-    });
-  }
-}
