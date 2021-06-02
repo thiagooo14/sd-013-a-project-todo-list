95fd44162bbcdb6fa1a159240c5142c8a71292b1
V. Dionysio <vini.dionysio@gmail.com>
2021-06-02 15:38:26 -0300

    Solving challenge 8

--- script.js
+++ script.js
@@ -11,9 +11,20 @@ window.onload = function() {
 
   document.getElementById('criar-tarefa').addEventListener('click', addTarefa);
 
-  document.addEventListener('click', function (event) {
+  function selectItem(event) {
     if (event.target.classList.contains('tarefa')) {
-      event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+      if (event.target.style.backgroundColor !== 'rgb(128, 128, 128)') {
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+      }
+      const taskArray = document.getElementsByClassName('tarefa');
+      for (let i = 0; i < taskArray.length; i += 1) {
+        if (taskArray[i] !== event.target){
+          taskArray[i].style.backgroundColor = ''
+       }
+      }
     }
-  }, false);
+  }
+
+  document.addEventListener('click', selectItem , false);
+
 };
