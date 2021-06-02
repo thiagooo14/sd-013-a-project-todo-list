5f9ef8c75edf12b1d7466846efadf88863822e12
Rafael Victor <rafavictorgs@gmail.com>
2021-06-02 16:51:18 -0300

    Resolve o requisito 3 do Todo List Project

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ function createHeaderChild() {
   const headContainer = document.querySelector('#head-container');
   const newTitle = document.createElement('h1');
   newTitle.innerText = 'Minha Lista de Tarefas';
-  newTitle.setAttribute('id','title-h1');
+  newTitle.setAttribute('id', 'title-h1');
   headContainer.appendChild(newTitle);
 
   const newParagraph = document.createElement('p');
   newParagraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
-  newParagraph.setAttribute('id','funcionamento');
+  newParagraph.setAttribute('id', 'funcionamento');
   headContainer.appendChild(newParagraph);
 }
 
 createHeaderChild();
 
-// function createParagraph() {
-//   const headContainer = document.querySelector('#head-container');
+function createInputBox() {
+  const inputBoxContainer = document.querySelector('#input-box');
+  const newInputBox = document.createElement('input');
+  newInputBox.setAttribute('id', 'texto-tarefa');
+  inputBoxContainer.appendChild(newInputBox);
+}
 
-
-// createParagraph();
+createInputBox();
