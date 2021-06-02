b8bbdc8ff6fc7bea57460b4b6e72debd88e6500e
Leme <andresleme@gmail.com>
2021-06-02 17:49:20 -0300

    Requirements 5 & 6

---
+++ script.js
@@ -0,0 +1,15 @@
+function getTextoTarefa() {
+  return document.getElementById('texto-tarefa').value;
+};
+
+function clearTextoTarefa() {
+  return document.getElementById('texto-tarefa').value = '';
+};
+
+function adicionarTarefa() {
+  const lista = document.getElementById('lista-tarefas');
+  const filhoLista = document.createElement('li');
+  filhoLista.innerText = getTextoTarefa();
+  lista.appendChild(filhoLista);
+  clearTextoTarefa();
+};
