4e929dffbb9f07d3ad854f153db5549f34f3b365
Cássio Pereira <cassiorodp@gmail.com>
2021-06-02 15:47:27 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const buttonTask = document.querySelector('#criar-tarefa');
 const inputTask = document.querySelector('#texto-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
-buttonTask.addEventListener('click', function () {
-  let liTask = document.createElement('li');
+
+// Adiciona botão que cria tarefas
+buttonTask.addEventListener('click', () => {
+  const liTask = document.createElement('li');
   liTask.innerHTML = inputTask.value;
   inputTask.value = '';
   taskList.appendChild(liTask);
+});
+
+taskList.addEventListener('click', (event) => {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 })
