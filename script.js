a2e2a6b7520e5b87bc90caadbfbd6a8d5061471a
Jean V  Pacheco <jeanvitorp@gmail.com>
2021-06-02 16:54:53 -0300

    Adiciona requisito 5

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-//Chama o conteúdo
-var content = document.querySelector('#content');
-console.log(content);
+//Chama o conteúdo (adicionar o input dentro do content)
+// var content = document.querySelector('#content');
+// console.log(content);
+
 // Cria o input
 var inputField = document.createElement('input');
+inputField.id = 'texto-tarefa';
 inputField.setAttribute('type', 'text');
 document.body.appendChild(inputField);
+
+
+// Cria o botão
+var addTask = document.createElement('button');
+addTask.id = 'criar-tarefa';
+addTask.innerText = 'Adicionar';
+document.body.appendChild(addTask);
