f4751b19bbfc94fdcfbb9adc120c6fc47bb4c63a
V. Dionysio <vini.dionysio@gmail.com>
2021-06-02 15:18:38 -0300

    Solving challenge 7

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function() {
-  function addTarefa (event) {
+  function addTarefa () {
     const listItem = document.createElement('li');
+    listItem.classList.add('tarefa')
     const taskList = document.getElementById('lista-tarefas');
     let inputValue = document.getElementById('texto-tarefa').value;
     listItem.innerHTML = inputValue;
@@ -9,4 +10,10 @@ window.onload = function() {
   };
 
   document.getElementById('criar-tarefa').addEventListener('click', addTarefa);
+
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('tarefa')) {
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+    }
+  }, false);
 };
