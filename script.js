6d3f98a97cba7bbf54dafc0d6799ad15f3fa32f5
Julia Baptista <baptista.julia@yahoo.com.br>
2021-06-02 16:44:46 -0300

    9. Clicar duas vezes em um item, faz com que ele seja riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item

--- script.js
+++ script.js
@@ -8,15 +8,52 @@ window.onload = function() {
     btnCriarTarefa.addEventListener('click', ()=> {
       let tarefa = inputTarefa.value;
       let itemLista = document.createElement('li');
+      itemLista.classList.add('item-da-lista');
       itemLista.innerHTML = tarefa;
       listaDeTarefas.appendChild(itemLista);
       inputTarefa.value = "";
-    });
-    
+    }); 
   }
   criarTarefa();
 
+  function selecionarTarefa() {
+    let listaDeTarefas = document.querySelector('#lista-tarefas');
 
+    listaDeTarefas.addEventListener('click', (event)=> {
+
+      if(event.target.classList.contains('item-da-lista')) {
+        let itensDaLista = document.querySelectorAll('#lista-tarefas .item-da-lista');
+        for (let i = 0; i < itensDaLista.length; i += 1) {
+          itensDaLista[i].classList.remove('selected');
+        }
+        event.target.classList.add('selected');
+      }
+    });
+  }
+  selecionarTarefa();
+
+  function riscarTarefa() {
+    let listaDeTarefas = document.querySelector('#lista-tarefas');
+
+    listaDeTarefas.addEventListener('dblclick', (event)=> {
+
+      if(event.target.classList.contains('item-da-lista')) {
+        console.log(event.target);
+        event.target.classList.toggle('completed');
+        
+        // event.target.classList.toggle('completed');
+
+        // if(event.target.classList.contains('riscado')) {
+        //   event.target.classList.remove('riscado');
+        // } else if () {
+        //   event.target.classList.add('riscado');
+        // }
+          
+        // }
+      }
+    });
+  }
+  riscarTarefa();
 
 
 
