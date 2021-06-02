19af1be7e0c6192338e9eaeeff389970cabd0f7a
turtore <brunosap@gmail.com>
2021-06-02 19:47:12 -0300

    feito até o 7

--- script.js
+++ script.js
@@ -2,31 +2,48 @@ let botaoApaga = document.querySelector("#apaga-tudo");
 
 let botaoCriar = document.querySelector("#criar-tarefa");
 
-let listaArray = document.querySelectorAll('li');
-for (let i = 0; i < listaArray.length ; i += 1 ) {
-    listaArray[i].addEventListener("dblclick", riscaElemento)
-    listaArray[i].addEventListener("click", mudaCorFundo)
+let listaOrdenada = document.querySelector("#lista-tarefas");
+
+
+//bloco do requerimento 7
+//bloco do requerimento 7
+// function carregaArrayLista() {
+//     let listaArray = document.querySelectorAll('li');
+//     for (let i = 0; i > listaArray.length ; i += 1 ) {
+//         listaArray[i].addEventListener("dblclick", riscaElemento);
+//         listaArray[i].addEventListener("click", mudaCorFundo);
+//     }
+//     return listaArray;
+// }
+
+function mudaCorFundo(event) {
+    //let temporario = event.target.className
+    //window.alert(temporario);
+    event.target.className = "selected";
 }
 
-function mudaCorFundo() {
-    window.alert('mudacor');
-}
 function riscaElemento() {
     window.alert('riscaelemento');
 }
 
-function addTarefa(){
-    let campoTexto = document.querySelector("#texto-tarefa").value;
-    let lugarNaLista = document.querySelector("#lista-tarefas");
-    let li = document.createElement('li');
-    li.innerHTML = campoTexto;
-    lugarNaLista.appendChild(li)
-    document.querySelector("#texto-tarefa").value = null
-}
-
-function apagaTudo() {
-    window.location.reload();
-}
-
-botaoApaga.addEventListener("click",apagaTudo);
-botaoCriar.addEventListener("click", addTarefa);
+    //fim bloco requerimento 7
+    //fim bloco requerimento 7
+    
+    function addTarefa(){
+        let campoTexto = document.querySelector("#texto-tarefa").value;
+        let lugarNaLista = document.querySelector("#lista-tarefas");
+        let li = document.createElement('li');
+        li.innerHTML = campoTexto;
+        li.className = 'comum';
+        lugarNaLista.appendChild(li);
+        document.querySelector("#texto-tarefa").value = null;
+    }
+    
+    function apagaTudo() {
+        window.location.reload();
+    }
+    
+    botaoApaga.addEventListener("click",apagaTudo);
+    botaoCriar.addEventListener("click", addTarefa);
+    // botaoCriar.addEventListener("click", carregaArrayLista)
+    listaOrdenada.addEventListener("click", mudaCorFundo);
