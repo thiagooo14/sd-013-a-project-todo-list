7c25c11d01973a54a6110a0b3e0dbe85574e8b65
ALINE <hoshinoaline@yahoo.com.br>
2021-06-02 17:59:52 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-const ol= document.createElement('ol');
-ol.id= "lista-tarefas"
+const ol = document.createElement('ol');
+ol.id = 'lista-tarefas';
 document.body.appendChild(ol);
 
-function CreateTask(){
-inputtext= document.querySelector('#texto-tarefa')//criei variavel paa o input 
-buttonAddTask= document.querySelector("#criar-tarefa");//variavel para o botao
-buttonAddTask.addEventListener("click", function(){   //botao ao clicar
-  if(inputtext.value !==""){   //se estiver diferente de vazio , ou seja você escreveu
-    const list= document.createElement('li');//cria uma lista
-    list.classList="lista"//lista ganha classe
-    list.innerHTML= inputtext.value;//inseri o valor
-    ol.appendChild(list);//filho da ol
-  }
-  inputtext.value=""; //zera
-})
+function CreateTask() {
+  inputtext = document.querySelector('#texto-tarefa'); //criei variavel para o input
+  buttonAddTask = document.querySelector('#criar-tarefa'); //variavel para o botao
+  buttonAddTask.addEventListener('click', function () {
+    //botao ao clicar
+    if (inputtext.value !== '') {
+      //se estiver diferente de vazio , ou seja você escreveu
+      const list = document.createElement('li'); //cria uma lista
+      list.classList = 'lista'; //lista ganha classe
+      list.addEventListener('click', ChangeBackground);
+      list.innerHTML = inputtext.value; //insere o valor
+      ol.appendChild(list); //filho da ol
+    }
+    inputtext.value = ''; //zera
+  });
 }
 
 CreateTask();
+
+function ChangeBackground(event) {
+  const arrayList = document.querySelectorAll('.lista');
+  for (let index = 0; index < arrayList.length; index += 1) {
+    arrayList[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
