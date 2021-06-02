f5f959e6a02505028bd6fd810afd8cd26393789e
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-06-02 17:55:16 -0300

    Requisito 09 - Ok

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const itemTasks = document.getElementsByClassName('task-list');
 // Função que add a tafera a lista
 function addTasks() {
   const taskList = document.createElement('li');
-  taskList.className = "task-list"
+  taskList.className = 'task-list';
 
   // Pega o calor do input e coloca na li.
   taskList.innerText = document.getElementById('texto-tarefa').value;
@@ -13,25 +13,30 @@ function addTasks() {
   listaTasks.appendChild(taskList);
 }
 
-
 function addColorItem(event) {
   // Um for mais simples para remove os style do item da lista.
-  for (let item of itemTasks) {
-
-    // So remove o style 
-    if(event.target.className === 'task-list'){
-      item.removeAttribute('style')
+  for (const item of itemTasks) {
+    // So remove o style
+    if (event.target.className.startsWith('task-list')) {
+      item.removeAttribute('style');
     }
 
-    if(event.target.className === 'task-list'){
-      event.target.style.backgroundColor = 'rgb(128, 128, 128)'
-    }else {
+    if (event.target.className.startsWith('task-list')) {
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
     }
-    console.log(event.target);
   }
 }
 
-
-
+// Função que coloca sublinhado no item.
+function underlinedItem(event) {
+  if(event.target.className === 'task-list'){
+    event.target.className += ' completed'
+  }else if(event.target.className === 'task-list completed'){
+    event.target.className = 'task-list';
+    console.log(event.target)
+  }
+}
+''.startsWith
 btnAddTask.addEventListener('click', addTasks);
-listaTasks.addEventListener('click', addColorItem)
+listaTasks.addEventListener('click', addColorItem);
+listaTasks.addEventListener('dblclick', underlinedItem);
