533fe6a131a8c345941afb3c7469a0db7b060d85
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-06-02 15:53:50 -0300

    requisito 7

--- script.js
+++ script.js
@@ -7,21 +7,45 @@ function clearInputText() {
   document.querySelector('#texto-tarefa').value = '';
 }
 
-function getListItems() {
+function getList() {
   const list = document.querySelector('#lista-tarefas');
   return list;
+}
+
+function getListItems() {
+  const listItems = document.querySelectorAll('.list-item');
+  return listItems;
 }
 
 function addTask() {
   const button = document.querySelector('#criar-tarefa');
 
   button.addEventListener('click', () => {
-    const list = getListItems();
+    const list = getList();
     const li = document.createElement('li');
+
     li.innerHTML = getInputText();
-    list.appendChild(li)
+    li.classList.add('list-item')
+    list.appendChild(li);
     clearInputText();
-  })
+    selectListItem();
+  });
 }
+
+function selectListItem() {
+  const list = getListItems();
+
+  for (let listIndex = 0; listIndex < list.length; listIndex++) {
+    list[listIndex].addEventListener('click', (event) => {
+      const item = event.target;
+
+      item.classList.add('selected');
+      // item.style.backgroundColor = 'rgb(128,128,128)';
+    });
+  }
+
+}
+
+selectListItem();
 
 addTask();
