259f0ff91fd05489a788f1a094608293e797678c
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-06-02 16:57:16 -0300

    Melhorei um pouco com o lint

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
-var main = document.querySelector('main');
+const main = document.querySelector('main');
 
 inputCreator();
-listOlCreator ();
+listOlCreator();
 
-function inputCreator(){
+function inputCreator() {
   // Cria o botão input Text abaixo
-  var inputText = document.createElement("INPUT");
-  inputText.setAttribute("type", "text");
-  inputText.setAttribute("id", "texto-tarefa");
+  const inputText = document.createElement('input');
+  inputText.setAttribute('type', 'text');
+  inputText.setAttribute('id', 'texto-tarefa');
   main.appendChild(inputText);
-  //// Cria o botão Adicionar embaixo
-  var inputSub = document.createElement("button");
-  inputSub.setAttribute("id", "criar-tarefa");
-  inputSub.innerText = "ADICIONAR";
+  // Cria o botão Adicionar embaixo
+  const inputSub = document.createElement('button');
+  inputSub.setAttribute('id', 'criar-tarefa');
+  inputSub.innerText = 'ADICIONAR';
   textFunc(inputSub);
 }
 
 // Cria a OL onde vai receber a lista das tarefas
 function listOlCreator() {
-  var listOl = document.createElement("ol");
-  listOl.setAttribute("id", 'lista-tarefas');
+  const listOl = document.createElement('ol');
+  listOl.setAttribute('id', 'lista-tarefas');
   main.appendChild(listOl);
 }
 
-function textFunc(inputSub){
+function textFunc(inputSub) {
   main.appendChild(inputSub);
 
   inputSub.addEventListener('click', () => {
-  const li = document.createElement('li');
-  const criarTarefa = document.getElementById('lista-tarefas');
-  const texto = document.createTextNode(document.getElementById('texto-tarefa').value)
-  li.appendChild(texto);
+    const li = document.createElement('li');
+    const criarTarefa = document.getElementById('lista-tarefas');
+    const texto = document.createTextNode(document.getElementById('texto-tarefa').value);
+    li.appendChild(texto);
 
-  criarTarefa.appendChild(li);
+    criarTarefa.appendChild(li);
 
-  const limpador = document.getElementById('texto-tarefa').value = '';
-   } );
+    document.getElementById('texto-tarefa').value = '';
+  });
 }
-
-
-
-
-
-
