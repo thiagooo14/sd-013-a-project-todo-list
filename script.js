6ca799c0bf3dfd5a977c4e1bf48a7577d0691c81
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-06-02 16:07:40 -0300

    Correção requisito 4

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 let textoDigitado = document.getElementById('texto-tarefa');
     let botao = document.getElementById('criar-tarefa');
-    let lista = document.getElementById('lista-tarefa');
-    let item = document.querySelectorAll('li');
+    let lista = document.getElementById('lista-tarefas');
+    let item = document.getElementsByClassName('liList');
 
     function adicionarTarefa() {
       let li = document.createElement('li');
       li.innerText = textoDigitado.value;
       lista.appendChild(li);
+      li.classList = 'liList';
       textoDigitado.value = ' ';
     }
-    // item[0].addEventListener('click', pintaBack);
 
-    // function pintaBack(event) {
-    //   item.classList.remove('itemLista');
-    //   event.target.classList.add('itemLista');
-    // }
+    function paintBack() {
+      for (let i in item) {
+        item[i].addEventListener('click', (el) => {
+          el.target.style.backgroundColor = 'red';
+        });
+      }
+    }
+  botao.addEventListener('click', adicionarTarefa);
 
-    botao.addEventListener('click', adicionarTarefa);
