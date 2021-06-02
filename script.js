9ebdc201ed98b348f6fbcf3c5a0be53a5e440afa
Marcelo Cunha <marcelo.cunha1@outlook.com>
2021-06-02 17:23:50 -0300

    resolved issue five end six

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 window.onload = function(){
 
+// declarando variaveis..
+  var textInput = document.getElementById("texto-tarefa");
+  var buttonInputTesk = document.getElementById("criar-tarefa");
+  var itensOl = document.getElementById("lista-tarefas");
+
+// função para adicionar tesks
+  function addTask() {
+    if(textInput.value.length !== 0) { // verifica se o texto é diferente de 0 / vazio
+      let newLi = document.createElement("li"); // cria uma li 
+      itensOl.appendChild(newLi); // coloca li como filho da Ol
+      newLi.innerText = textInput.value; // transferi o valor do input em um novo item da lista
+      textInput.value = ""; // limpa input depois da adição
+    }
+  }
+buttonInputTesk.addEventListener("click", addTask);
 
 
 
@@ -8,5 +23,5 @@ window.onload = function(){
 
 
 
-  
+
 }
