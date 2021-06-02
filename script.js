8dee22c9016a485b6349c26b204a5a38846f7fc3
ArthurMGoulart <budiarthur@gmail.com>
2021-06-02 18:33:29 -0300

    Realizando requisitos do 1 ao 5, VQV

---
+++ script.js
@@ -0,0 +1,21 @@
+function createTask(text){
+  let task = document.createElement("li");
+  task.className = "task";
+  task.innerText = text;
+  return task;
+}
+function assignTaskToList(task){
+  let list = document.querySelector("#lista-tarefas");
+  list.appendChild(task);
+}
+function setButtonToAddTask(){
+  let button = document.querySelector("#criar-tarefa");
+  let input = document.querySelector("#texto-tarefa");
+  button.addEventListener("click", function (event) {
+    let text = input.value;
+    let task = createTask(text);
+    assignTaskToList(task);
+    input.innerHTML = '';
+  })
+}
+setButtonToAddTask();
