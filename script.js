7c05fdd0f757aaf7962b236791087b6338ee7556
Icaro Ferreira <icaro.iky@gmail.com>
2021-06-02 18:31:53 -0300

    Req 7

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 let createTaskButton = document.querySelector('#criar-tarefa');
+let taskList =document.querySelector('#lista-tarefas');
 
 function createTask(){
   let taskTextInput = document.querySelector('#texto-tarefa').value;
   let newTask = document.createElement('li');
-  let taskList = document.querySelector('#lista-tarefas');
+  newTask.className = 'item-list'
   newTask.innerHTML = taskTextInput;
   taskList.appendChild(newTask);
   document.querySelector('#texto-tarefa').value = '';
+  focusColorList();
 }
 createTaskButton.addEventListener('click', createTask);
 
+function focusColorList(){
+  let taskListItems = document.querySelectorAll('.item-list');
+  for (let i = 0; i < taskListItems.length; i += 1){
+    taskListItems[i].addEventListener('click', changeColorFocus);
+  }
+}
+focusColorList();
+
+function changeColorFocus(event){
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
+}
