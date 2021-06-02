7cdb617b6794fa56dec73c613d33bee9f8860534
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-06-02 18:53:03 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-let textoDigitado = document.getElementById('texto-tarefa');
+    let textoDigitado = document.getElementById('texto-tarefa');
     let botao = document.getElementById('criar-tarefa');
     let lista = document.getElementById('lista-tarefas');
-    let item = document.getElementsByClassName('liList');
+    let item = document.getElementsByClassName('lista');
 
     function adicionarTarefa() {
       let li = document.createElement('li');
+      li.classList = 'lista';
       li.innerText = textoDigitado.value;
+      li.addEventListener('click', paintBack);
       lista.appendChild(li);
-      li.classList = 'liList';
       textoDigitado.value = ' ';
+      
     }
-
-    function paintBack() {
-      for (let i in item) {
-        item[i].addEventListener('click', (el) => {
-          el.target.style.backgroundColor = 'red';
-        });
+    function paintBack(event) {
+      
+      for(let i =0; i < item.length; i += 1){
+      item[i].classList.remove('selected');
       }
-    }
-  botao.addEventListener('click', adicionarTarefa);
-
+      event.target.classList.add('selected'); 
+    }  
+    
+    botao.addEventListener('click', adicionarTarefa);
