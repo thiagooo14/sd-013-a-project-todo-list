252fb7f151e898a4d6feb3ad389c6dd6f276b138
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2021-06-02 19:26:50 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+    /*Requisito 5*/
+    
     let button = document.getElementById("criar-tarefa");
 
     button.addEventListener("click", adicionaTarefa);
@@ -11,6 +13,21 @@
         tarefa.value ="";
         tarefa.focus();
     }
+
+    /*Requisito 6*/
+    
+    let listaOrdenada = document.querySelector("#lista-tarefas");
+      function pintaLista(event) {
+         let liClicado = event.target;
+         liClicado.classList.add("color")
+      }
+        listaOrdenada.addEventListener("click", pintaLista);
+    
+  
         
 
+
+/*Referências
+
+Requisito 5 - https://www.youtube.com/watch?v=ucfNgEl_Vcw*/
 
