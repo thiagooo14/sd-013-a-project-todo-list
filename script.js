aefae9569d4ca38a1c75ee5a714102b2f3498f86
Reinaldo Paixao <reipaixao.b@gmail.com>
2021-06-02 19:14:24 -0300

    Correções do 4 ao 7

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-    let textoDigitado = document.getElementById('texto-tarefa');
-    let botao = document.getElementById('criar-tarefa');
-    let lista = document.getElementById('lista-tarefas');
-    let item = document.getElementsByClassName('lista');
+let textoDigitado = document.getElementById('texto-tarefa');
+let botao = document.getElementById('criar-tarefa');
+let lista = document.getElementById('lista-tarefas');
+let item = document.getElementsByClassName('lista');
 
-    function adicionarTarefa() {
-      let li = document.createElement('li');
-      li.classList = 'lista';
-      li.innerText = textoDigitado.value;
-      li.addEventListener('click', paintBack);
-      lista.appendChild(li);
-      textoDigitado.value = ' ';
-      
-    }
-    function paintBack(event) {
-      
-      for(let i =0; i < item.length; i += 1){
-      item[i].classList.remove('selected');
-      }
-      event.target.classList.add('selected'); 
-    }  
-    
-    botao.addEventListener('click', adicionarTarefa);
+function adicionarTarefa() {
+  let li = document.createElement('li');
+  li.classList = 'lista';
+  li.innerText = textoDigitado.value;
+  li.addEventListener('click', paintBack);
+  lista.appendChild(li);
+  textoDigitado.value = ' ';
+}
+function paintBack(event) {
+  for(let i =0; i < item.length; i += 1){
+  item[i].classList.remove('selected');
+  }
+  event.target.classList.add('selected'); 
+}  
+
+botao.addEventListener('click', adicionarTarefa);
