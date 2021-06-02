d3a2b6567cd4b3888752e89258913132a411f635
Jean V  Pacheco <jeanvitorp@gmail.com>
2021-06-02 20:53:44 -0300

    Adiciona o requisito 9

--- script.js
+++ script.js
@@ -23,11 +23,38 @@ function addTask (){
     let newElement = document.createElement('li');
     //coloca o valor do campo input numa nova li e adiciona à lista
     newElement.innerText = newTask;
-    let wholeList = document.querySelector('#lista-tarefas');
-    wholeList.appendChild(newElement);
+    let fullList = document.querySelector('#lista-tarefas');
+    fullList.appendChild(newElement);
     //zera o campo
     inputField.value = '';
   })
 };
-
 addTask();
+
+// Adiciona uma classe na tarefa clicada
+
+function selectedTask (){
+  let fullList = document.getElementById('lista-tarefas');
+  fullList.addEventListener ('click', function(event) {
+    event.target.className = 'selected';
+  })
+}
+selectedTask();
+
+// Coloca o fundo cinza na tarefa clicada
+
+// Risca o item ao ser clicado duas vezes
+// Arrumar para poder concatenar classes
+
+function completedTask (){
+  let fullList = document.getElementById('lista-tarefas');
+  fullList.addEventListener('dblclick', function (event) {
+    if (event.target.className !== 'completed') {
+      event.target.className = 'completed'
+    }
+    else {
+      event.target.classLista.remove('completed');
+    }
+  })
+}
+completedTask ();
