6013c451d4674e9cb32b0576f97f5a5e19310d28
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-06-03 17:25:48 -0300

    add 7th requirement to project TL

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const buttonCreateTask = document.querySelector('#criar-tarefa');
 
-function addTask () {
+buttonCreateTask.addEventListener('click', function createTask() {
   const task = document.querySelector('#texto-tarefa');
   const taskText = document.querySelector('#texto-tarefa').value;
-  let ol = document.querySelector('#lista-tarefas');
-  let li = document.createElement('li');
+  const ol = document.querySelector('#lista-tarefas');
+  const li = document.createElement('li');
+  li.className = 'liTask'
   li.innerHTML = taskText;
   ol.appendChild(li);
   task.value = '';
-}
-
-buttonCreateTask.addEventListener('click', addTask);
+  li.addEventListener('click', function printNewBG() {
+    li.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
+});
