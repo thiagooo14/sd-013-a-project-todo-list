cb127efdb190890ec7188ecd60f4c9704e9e6600
Yuri Garrido <y.garrido.yuri@gmail.com>
2021-06-03 20:47:51 -0300

    envia o 7

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ botaoCria.addEventListener('click', input);
 
 let lista = document.querySelector('#lista-tarefas');
 
+const itemLista =  document.querySelectorAll('.pinta');
+
+function select (event){
+  for (let index = 0; index < itemLista.length; index += 1 ) {
+    itemLista[index].classList.add(' for');
+  }
+  event.target.className = 'cinza';
+}
 function input (){
 
 let input = document.querySelector('#texto-tarefa');
@@ -21,7 +29,10 @@ let input = document.querySelector('#texto-tarefa');
 let valor = input.value;
 let item = document.createElement('li');
 document.querySelector('#lista-tarefas').appendChild(item);
-
+item.className='pinta';
 item.innerHTML = valor;
+item.addEventListener('click', select);
 input.value = '';
 } 
+
+//----------------------------------------
