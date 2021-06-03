37c37f10db0a38ed70caa6e903d80f9279fed7b3
icaroMelo <exemployt@gmail.com>
2021-06-03 14:33:35 -0300

    alguns testes

--- script.js
+++ script.js
@@ -25,16 +25,64 @@ function req7() {
     if (gray === ''){
       event.target.classList.add('gray');
       gray = document.querySelector('.gray');
-      // gray.style.cssText = 'background-color : rgb(128,128,128);'
+     
     } else {
       gray.classList.remove('gray');
       event.target.classList.add('gray');
-      gray = document.querySelector('.gray')
+      gray = document.querySelector('.gray');
     }
 
   })
 }
 req7();
 
+function req9(){
+let tarefas = document.querySelector('#lista-tarefas');
+let completed = '';
+tarefas.addEventListener('dblclick', (event) => {
+ 
+    event.target.classList.add('completed');
+    completed = document.querySelector('.completed');
+})
+ 
+tarefas.addEventListener('dblclick', (event) => {
+    completed.classList.remove('completed');
+    event.target.classList.add('completed');
+    completed = document.querySelector('.completed');
+})
+  
+}
 
+req9();
+
+function req10() {
+
+  let button = document.querySelector('#apaga-tudo');
+ 
+
+  button.addEventListener('click', function(){
+    let lista = document.querySelector('#lista-tarefa');
+
+    lista.innerTex
+
+  })
+
+}
+
+req10();
+
+// function req11(){
+//   let buttoon = document.querySelector('#remove-finalizados');
+//   let remove = document.querySelectorAll('.completed');
+//   let tarefas = document.querySelector('#lista-tarefas');
+
+//   buttoon.addEventListener('click',function(){
+
+//   for (let i = 0; i < remove.length; i +=1) {  
+//       tarefas.removeChild(remove[i]);
+//   }
+// })
+// }
+
+// req11();
 
