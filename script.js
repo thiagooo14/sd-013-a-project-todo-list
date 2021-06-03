1a1a3d14a0e5371b9dcf5f13493f8a1781dd93e1
Bruno Pinho <brunopinho@outlook.com>
2021-06-02 22:03:09 -0300

    realizado requisito 7 (com a ajuda do Rogerio P. da Silva)

--- script.js
+++ script.js
@@ -4,11 +4,25 @@ const taskList = document.getElementById('lista-tarefas');
 
 taskButton.addEventListener('click', createItem);
 
+let taskItens;
+
 function createItem() {
-  let taskItem = document.createElement('li');
-  taskItem.classList.add('tarefa');
+  const taskItem = document.createElement('li');
+  taskItem.classList.add('tarefa'); 
   taskItem.innerText = taskInput.value;
   taskList.appendChild(taskItem);
   //Thread do StackOverFlow que ajudou a resolver a questão de como zerar o input: https://stackoverflow.com/questions/37511093/reset-is-not-a-function-in-javascript
   taskInput.value = "";
+  taskItens = document.querySelectorAll('.tarefa');
+
+
 }
+
+//Com a ajuda do Rogério P. Da Silva
+function itemColor (event) {
+  if (event.target.className.includes('tarefa')) {
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+}
+
+document.body.addEventListener('click', itemColor);
