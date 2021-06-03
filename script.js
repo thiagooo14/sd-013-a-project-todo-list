d675851d6f6723b4b5f722088b208f2966b2e1d7
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-06-03 00:57:18 -0300

    feito ate o desafio 9

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const input = document.querySelector('#texto-tarefa');
-
+const taskList = document.querySelector('#lista-tarefas');
 const btn = document.querySelector('#criar-tarefa');
 
 function generateList() {
@@ -9,7 +9,7 @@ function generateList() {
     const listItem = document.createElement('li');
     listItem.classList.add('items');
     listItem.innerText = input.value;
-    document.querySelector('#lista-tarefas').appendChild(listItem);
+    taskList.appendChild(listItem);
     input.value = '';
   }
 }
@@ -20,9 +20,21 @@ function giveClass(element) {
   if (element.target.className === 'items') {
     const selected = document.querySelector('.selected');
     element.target.classList.add('selected');
-    selected.classList.remove('selected');
+    if (selected !== null) {
+      selected.classList.remove('selected');
+    }
   }
 }
 
 const giveProp = document.querySelector('#lista-tarefas');
 giveProp.addEventListener('click', giveClass);
+
+function giveCompleted(element) {
+  if (element.target.classList.contains('completed') === false) {
+    element.target.classList.add('completed');
+  } else if (element.target.classList.contains('completed')) {
+    element.target.classList.remove('completed');
+  }
+}
+
+taskList.addEventListener('dblclick', giveCompleted);
