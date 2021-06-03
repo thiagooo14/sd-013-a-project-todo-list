36e220dd78d86ca8549a4d7fcddb08d58711275a
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2021-06-03 20:47:00 -0300

    Projeto lista de tarefas - requisito 5 - correçao

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ window.onload = function () {
     createTasks.addEventListener("click", addTasks, false);
   }
   waitCreateTasks();
+
   function addTasks() {
     let inputValue = document.getElementById('texto-tarefa');
     let tasksList = document.getElementById('lista-tarefas');
     let newTasks = document.createElement('li');
     newTasks.textContent=inputValue.value;
     tasksList.appendChild(newTasks);
+    inputValue.value="";
+  }
+
+  function waitItemSelect() {
+    let itemSelect = document.getElementById('lista-tarefas');
+    itemSelect.addEventListener("click", function(i){markItem(i.target.id)}, false)
+  }
+  waitItemSelect();
+
+  function markItem(idItem) {
+
   }
 }
