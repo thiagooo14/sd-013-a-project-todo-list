e9f34d4148afa3a9986baa099fc3775e3b4d3a81
Max Salewski <maxsalewski@gmail.com>
2021-06-02 21:04:34 -0300

    Requisitos 6 ao 10

--- script.js
+++ script.js
@@ -6,5 +6,19 @@ function getInput() {
   console.log(txtbox);
   document.getElementById("lista-tarefas").appendChild(document.createElement("li")).textContent = txtbox;
   document.getElementById("texto-tarefa").value = '';
+}
 
+function changeBg(event) {
+  const liList = document.querySelectorAll('li');
+  liList.forEach(e => e.classList.remove('selectLi'));
+  event.target.classList.add('selectLi');
+}
+
+function line(event) {
+  event.target.classList.toggle('completed');
+}
+
+function clearList() {
+let liList = document.getElementById("lista-tarefas");
+while(liList.firstChild) liList.removeChild(liList.firstChild);
 }
