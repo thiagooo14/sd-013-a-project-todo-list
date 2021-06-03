d4a18712eb0b675248e9450f0a61a4223c9725c5
icaroMelo <exemployt@gmail.com>
2021-06-03 11:38:25 -0300

    realizo requisito 7

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 
     let teste = document.querySelector('#texto-tarefa').value;
     let teste3 = document.createElement('li');
-
     teste3.innerHTML = teste;
+    teste3.className = 'task';
     
     document.querySelector('#lista-tarefas').appendChild(teste3);
     
@@ -15,13 +15,26 @@
     })
 
     
-  }
+  } 
 req5();
 
-// function clear() {
+function req7() {
+  let tarefas = document.querySelector('#lista-tarefas');
+  let gray = '';
+  tarefas.addEventListener('click', (event) => {
+    if (gray === ''){
+      event.target.classList.add('gray');
+      gray = document.querySelector('.gray');
+      // gray.style.cssText = 'background-color : rgb(128,128,128);'
+    } else {
+      gray.classList.remove('gray');
+      event.target.classList.add('gray');
+      gray = document.querySelector('.gray')
+    }
 
-// }
-
+  })
+}
+req7();
 
 
 
