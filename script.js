fd8bd6b50fc2e08446ef86963f801c3c5725b8c0
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-06-03 01:54:06 -0300

    finaliza desafio 11

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('#texto-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const btn = document.querySelector('#criar-tarefa');
-const item = document.querySelector('.items')
+const selectItem = document.getElementsByClassName('items');
 
 function generateList() {
   if (input.value === '') {
@@ -30,7 +30,7 @@ function giveClass(element) {
 const giveProp = document.querySelector('#lista-tarefas');
 giveProp.addEventListener('click', giveClass);
 
-//feito com ajuda do Matheus Duarte. O cara é bom d+
+// feito com ajuda do Matheus Duarte. O cara é bom d+
 function giveCompleted(element) {
   if (element.target.classList.contains('completed') === false) {
     element.target.classList.add('completed');
@@ -43,8 +43,40 @@ taskList.addEventListener('dblclick', giveCompleted);
 
 // https://www.codecademy.com/forum_questions/4fc4e20c48eae700030456bb
 function clearList() {
-    taskList.innerHTML = '';
+  taskList.innerHTML = '';
 }
 
-const btnClear = document.querySelector('#apaga-tudo');
-btnClear.addEventListener('click', clearList);
+const btnDelete = document.querySelector('#apaga-tudo');
+btnDelete.addEventListener('click', clearList);
+
+// https://www.codegrepper.com/code-examples/javascript/remove+element+with+class+name+javascript
+function clearFinished() {
+  const element = document.getElementsByClassName('completed');
+  while (element.length > 0) {
+    element[0].parentNode.removeChild(element[0]);
+  }
+}
+
+const btnClear = document.querySelector('#remover-finalizados');
+btnClear.addEventListener('click', clearFinished);
+
+// function moveUp() {
+//   const teste = this.parentElement;
+
+//   if (teste.previousElementSibling) {
+//     teste.parentNode.insertBefore(teste, teste.previousElementSibling);
+//   }
+// }
+
+// function moveDown() {
+//   // if (selectItem.classList.contains('.selected')) {
+//     const teste = this.parentElement;
+
+//     if (teste.nextElementSibling) {
+//       teste.parentNode.insertBefore(teste.nextElementSibling, teste);
+//     // }
+//   }
+// }
+
+// const btnDown = document.querySelector('#mover-baixo');
+// btnDown.addEventListener('click', moveDown);
