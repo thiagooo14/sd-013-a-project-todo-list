deffa6ea1285713bc2747f7da71028a14fb71a3d
Julio Fallace <julioscoffee@gmail.com>
2021-06-03 20:59:26 -0300

    resolvendo requisitos 9

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ const CriarTarefaBtn = document.querySelector('#criar-tarefa');
 const ol = document.querySelector('#lista-tarefas');
 
 function selecionaItem(event) {
+  const evenTarget = event.target;
   document.querySelector('#selected').style.backgroundColor = 'white';
   document.querySelector('#selected').removeAttribute('id');
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  event.target.id = 'selected';
+  evenTarget.style.backgroundColor = 'rgb(128, 128, 128)';
+  evenTarget.id = 'selected';
+}
+
+function completed(event) {
+  const evenTarget = event.target;
+  evenTarget.classList.toggle('completed');
 }
 
 function addTarefa() {
   const li = document.createElement('li');
   li.classList.add('tarefa');
   li.addEventListener('click', selecionaItem);
+  li.addEventListener('dblclick', completed);
   ol.appendChild(li);
   const tarefa = document.querySelector('#texto-tarefa');
   li.innerText = tarefa.value;
