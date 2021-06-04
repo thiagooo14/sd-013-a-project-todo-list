436d4eaa89904698110ff62317a586b43ea83d66
pedroalles <pedroalles@gmail.com>
2021-06-03 23:56:51 -0300

    Realizado desafio 9.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function clearInput() {
   document.getElementById('texto-tarefa').value = '';
 }
+function setupTaskDubleClick(event) {
+  const element = event.target;
+  element.classList.toggle('completed');
+}
 function setupTaskClick(event) {
   const currentSelectedTask = document.getElementsByClassName('selected')[0];
   const element = event.target;
@@ -9,13 +13,17 @@ function setupTaskClick(event) {
   }
   element.classList.toggle('selected');
 }
+function addTaskEvents(task) {
+  task.addEventListener('click', setupTaskClick);
+  task.addEventListener('dblclick', setupTaskDubleClick);
+}
 function addTask() {
   const inputText = document.getElementById('texto-tarefa').value;
   const orderedList = document.getElementById('lista-tarefas');
-  const newLine = document.createElement('li');
-  newLine.innerText = inputText;
-  newLine.addEventListener('click', setupTaskClick);
-  orderedList.appendChild(newLine);
+  const newTask = document.createElement('li');
+  newTask.innerText = inputText;
+  addTaskEvents(newTask);
+  orderedList.appendChild(newTask);
   clearInput();
 }
 function setupAddButton() {
