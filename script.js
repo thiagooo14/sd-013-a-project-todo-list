802ee96e7c57a3e6905948f49a42d1954e072b3a
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-06-04 00:33:06 -0300

    Adiciona comentarios para melhor entendimento do codigo

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ const input = document.getElementById('texto-tarefa');
 //Botão para adicionar o texto do input na lista "ol".
 function addTask() {
   function newListItem() {
-  let textInput = input.value;
-  let listItem = document.createElement('li');
+  let textInput = input.value; /* textInput vai receber o texto no input da lista. */
+  let listItem = document.createElement('li'); /* listItem cria uma "li". */
 
-  listItem.innerHTML = textInput;
-
+  listItem.innerHTML = textInput; /* Adiciona texto do input na lista ordenada */
+  
   list.appendChild(listItem);
   input.value = '';
+  /* Adiciona listItem (li) como filho da list e volta a deixar o input vazio o fazendo receber uma string vazia. */
   }
   button.addEventListener('click', newListItem);
   }
-addTask();
+addTask(); //Chama a função
