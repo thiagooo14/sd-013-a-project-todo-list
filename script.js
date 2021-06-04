7db19ec3758bda24b9330376ca4bf3c48f7437b5
Diego Vilela <vilelaphp@gmail.com>
2021-06-04 14:23:59 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -2,15 +2,30 @@ const input = document.getElementById('texto-tarefa');
 const btnCreate = document.getElementById('criar-tarefa');
 const btnClear = document.getElementById('apaga-tudo');
 const btnClearCompleted = document.getElementById('remover-finalizados');
+const btnSave = document.getElementById('salvar-tarefas');
 const taskList = document.getElementById('lista-tarefas');
 
-function createTask() {
+/**
+ * Create and append a new task to ol#lista-tarefas.
+ * @param obj The event when adding a new task or the
+ * object retrieved from localStorage when loading the page.
+ */
+function createTask(obj) {
   const li = document.createElement('li');
 
-  li.innerText = input.value;
   li.className = 'task';
+  if (obj.target !== btnCreate) {
+    li.innerText = obj.text;
+    if (obj.completed) li.classList.add('selected');
+  } else {
+    li.innerText = input.value;
+    input.value = null;
+  }
   taskList.appendChild(li);
-  input.value = null;
+}
+
+function createTaskList(list) {
+  list.forEach(createTask); // Each object is passed to createTask
 }
 
 function selectTask(e) {
@@ -38,10 +53,33 @@ function clearTasksCompleted() {
   while (tasksCompleted.length > 0) tasksCompleted[0].remove();
 }
 
+function save() {
+  const listObj = [];
+
+  Array.prototype.map.call(taskList.children, (task) => {
+    listObj.push({
+      text: task.innerHTML,
+      completed: task.classList.contains('selected'),
+    });
+  });
+
+  localStorage.setItem('taskList', JSON.stringify(listObj));
+}
+
+function retrieveTaskListFromLocalStorage() {
+  const list = localStorage.getItem('taskList');
+
+  if (list) {
+    createTaskList(JSON.parse(list));
+  }
+}
+
 window.onload = () => {
+  retrieveTaskListFromLocalStorage();
   btnCreate.addEventListener('click', createTask);
   btnClear.addEventListener('click', clearTaskList);
   btnClearCompleted.addEventListener('click', clearTasksCompleted);
+  btnSave.addEventListener('click', save);
   document.addEventListener('click', selectTask);
   document.addEventListener('dblclick', completeTask);
 };
