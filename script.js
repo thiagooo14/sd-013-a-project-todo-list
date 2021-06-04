8d4776f1a586eb590b65ce86e76a5dc4514198e8
FelipeNevess <felipe012neves@gmail.com>
2021-06-04 15:00:10 -0300

    desafio 7 ao 9

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let listTarefa = document.querySelector('#lista-tarefas');
 let textoTarefa = document.querySelector('#texto-tarefa');
 let buttonAdicionar = document.querySelector('#criar-tarefa');
+const getLi = document.getElementsByTagName('li');
 
 function adicionar() {
   let lis = document.createElement('li');
@@ -10,8 +11,29 @@ function adicionar() {
     listTarefa.appendChild(lis);
     lis.innerHTML = texto;
     textoTarefa.value = '';
-    lis.classList.add('colorir');
   }
 }
 
 buttonAdicionar.addEventListener('click', adicionar);
+
+// Desafio 7 foi com a ajuda de Thiago Carboneri
+
+function selectedClass() {
+  listTarefa.addEventListener('click', (event) => {
+    
+    for (let index = 0; index < getLi.length; index += 1) {
+      getLi[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  });
+}
+
+selectedClass();
+
+function selectedClass_2() {
+  listTarefa.addEventListener('dblclick', (event) => {
+    event.target.classList.toggle('completed');
+  });
+}
+
+selectedClass_2();
