210824669f46b79c17c7ac0b105ba22c39cfa656
jp Viegas <jp.viegas@hotmail.com>
2021-06-04 15:28:45 -0300

    finalizando questao 11

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var input = document.getElementById('texto-tarefa');
 var button = document.getElementById('criar-tarefa');
 var ol = document.querySelector('ol');
+var clear = document.getElementById('apaga-tudo');
+var done = document.getElementsByClassName('completed');
+var remove = document.getElementById('remover-finalizados');
 
 function createListElement() {
   var li = document.createElement('li');
   li.appendChild(document.createTextNode(input.value));
+  li.setAttribute('onclick', 'oneClick()');
+  li.setAttribute('ondblclick', 'dblClick()')
   ol.appendChild(li);
   input.value = '';
 }
@@ -15,12 +20,44 @@ function addListClick() {
   }
 }
 
-function addListEnter(event) {
-  if (input.value.length > 0 && event.keyCode === 13) {
+function addListEnter(e) {
+  if (input.value.length > 0 && e.key === 'Enter') {
   createListElement();
+  }
+}
+
+function oneClick() {
+  var li = document.querySelectorAll('li');
+  for (let i = 0; i < li.length; i += 1) {
+    li[i].classList.remove('bgc');
+  }
+  event.target.classList.toggle('bgc');
+}
+
+function dblClick() {
+  event.target.classList.toggle('completed');
+}
+
+function clearAll() {
+  var li = document.querySelectorAll('li');
+  for (let i = 0; i < li.length; i += 1) {
+    ol.removeChild(li[i]);
+  }
+}
+
+function removeDone() {
+  var li = document.querySelectorAll('li');
+  for (let i = 0; i < li.length; i += 1) {
+    if (li[i].classList.contains('completed')) {
+      ol.removeChild(li[i]);
+    }
   }
 }
 
 button.addEventListener('click', addListClick);
 
 input.addEventListener('keypress', addListEnter);
+
+clear.addEventListener('click', clearAll);
+
+remove.addEventListener('click', removeDone);
