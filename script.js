3bbf51adf9a9ab8044bcf71735438a0c04efce47
pedroalles <pedroalles@gmail.com>
2021-06-03 22:23:05 -0300

    Modificaçoes para atender o Lint.

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ function clearInput() {
   const inputElement = document.getElementById('texto-tarefa');
   inputElement.value = '';
 }
+function setupTaskLineStyle(event) {
+  const element = event.target;
+  element.style.backgroundColor = 'rgb(128,128,128)';
+}
 function addTask() {
   const inputText = document.getElementById('texto-tarefa').value;
   const orderedList = document.getElementById('lista-tarefas');
-  let newLine = document.createElement('li');
+  const newLine = document.createElement('li');
   newLine.innerText = inputText;
   newLine.addEventListener('click', setupTaskLineStyle);
   orderedList.appendChild(newLine);
   clearInput();
-}
-function setupTaskLineStyle(event) {
-  let element = event.target;
-  element.style.backgroundColor = 'rgb(128,128,128)';
 }
 function setupAddButton() {
   const addBt = document.getElementById('criar-tarefa');
