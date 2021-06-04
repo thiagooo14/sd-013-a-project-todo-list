f15c78e501cfe5c997904ef4a52e2383f43c2923
Jose Victor Baptista <dixxbaptista@gmail.com>
2021-06-04 14:56:11 -0300

    corrigido bugs

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const ul = document.querySelector('#lista-tarefas')
 const botao = document.querySelector('#criar-tarefa')
 const input = document.querySelector('#texto-tarefa')
-const li = document.createElement('li')
 
 var textoNulo = ""
-
-// teste
+var tarefas = [];
+console.log(tarefas)
 
 botao.addEventListener('click', adicionarTarefa)
 
 function adicionarTarefa () {
-  li.innerHTML = input.value
-  ul.appendChild(li)
-  input.value = textoNulo
+  let nomeTarefa = input.value
+  tarefas.push(nomeTarefa)
+  for (let i = 0; i < tarefas.length; i++) {
+    let li = document.createElement('li')
+    li.innerHTML = tarefas[i]
+    ul.appendChild(li)
+  }
 }
 
 
