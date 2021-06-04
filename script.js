a673b77be4bfa9b51883426f8c0898879177ca65
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-06-04 03:29:06 -0300

    Resolve requisitos 7 e 8

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ function addTask() {
   button.addEventListener('click', newListItem);
   }
 addTask(); //Chama a função
+
+
+/* Adicionar a cor rgb(128, 128, 128) ao clicar nos itens da lista */
+function changeColor() {
+  function itemColorChange(event) {
+    let listaLi = document.getElementsByTagName('li');
+    for (let i = 0; i < listaLi.length; i += 1) { 
+      if (listaLi[i].classList.contains('color-change')) { 
+        listaLi[i].classList.remove('color-change'); // Garante que a classe "color-change" esteja somente em um elemento. 
+      }
+    }
+    event.target.classList.add('color-change');
+  }
+  list.addEventListener('click', itemColorChange); //Evento "ao clicar" realiza a função para mudar a cor de fundo
+}
+  
+changeColor();
