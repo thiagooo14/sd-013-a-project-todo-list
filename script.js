f7ac8d5b8d23a29608acc3996f2eec2fb412a0fa
Daniel <daniel.melo.oliveira27.com.br>
2021-06-04 18:10:36 -0300

    requisitos 5 ao 9

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 
-const elementList = document.createElement('li')
-
-function addTask(evento){
-  const elementText = document.getElementById('texto-tarefa').value 
-  const list = document.getElementById('lista-tarefas')
-  const elementList = document.createElement('li')
-  elementList.innerHTML = elementText
-  list.appendChild(elementList)
-  elementList.addEventListener('dblclick', finished)
+function addTask(evento) {
+  const elementText = document.getElementById('texto-tarefa').value;
+  const list = document.getElementById('lista-tarefas');
+  const elementList = document.createElement('li');
+  elementList.innerHTML = elementText;
+  list.appendChild(elementList);
+  document.getElementById('texto-tarefa').value = '';
+  elementList.addEventListener('dblclick', finished);
+  elementList.addEventListener('click', changeSelectedColor);
 }
 
-
+function changeSelectedColor(evento) {
+  let list = document.querySelectorAll("li")
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].classList.contains('selected')) {
+      list[i].classList.remove('selected')
+    }
+    
+  }
+  evento.target.classList.add('selected')
+   
+  
+}
 function finished(evento) {
-  const selected = evento.target
   if (evento.target.classList.contains('completed')) {
     evento.target.classList.remove('completed');
+    
   } else {
     evento.target.classList.add('completed');
+    evento.target.classList.remove('selected');
   }
 }
