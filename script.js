92ef4a9bb6304db3a1fb66eb9885d56673f77eff
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2021-06-04 19:54:28 -0300

    requisito 8

--- script.js
+++ script.js
@@ -5,6 +5,19 @@
 /* eslint-disable prefer-const */
 /* eslint-disable func-names */
 /* eslint-disable prefer-arrow-callback */
+function setColorTask(event) {
+  const selectedTask = document.getElementsByClassName('task');
+  const grayBackgroundItem = 'rgb(128, 128, 128)';
+  if (event.target.style.backgroundColor === grayBackgroundItem) {
+    event.target.style.backgroundColor = 'white';
+  } else {
+    for (let indexColor = 0; indexColor < selectedTask.length; indexColor += 1) {
+      selectedTask[indexColor].style.backgroundColor = 'white';
+    }
+    event.target.style.backgroundColor = grayBackgroundItem;
+  }
+}
+
 function createTask() {
   const inputTask = document.querySelector('#texto-tarefa');
   const list = document.querySelector('#lista-tarefas');
@@ -15,27 +28,10 @@ function createTask() {
       let newTask = document.createElement('li');
       newTask.className = 'task';
       newTask.innerHTML = inputTask.value;
+      newTask.addEventListener('click', setColorTask);
       list.appendChild(newTask);
       inputTask.value = '';
     }
   });
 }
 createTask();
-
-function setColorTask() {
-  const selectedTask = document.querySelectorAll('#lista-tarefas');
-  const grayBackgroundItem = 'rgb(128,128,128)';
-  for (let index = 0; index < selectedTask.length; index += 1) {
-    selectedTask[index].addEventListener('click', function (event) {
-      if (event.target.style.backgroundColor === grayBackgroundItem) {
-        event.target.style.backgroundColor = 'white';
-      } else {
-        for (let indexColor = 0; indexColor < selectedTask.length; indexColor += 1) {
-          selectedTask[indexColor].style.backgroundColor = 'white';
-        }
-        event.target.style.backgroundColor = grayBackgroundItem;
-      }
-    });
-  }
-}
-setColorTask();
