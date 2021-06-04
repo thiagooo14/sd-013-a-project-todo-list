b071812f6a9f3e7b068a7c47c913c1ee07c72bd2
Diego Vilela <vilelaphp@gmail.com>
2021-06-04 13:13:32 -0300

    Requisito 11

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('texto-tarefa');
 const btnCreate = document.getElementById('criar-tarefa');
 const btnClear = document.getElementById('apaga-tudo');
+const btnClearCompleted = document.getElementById('remover-finalizados');
 const taskList = document.getElementById('lista-tarefas');
 
 function createTask() {
@@ -31,9 +32,16 @@ function clearTaskList() {
   while (taskList.firstChild) taskList.firstChild.remove();
 }
 
+function clearTasksCompleted() {
+  const tasksCompleted = document.getElementsByClassName('completed');
+
+  while (tasksCompleted.length > 0) tasksCompleted[0].remove();
+}
+
 window.onload = () => {
   btnCreate.addEventListener('click', createTask);
   btnClear.addEventListener('click', clearTaskList);
+  btnClearCompleted.addEventListener('click', clearTasksCompleted);
   document.addEventListener('click', selectTask);
   document.addEventListener('dblclick', completeTask);
 };
