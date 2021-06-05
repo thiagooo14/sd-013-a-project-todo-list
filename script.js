6fa0dba90da89e2a01bb6e6a88f1def6d6713c17
Jose Victor Baptista <dixxbaptista@gmail.com>
2021-06-05 15:00:18 -0300

    ate ex6 ok

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-const ul = document.querySelector('#lista-tarefas')
+const ol = document.querySelector('#lista-tarefas')
 const botao = document.querySelector('#criar-tarefa')
 const input = document.querySelector('#texto-tarefa')
 
 var textoNulo = ""
-var tarefas = [];
-console.log(tarefas)
 
 botao.addEventListener('click', adicionarTarefa)
 
 function adicionarTarefa () {
-  let nomeTarefa = input.value
-  tarefas.push(nomeTarefa)
-  for (let i = 0; i < tarefas.length; i++) {
-    let li = document.createElement('li')
-    li.innerHTML = tarefas[i]
-    ul.appendChild(li)
-  }
+  const li = document.createElement('li')
+  li.innerHTML = input.value
+  li.classList.add('tarefa')
+  li.addEventListener('click', selecionarTarefa)
+  li.addEventListener('dbclick', tarefaCompleta)
+  ol.appendChild(li)
+  input.value = textoNulo
 }
 
+function selecionarTarefa () {
 
+}
+
+function tarefaCompleta () {
+
+}
 
 
 
