381e6c7edb0a7f3eef14b7be556ad783e43b337b
Gustavo Mauricio <gmauricio2072gmail.com>
2021-06-05 17:02:14 -0300

    Corrigindo linter

--- script.js
+++ script.js
@@ -1,62 +1,46 @@
 const textInput = document.querySelector('#texto-tarefa');
 const botaoAdc = document.querySelector('#criar-tarefa');
+const item = document.getElementsByTagName('li');
+const botaoLimpar = document.querySelector('#apaga-tudo');
 // let listaOrdenada = document.querySelector('#lista-tarefas');
-const item = document.querySelectorAll('li');
 
-//Adicionar Lista Ordenada
-let listaOrdenada = document.createElement('ol');
+// Adicionar Lista Ordenada
+const listaOrdenada = document.createElement('ol');
 document.body.appendChild(listaOrdenada);
-listaOrdenada.setAttribute("id" , "lista-tarefas");
-// listaOrdenada.addEventListener('click', mudaCor)
+listaOrdenada.setAttribute('id', 'lista-tarefas');
+//  listaOrdenada.addEventListener('click', selectClass)
 
-//Adicionar Botão
-function adicionaTarefa() {
-let item = document.createElement('li');
-listaOrdenada.appendChild(item);
-item.innerHTML = textInput.value;
-textInput.value = "";
-item.addEventListener('click', mudaCor);
-item.addEventListener('dblclick', duploClick)		
- }
-
-botaoAdc.addEventListener('click', adicionaTarefa)
+// EFEITO RISCADO
+function duploClick(event) {
+  event.target.classList.toggle('completed');
+}
 
 // Adiciona cor ao item clicado.
-
-function mudaCor(event){
-	for (let i = 0; i < item.length; i += 1){
-		item[i].removeAttribute('class');
-	}
-		event.target.classList.toggle('selected');
+function mudaCor(event) {
+  for (let i = 0; i < item.length; i += 1) {
+    item[i].id = 'unselected';
+  }	event.target.id = 'selected';
+}
+// Adicionar Botão
+function adicionaTarefa() {
+  const item = document.createElement('li');
+  listaOrdenada.appendChild(item);
+  item.innerHTML = textInput.value;
+  textInput.value = '';
+  item.id = 'unselected';
+  item.classList.add('item');
+  item.addEventListener('click', mudaCor);
+  item.addEventListener('dblclick', duploClick);
 }
 
-
-// function mudaCor(event){
-	
-// 			removeStyles();
-// 			event.target.classList.toggle('selected');
-// 	}
-
-
-// function removeStyles(){
-// 	{
-// 		item[i].removeAttribute('class');
-// 	}
-// }
-
-//EFEITO RISCADO
-function duploClick(event){
-	event.target.classList.toggle('completed');
-}
+botaoAdc.addEventListener('click', adicionaTarefa);
 
 // BOTÃO LIMPAR LISTA
-const botaoLimpar = document.querySelector('#apaga-tudo');
-console.log(botaoLimpar)
-
-function limparLista(){
-	remove(item)
-	// for(let i = 0;i < item.length;i++){
-	// 	item[i].innerHTML=""
-	// }
+function resetBoard() {
+  const li = document.querySelectorAll('.item');
+  for (let i = 0; i < li.length; i += 1) {
+    listaOrdenada.removeChild(li[i]);
+  }
+// listaOrdenada.innerHTML = null;
 }
-botaoLimpar.addEventListener('click', limparLista)
+botaoLimpar.addEventListener('click', resetBoard);
