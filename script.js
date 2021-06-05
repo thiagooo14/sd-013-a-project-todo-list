776278a5ed89ca906af8f21623f8dc4540508efe
Lanai Conceição <conceicao.lanai@gmail.com>
2021-06-05 11:01:12 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -12,17 +12,29 @@ selectButton.addEventListener('click', () => {
   createTask.value = '';
 });
 
-// Requisito 7
+// Requisito 7 e 8
 // Desenvolvido com a ajuda do estudante Trybe Thiago Carboneri
-function selectedClass() {
-  const getOl = document.querySelector('ol');
-  getOl.addEventListener('click', (event) => {
-    const getLi = document.getElementsByTagName('li');
-    for (let index = 0; index < getLi.length; index += 1) {
-      getLi[index].classList.remove('selected');
+function selectedClass() { // criar uma função
+  const getOl = document.querySelector('ol'); // que dê o caminho de onde faremos o evento
+  getOl.addEventListener('click', (event) => { // criar o evento de click
+    const getLi = document.getElementsByTagName('li'); // que vai ser executado dentro das lis, portanto o caminho delas
+    for (let index = 0; index < getLi.length; index += 1) { // e para percorrer cada uma dessas lis
+      getLi[index].classList.toggle('selected'); // onde removeremos a classe selected caso ela já exista e caso ela não exista vamos criá-la usando o toggle
     }
-    event.target.classList.add('selected');
   });
 }
 
 selectedClass();
+
+// Requisito 9
+function selectedClassDoubleClick() { // criar uma função
+  const getOl = document.querySelector('ol'); // que dê o caminho de onde faremos o evento
+  getOl.addEventListener('dblclick', (event) => { // criar o evento de click
+    const getLi = document.getElementsByTagName('li'); // que vai ser executado dentro das lis, portanto o caminho delas
+    for (let index = 0; index < getLi.length; index += 1) { // e para percorrer cada uma dessas lis
+      getLi[index].classList.toggle('completed'); // onde removeremos a classe completed caso ela já exista e caso ela não exista vamos criá-la usando o toggle
+    }
+  });
+}
+
+selectedClassDoubleClick();
