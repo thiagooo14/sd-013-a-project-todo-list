53a4c7ebbc4111af2dfc39b911c8acc7240dcbe1
Jean V  Pacheco <jeanvitorp@gmail.com>
2021-06-05 15:28:29 -0300

    Refatorando o código para adicionar botão e input no HTML

--- script.js
+++ script.js
@@ -1,60 +1,19 @@
-//Chama o conteúdo (adicionar o input dentro do content)
-// var content = document.querySelector('#content');
-// console.log(content);
-
-// Cria o input
-var inputField = document.createElement('input');
-inputField.id = 'texto-tarefa';
-inputField.setAttribute('type', 'text');
-document.body.appendChild(inputField);
-
-
-// Cria o botão
-var addButton = document.createElement('button');
-addButton.id = 'criar-tarefa';
-addButton.innerText = 'Adicionar';
-document.body.appendChild(addButton);
-
+// Pega o botão
+var getButton = document.querySelector('#criar-tarefa');
+// Pega o input
+var inputField = document.querySelector('#addTaskButton');
+//Lista completa de tarefas
+let fullList = document.querySelector('#lista-tarefas');
 // Adiciona as tarefas à lista
 function addTask (){
-  addButton.addEventListener('click', function (){
-    //pega o valor do campo input
-    let newTask = inputField.value;
+  getButton.addEventListener('click', function (){
+    //cria uma nova li
     let newElement = document.createElement('li');
-    //coloca o valor do campo input numa nova li e adiciona à lista
-    newElement.innerText = newTask;
-    let fullList = document.querySelector('#lista-tarefas');
+     //coloca o valor do campo input numa nova li e adiciona à lista
+    newElement.innerText = inputField.value;;
     fullList.appendChild(newElement);
-    //zera o campo
-    inputField.value = '';
+    //Zera o campo
+    inputField.value = null;
   })
 };
 addTask();
-
-// Adiciona uma classe na tarefa clicada
-
-function selectedTask (){
-  let fullList = document.getElementById('lista-tarefas');
-  fullList.addEventListener ('click', function(event) {
-    event.target.className = 'selected';
-  })
-}
-selectedTask();
-
-// Coloca o fundo cinza na tarefa clicada
-
-// Risca o item ao ser clicado duas vezes
-// Arrumar para poder concatenar classes
-
-function completedTask (){
-  let fullList = document.getElementById('lista-tarefas');
-  fullList.addEventListener('dblclick', function (event) {
-    if (event.target.className !== 'completed') {
-      event.target.className = 'completed'
-    }
-    else {
-      event.target.classLista.remove('completed');
-    }
-  })
-}
-completedTask ();
