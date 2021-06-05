70298f3e86d0a4f756efa3b6713a878566893e17
GustavoPia <piagja@gmail.com>
2021-06-05 14:56:44 -0300

    Atendeu ao requisito 7 e realizou melhorias no visual, script e html

--- script.js
+++ script.js
@@ -10,7 +10,21 @@ const taskList = document.querySelector('#lista-tarefas');
 const createTask = () => {
   buttonCreateTask.addEventListener('click', () => {
     const inputValue = inputCreateTask.value;
-    taskList.innerHTML += `<li>${inputValue}</li>`;
-    inputCreateTask.value = '';
+    if (inputValue === '' || inputValue === null || inputValue === undefined) {
+      console.log('Valor inválido');
+    } else {
+      taskList.innerHTML += `<li class="task-item">${inputValue}</li>`;
+      inputCreateTask.value = '';
+    }
+
+    highlightTask();
+  });
+};
+
+const highlightTask = () => {
+  const taskItem = document.querySelector('.task-item');
+  taskItem.addEventListener('click', (event) => {
+    const { classList } = event.target;
+    console.log(classList.toggle('gray-bgcolor'));
   });
 };
