1b0758b79876fd48e238a5dfcecbb916b32b4c0a
Jéssica Queiroz <gnr0034@gmail.com>
2021-06-05 11:42:37 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let buttomNewTask = document.getElementById('criar-tarefa');
-
+const buttomNewTask = document.getElementById('criar-tarefa');
 buttomNewTask.addEventListener('click', () => {
   let newTask = document.getElementById('texto-tarefa');
   let texto = document.getElementById('texto-tarefa').value;
@@ -7,7 +6,22 @@ buttomNewTask.addEventListener('click', () => {
   let ol = document.querySelector('ol');
   li.innerHTML = texto;
   ol.appendChild(li);
+  li.classList.add('tarefa');
   newTask.value = '';  
-})
+});
 
+function backgroundGray() {
+  const listaDeTarefas = document.getElementById('lista-tarefas');
+  console.log(listaDeTarefas);
+  listaDeTarefas.addEventListener('click', (e) => {
+    if (e.target.classList.contains('tarefa')){
+    const listaLis = document.getElementsByClassName('tarefa');    
+    for (let i = 0; i < listaLis.length; i += 1){
+        listaLis[i].classList.remove('backgroundColorGray');
+      } 
+      e.target.classList.add('backgroundColorGray');
+    }
+  });
+}
 
+backgroundGray();
