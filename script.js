7ac2385332b84ab7cafe43f6eb09faa0a877140c
Debora Teodorico <debora.steodorico@gmail.com>
2021-06-05 10:51:00 -0300

    Adicionado questao 8

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+function adicionaCor(evento) {
+  const elementoClicado = evento.target;
+  const todosLi = document.querySelectorAll('.cor-li');
+  for (let i = 0; i < todosLi.length; i += 1) {
+    const posicaoLi = todosLi[i];
+    if (posicaoLi.classList.contains('clicado')) {
+      posicaoLi.classList.remove('clicado');
+    }
+  }
+  elementoClicado.classList.add('clicado');
+}
 const button = document.querySelector('#criar-tarefa');
+// Adiciona addEventListener:
 button.addEventListener('click', () => {
+  // Guarda o texto que foi digitado no input:
   const input = document.querySelector('#texto-tarefa');
   const texto = input.value;
-  const criarLi = document.createElement('li');
-  criarLi.innerHTML = texto;
-  criarLi.classList.add('cor-li');
-  criarLi.addEventListener('click', (evento) => {
-    const elementoClicado = evento.target;
-    elementoClicado.style.backgroundColor = 'rgb(128, 128, 128)';
-  });
+  // Cria o li:
+  const novaLi = document.createElement('li');
+  novaLi.innerHTML = texto;
+  // Adicona classe a li:
+  novaLi.classList.add('cor-li');
+  novaLi.addEventListener('click', adicionaCor);
   const lista = document.getElementById('lista-tarefas');
-  lista.appendChild(criarLi);
+  lista.appendChild(novaLi);
   input.value = '';
 });
