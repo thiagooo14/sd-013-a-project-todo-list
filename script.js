04156d46a7e7729376baac36421f0b058616dfb7
PC <sthecso>
2021-06-05 03:03:42 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const buttonTask = document.querySelector("#criar-tarefa");
-buttonTask.addEventListener("click", function(){
-    const input = document.querySelector("#texto-tarefa");
-    const textValue = input.value;
-    const createList = document.createElement('li');
-    createList.innerText = textValue;
-    const olList = document.querySelector("#lista-tarefas");
-    olList.appendChild(createList);
-    input.value = '';
+const buttonTask = document.querySelector('#criar-tarefa');
+buttonTask.addEventListener('click', function() {
+  input = document.querySelector('#texto-tarefa');
+  const textValue = input.value;
+  const createList = document.createElement('li');
+  createList.innerText = textValue;
+  createList.className = 'task'
+  const olList = document.querySelector("#lista-tarefas");
+  olList.appendChild(createList);
+  input.value = '';
 }); 
+const li = document.getElementsByClassName("task");
+const olList = document.querySelector("ol");
+function deleteTask() {
+    olList.addEventListener("click", (event)=> {
+    for (let index = 0; index < li.length; index += 1) {
+        li[index].classList.remove("selected");
+    };
+    event.target.classList.add("selected");
+});
+}
+deleteTask();
