ee4db6d2931616e5e4e54949d5c3a7254c7b33f3
Emídio Rebouças <emidioreb@gmail.com>
2021-06-06 10:55:19 -0300

    requirement 2 done

---
+++ script.js
@@ -0,0 +1,7 @@
+// const getBody = document.querySelector('body');
+// const getHeader = document.createElement('header');
+// getHeader.className = 'header';
+// getHeader.innerText = 'Minha Lista de Tarefas';
+// getBody.appendChild(getHeader);
+
+
