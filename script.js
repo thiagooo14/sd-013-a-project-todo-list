bff4e3a26518f204a4a2c9639ee543a6df4350c5
Gabriel Lenz <lenzordmusic@gmail.com>
2021-06-05 20:07:58 -0400

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-
+//------------------------------------------------------------------//
+//---------------------------REQUISITO 5----------------------------//
+//------------------------------------------------------------------//
 
 function addTask() {
   let valueInput = document.querySelector("#texto-tarefa");
   let list = document.createElement('li');
   list.innerHTML = valueInput.value
+  list.classList.add('tasks')
   document.querySelector("#lista-tarefas").appendChild(list)
   valueInput.value = "";
 }
+
+//------------------------------------------------------------------//
+//---------------------------REQUISITO 7----------------------------//
+//------------------------------------------------------------------//
+
+const ol = document.querySelector('#lista-tarefas');
+const lis = document.getElementsByTagName('li');
+ol.addEventListener('click', (event) => {
+if (event.target.classList.contains('tasks')) {
+  for (let i = 0; i < lis.length; i += 1) {
+    if (lis[i].classList.contains('selected')){
+    lis[i].classList.remove('selected');
+    }
+  }
+  event.target.classList.add('selected');
+}
+});
+
+
