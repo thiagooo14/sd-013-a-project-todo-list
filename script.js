b1e9cca614778626cc3e202e5e844e7726afeb8e
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-06-06 00:33:46 -0300

    resolvendo requesitos 5 e 6

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function inicia() {
-  
-}
+const inicia = () => {
+  document.querySelector('#criar-tarefa').addEventListener('click', function() {
+    const listaTarefas = document.querySelector('#lista-tarefas');
+    const textoTarefa = document.querySelector('#texto-tarefa');
+    const li = document.createElement('li');    
+    console.log(listaTarefas);
+    console.log(textoTarefa);
+    li.textContent = textoTarefa.value;
+    listaTarefas.appendChild(li);
+    textoTarefa.value = '';
+    textoTarefa.focus();
+  });
+};
+
+
+
+window.addEventListener('load', inicia);
