2ee5107fe62d7847ec2097537457c2b5049610d6
Ramon Vitor <vitorramon@hotmail.com>
2021-06-06 20:01:22 -0300

    Questão 4

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function listaOrdenada() {
+  let listaTarefas = document.getElementById('lista-tarefas');
+  for (let i = 0; i < 10; i += 1) {
+    let lista = document.createElement('li');
+    listaTarefas.appendChild(lista);
+  }
+}
+listaOrdenada();
