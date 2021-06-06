9bedf09924a78453c8287ff6cea053731adb021f
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-06-06 05:41:49 -0300

    Adiciona requisito 12

--- script.js
+++ script.js
@@ -6,6 +6,39 @@ const botaoRemoveFinalizadas = document.querySelector('#remover-finalizados');
 const botaoRemoveSelec = document.querySelector('#remover-selecionado');
 const botaoSubir = document.querySelector('#mover-cima');
 const botaoDescer = document.querySelector('#mover-baixo');
+const botaoSalvar = document.querySelector('#salvar-tarefas');
+
+function recuperaTexto() {
+  if (localStorage.getItem('textoLista')){
+    let textos = localStorage.getItem('textoLista').split('\n');
+    let classes = localStorage.getItem('classeLi').split('\n');
+    const tamanho = textos.length;
+    console.log(textos);
+    console.log(classes);
+      for (let i =0; i < tamanho; i += 1) {
+        const li = document.createElement('li');
+        li.className = classes [i];
+        li.innerText = textos[i];
+        li.addEventListener('click', trocaCorTarefa);
+        li.addEventListener('dblclick', completarTarefa);
+        listaTarefas.appendChild(li);
+      }
+    }
+  }
+  
+  function salvartarefas() {
+    let texto = listaTarefas.innerText;
+    console.log(texto);
+    let classes = "";
+    let listaclasse = document.querySelectorAll('li');
+    for (let i =0; i < listaclasse.length; i += 1) {
+      classes += listaclasse[i].className + '\n'
+    }
+    console.log(classes);
+    localStorage.setItem('textoLista', texto);
+    localStorage.setItem('classeLi', classes);
+    console.log(localStorage);
+  }
 
 function verificaSelected() {
   let verifica = false;
@@ -89,10 +122,12 @@ function criarTarefa() {
   li.addEventListener('dblclick', completarTarefa);
   listaTarefas.appendChild(li);
 }
-
+window.document.onload = recuperaTexto();
 botaoAdicionar.addEventListener('click', criarTarefa);
 botaoApagar.addEventListener('click', apagaLista);
 botaoRemoveFinalizadas.addEventListener('click', apagaFinalizadas);
 botaoRemoveSelec.addEventListener('click', removeSelect);
 botaoSubir.addEventListener('click', subirItem);
 botaoDescer.addEventListener('click', descerItem);
+botaoSalvar.addEventListener('click', salvartarefas);
+
