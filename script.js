c0ab4352dd26ec51ece162f048ef707c6359a2f5
Yuri Garrido <y.garrido.yuri@gmail.com>
2021-06-06 09:35:03 -0300

    faz o requisito 9

--- script.js
+++ script.js
@@ -1,47 +1,3 @@
-/* /*  function criarItem (tarefa){
-  const item = document.createElement('li');
-  item.innerHTML = `<div> ${tarefa} <div/>`
-
-  document.getElementById('lista-tarefas').appendChild(item);
-} 
-
-
- */
-/* 
----------------------------------------------- */
-/* let botaoCria = document.querySelector('#criar-tarefa');
-botaoCria.addEventListener('click', input);
-
-let lista = document.querySelector('#lista-tarefas');
-/* --------------------------------------------- */
-/* const itemLista =  document.getElementsByClassName('.pinta');
-function input (){
-
-let input = document.querySelector('#texto-tarefa');
-
-let valor = input.value;
-let item = document.createElement('li');
-document.querySelector('#lista-tarefas').appendChild(item);
-item.innerHTML = valor;
-item.className = 'pinta';
-item.addEventListener('click', click);
-input.value = '';
-}  */
- 
-//------------------------ */----------------------------------------------
-/* let li = document.querySelectorAll('.pinta');
-
-
-function click(event) {
-for (let index = 0; index < li.length; index += 1) {
-li[index].className = 'pinta';
-}
-event.target.className = 'cinza';
-
-} */
-
-//-------------------------------------------------------------------------
-
 const list = document.getElementById('lista-tarefas');
 const button = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
@@ -65,14 +21,14 @@ lastItem.classList.remove('cinza');
 }
 
 
-/* function completed(event) {
+ function completed(event) {
 if (event.target.classList.contains('completed')) {
 event.target.classList.remove('completed');
 } else {
 event.target.classList.add('completed');
 }
 }
-**/
+
 button.addEventListener('click', createNew);
 list.addEventListener('click', selecting);
 list.addEventListener('dblclick', completed);
