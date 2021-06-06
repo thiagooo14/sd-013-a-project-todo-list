c0acf5e08bb5b9beb1c5cb3408b711f0237c5c79
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-06-06 12:23:44 -0300

    Adiciona requisito 10

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const createTaskButton = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
+const buttonClearAll = document.querySelector('#apaga-tudo');
+const buttonRemoveFinished = document.querySelector('#remover-finalizados');
 
 function createTask() {
   const taskTextInput = document.querySelector('#texto-tarefa').value;
@@ -14,16 +16,40 @@ createTaskButton.addEventListener('click', createTask);
 
 function focusColorList() {
   const taskListItems = document.querySelectorAll('.item-list');
-  for (let i = 0; i < taskListItems.length; i += 1) {
-    taskListItems[i].addEventListener('click', changeColorFocus);
+  for (let index = 0; index < taskListItems.length; index += 1) {
+    taskListItems[index].addEventListener('click', changeColorFocus);
+    taskListItems[index].addEventListener('dblclick', setCompletedStatus)
   }
 }
 focusColorList();
 
 function changeColorFocus(event) {
-  const actualFocus = document.querySelector('.selected');
-  if (actualFocus) {
-    actualFocus.className = 'item-list';
+  let taskListItems = document.querySelectorAll('.item-list');
+  for (let index = 0; index < taskListItems.length; index += 1) {
+    taskListItems[index].style.backgroundColor = '';
   }
-  event.target.className = 'item-list selected';
+  event.target.style.backgroundColor = 'rgb(128,128,128';
 }
+
+function setCompletedStatus(event) {
+  if (event.target.className == 'item-list completed') {
+    event.target.className = 'item-list';
+  } else {
+    event.target.className += 'completed';
+  }
+}
+
+function clearAll() {
+  let itemsToClear = document.querySelector('#lista-tarefas');
+  itemsToClear.innerHTML = '';
+}
+buttonClearAll.addEventListener('click', clearAll);
+
+function removeFinished() {
+  let itemsForRemove = document.querySelectorAll('.completed');
+  let allItems = document.querySelector('#lista-tarefas');
+  for (let index = 1; index < itemsForRemove.length; index += 1) {
+    allItems.removeChild(itemsForRemove[index]);
+  }
+}
+buttonRemoveFinished.addEventListener('click', removeFinished);
