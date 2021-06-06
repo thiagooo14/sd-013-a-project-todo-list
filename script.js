d9696ba4468519c3c5587319ccee471c2717842d
PC <sthecso>
2021-06-06 00:19:37 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const buttonTask = document.querySelector('#criar-tarefa');
+const olList = document.querySelector('ol');
+const li = document.getElementsByTagName('li');
 buttonTask.addEventListener('click', function() {
-  input = document.querySelector('#texto-tarefa');
+  const input = document.querySelector('#texto-tarefa');
   const textValue = input.value;
   const createList = document.createElement('li');
   createList.innerText = textValue;
-  createList.className = 'task'
+  createList.className = 'task';
   const olList = document.querySelector("#lista-tarefas");
   olList.appendChild(createList);
   input.value = '';
 }); 
-const li = document.getElementsByClassName("task");
-const olList = document.querySelector("ol");
 function deleteTask() {
-    olList.addEventListener("click", (event)=> {
+  olList.addEventListener('click', (event) => {
     for (let index = 0; index < li.length; index += 1) {
-        li[index].classList.remove("selected");
-    };
-    event.target.classList.add("selected");
-});
-}
+      li[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  });
+} 
 deleteTask();
+
+function risk() {
+olList.addEventListener('dblclick', (event) => {
+   event.target.classList.add('completed');
+   }
+)}
+risk();
