b4708e4ca6108ce8ae65e8532d704d3e13fd6424
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-06-06 05:03:27 -0300

    resolvendo requesitos 11 e 14

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const listaTarefas = document.querySelector('#lista-tarefas');
 
-
 const creatingTask = () => {
   document.querySelector('#criar-tarefa').addEventListener('click', () => {
     const textoTarefa = document.querySelector('#texto-tarefa');
@@ -19,7 +18,7 @@ const creatingTask = () => {
 
 window.addEventListener('load', creatingTask);
 
-const selectTask = () => {  
+const selectTask = () => {
   listaTarefas.addEventListener('dblclick', (event) => {
     event.target.classList.toggle('completed');
     localStorage.setItem('completed', event.target.classList);
@@ -28,14 +27,14 @@ const selectTask = () => {
 
 window.addEventListener('load', selectTask);
 
-const completedTask = () => {
+const completeTask = () => {
   listaTarefas.addEventListener('click', (event) => {
     event.target.classList.toggle('selected');
     localStorage.setItem('selected', event.target.classList);
   });
 };
 
-window.addEventListener('click', completedTask);
+window.addEventListener('load', completeTask);
 
 const deleteAllTasks = () => {
   const deleteTask = document.querySelector('#apaga-tudo');
@@ -44,4 +43,26 @@ const deleteAllTasks = () => {
   });
 };
 
-window.addEventListener('click', deleteAllTasks);
+window.addEventListener('load', deleteAllTasks);
+
+const deleteSelectedTasks = () => {
+  const completedTask = document.querySelector('#remover-selecionado');
+  completedTask.addEventListener('click', () => {
+    const taskSelected = document.querySelector('.selected');
+    taskSelected.parentNode.removeChild(taskSelected);
+  });
+};
+
+window.addEventListener('load', deleteSelectedTasks);
+
+const deleteCompletedTasks = () => {
+  const completedTask = document.querySelector('#remover-finalizados');
+  completedTask.addEventListener('click', () => {
+    const taskCompleted = document.querySelectorAll('.completed');
+    for (let i = 0; i < taskCompleted.length; i += 1) {
+      taskCompleted[i].parentNode.removeChild(taskCompleted[i]);
+    }
+  });
+};
+
+window.addEventListener('load', deleteCompletedTasks);
