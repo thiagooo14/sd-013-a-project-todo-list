f04bad4a99b4aed50655a90e3ea3e9d8c77336db
Thiago <thiagoc.carboneri@gmail.com>
2021-06-06 19:43:08 -0300

    resolução do exercicio  13

--- script.js
+++ script.js
@@ -98,10 +98,10 @@ removeCompleted();
 
 function saveTasks() {
     let button = document.getElementById('salvar-tarefas')
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         lista = document.getElementById('lista-tarefas');
-        localStorage.setItem('savedtasks',lista.innerHTML);
-})
+        localStorage.setItem('savedtasks', lista.innerHTML);
+    })
 }
 saveTasks();
 lista = document.getElementById('lista-tarefas');
@@ -124,3 +124,43 @@ removeSelected();
 lista = document.getElementById('lista-tarefas');
 lista.innerHTML = localStorage.getItem('savedtasks');
 // estilo CSS do id listComponents adquirido do site: https://stackoverflow.com/questions/40915850/css-ordered-list-styling-before-margins
+
+function scrollDown() {
+    listaOl = document.getElementById('lista-tarefas');
+    let button = document.getElementById('salvar-tarefas');
+    let button2 = document.createElement('button');
+    button2.innerText = "down";
+    button2.id = 'mover-baixo'
+    button.after(button2);
+    button2.addEventListener('click', function () {
+        listas = document.querySelectorAll('li')
+        for (let i = 0; i < listas.length-1; i++) {
+            if (listas[i].classList.contains('selected')) {
+                listaOl.insertBefore(listas[i+1],listas[i]);
+                break;
+            }
+
+        }
+    })
+}
+scrollDown();
+
+function scrollUp() {
+    listaOl = document.getElementById('lista-tarefas');
+    let button = document.getElementById('mover-baixo');
+    let button2 = document.createElement('button');
+    button2.innerText = "up";
+    button2.id = 'mover-cima'
+    button.after(button2);
+    button2.addEventListener('click', function () {
+        listas = document.querySelectorAll('li')
+        for (let i = 1; i < listas.length; i++) {
+            if (listas[i].classList.contains('selected')) {
+               listaOl.insertBefore(listas[i],listas[i-1])
+                break; 
+            }
+
+        }
+    })
+}
+scrollUp();
