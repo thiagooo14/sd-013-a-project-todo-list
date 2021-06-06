e2af98c95ac47af3663798b1aedf16353c0137d3
PC <sthecso>
2021-06-06 05:13:11 -0300

    Correção do requisito 9

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const buttonTask = document.querySelector('#criar-tarefa');
 const olList = document.querySelector('ol');
 const li = document.getElementsByTagName('li');
-buttonTask.addEventListener('click', function() {
+
+buttonTask.addEventListener('click', () => {
   const input = document.querySelector('#texto-tarefa');
   const textValue = input.value;
   const createList = document.createElement('li');
   createList.innerText = textValue;
   createList.className = 'task';
-  const olList = document.querySelector("#lista-tarefas");
+  const olList = document.querySelector('#lista-tarefas');
   olList.appendChild(createList);
   input.value = '';
-}); 
+});
+
 function deleteTask() {
   olList.addEventListener('click', (event) => {
     for (let index = 0; index < li.length; index += 1) {
@@ -18,12 +20,12 @@ function deleteTask() {
     }
     event.target.classList.add('selected');
   });
-} 
+}
 deleteTask();
 
-function risk() {
+  function risk() {
 olList.addEventListener('dblclick', (event) => {
-   event.target.classList.add('completed');
-   }
+  event.target.classList.toggle('completed');
+    }
 )}
 risk();
