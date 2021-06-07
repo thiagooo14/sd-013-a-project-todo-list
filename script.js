ef9b04105323b942f0623060aa5ea9d923aeb59d
Lucassauro <lucassauro.lt@gmail.com>
2021-06-07 18:38:17 -0300

    Adiciona requisitos obrigatorios completos

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
-window.onload = function () {
-  document.getElementById('texto-tarefa').focus();
+const lista = document.getElementById('lista-tarefas');
+const items = document.getElementsByClassName('item');
 
-  document.getElementById("criar-tarefa").onclick = function cria() {
-    console.log("click");
-    let text = document.getElementById('texto-tarefa').value;
-    let lista = document.getElementById('lista-tarefas');
-    let li = document.createElement("li");
-    let input = document.createTextNode(text);
-    li.appendChild(input)
-    lista.appendChild(li)
-    document.getElementById('texto-tarefa').value = null;
+document.getElementById("criar-tarefa").onclick = function cria() {
+  let text = document.getElementById('texto-tarefa').value;
+  let li = document.createElement("li");
+  let input = document.createTextNode(text);
   
+  li.appendChild(input)
+  lista.appendChild(li)
+
+  li.classList.add('item');
+
+  document.getElementById('texto-tarefa').value = null;
+
+  selectItem();
+
+}
+
+function unSelectItems() {
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].classList.remove('selected');
   }
 }
+
+function selectItem() {
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].addEventListener('click', function() {
+      unSelectItems();
+      items[i].classList.add('selected');
+    });
+  }
+}
+
+
+function completa(evento){
+  const completou = evento.target;
+  if (completou.classList.contains('item')){
+    if (completou.classList.contains('completed')){
+      completou.classList.remove('completed');
+    } else {
+      completou.classList.add('completed');
+    }
+  }
+
+}
+
+
+
+
+window.onload = function () {
+
+  lista.addEventListener('dblclick', completa);
+
+  document.getElementById('texto-tarefa').focus();
+
+}
+
+
+
