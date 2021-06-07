98601bcb8c97be758de3c7010b533c30a0ebdcdd
rafa-araujo <rafacarlosaraujo@gmail.com>
2021-06-07 19:40:46 -0300

    Requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const input = document.getElementById("texto-tarefa");
+const button = document.getElementById("criar-tarefa");
+const lista = document.getElementById("lista-tarefas");
+
+//Adicionando uma li 
 function criarTarefa() {
-  document.getElementById("texto-tarefa")
-}
+  const element = input.value;
+  const li = document.createElement('li');
+  li.innerText = element;
+  lista.appendChild(li);
+  input.value = null;
+};
+button.addEventListener("click", criarTarefa);
