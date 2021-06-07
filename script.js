d5fd4843ebac78fabf1128fea1fbd10992e0add5
Ramon Vitor <vitorramon@hotmail.com>
2021-06-07 17:29:50 -0300

    Questão 7

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btnAdd = document.getElementById('criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
-const inputText = document.getElementById('texto-tarefa')
+const inputText = document.getElementById('texto-tarefa');
 
 btnAdd.addEventListener('click', function(){
   const itemList = document.createElement('li');
@@ -8,11 +8,22 @@ btnAdd.addEventListener('click', function(){
   lista.appendChild(itemList);
   inputText.value = "";
 
+  // Evento para pintar o fundo do texto:
   itemList.addEventListener('click', function(){
     itemList.style.backgroundColor = 'rgb(128,128,128)';
+    
   })
 
+  // Evento para riscar o texto:
   itemList.addEventListener('dblclick', function(){
-    itemList.style.textDecoration = "line-through";
+    if (itemList.style.textDecoration = 'none') {
+      itemList.style.textDecoration = 'line-through';
+    }
+    // Evento para desriscar o texto:
+    itemList.addEventListener('dblclick', function(){
+      if (itemList.style.textDecoration = 'line-through') {
+      itemList.style.textDecoration = 'none';
+     }
+    })  
   })
 })
