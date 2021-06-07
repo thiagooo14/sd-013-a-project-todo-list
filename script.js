cd0d97830d25522f7fc0500c69d07018839bdc18
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-06-07 10:03:03 -0300

    Conclusao dos requisitos 7,8 e 9. Inicio do requisito 10

--- script.js
+++ script.js
@@ -3,29 +3,55 @@ button.id = 'criar-tarefa';
 posicaoBotao1 = document.querySelector('#section-text');
 button.innerHTML = "Adicionar";
 posicaoBotao1.appendChild(button);
-let tarefas = document.querySelectorAll('.tasks');
 
-function adicionarLista(){    
+let button2 = document.createElement('button');
+button2.id = 'apaga-tudo';
+posicaoBotao2 = document.querySelector('#section-text');
+button2.innerHTML = "Apagar todas as tarefas";
+posicaoBotao2.appendChild(button2);
+
+function adicionarLista() {    
     let listaTarefa = document.getElementById('lista-tarefas');
-    button.addEventListener('click', function(){
+    button.addEventListener('click', function() {
         let texto = document.getElementById('texto-tarefa').value;
-        let lista = document.createElement('li');
-        lista.classList.add('tasks');
+        var lista = document.createElement('li');
         listaTarefa.appendChild(lista);
         lista.innerHTML = texto;
-        document.getElementById('texto-tarefa').value = '';      
+        document.getElementById('texto-tarefa').value = '';     
     })
 }
-console.log(tarefas);
 
-function liFundo(){
-    for(let index = 0; index < tarefas.length; index += 1){
-        tarefas[index].addEventListener('click', function(){
-            tarefas[index].style.backgroundColor = 'rgb(128, 128, 128)';
-        })
+let listaTarefa = document.querySelector('#lista-tarefas');
+
+function corDeFundo(event) {
+  let elementoClicado = event.target;
+  var itensTarefa;
+  elementoClicado.classList.add('listaClicado');
+  itensTarefa = document.getElementsByClassName('listaClicado');
+    for (let index = 0; index < itensTarefa.length; index += 1) {
+          itensTarefa[index].classList.remove('corCinza');
+          itensTarefa[index].classList.add('corBranca');
+       }
+  let elementoClicado2 = event.target;
+   elementoClicado2.classList.remove('corBranca');
+   elementoClicado2.classList.add('corCinza');
+}
+
+function linhaRiscada(event) {
+    let elementoClicado = event.target;
+    var riscadoTarefas;
+    elementoClicado.classList.add('listaClicado');
+       if (elementoClicado.classList.contains('completed')) {
+           elementoClicado.classList.remove('completed');
+    } else {
+        elementoClicado.classList.add('completed');
     }
 }
 
-
+function apagarTodasTarefas() {
+    
+}
+   
+listaTarefa.addEventListener('click', corDeFundo);
+listaTarefa.addEventListener('dblclick', linhaRiscada);
 adicionarLista();
-liFundo();
