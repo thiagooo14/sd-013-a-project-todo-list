9a726d188607198ae7f401f746f3e606b97b0afe
Breno Lopes <brenokos4@gmail.com>
2021-06-07 19:28:58 -0300

    adiciona 80% das competencias ;).

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-// botão criar tarefa e campo de texto.
+// atributos.
 const lista = document.getElementById('lista-tarefas');
 const inputTarefa = document.getElementById('texto-tarefa');
-const botao = document.getElementById('criar-tarefa');
+const botaoCria = document.getElementById('criar-tarefa');
+const apagaTudo = document.getElementById('apaga-tudo');
+const marcacao = document.getElementsByClassName('completed');
+const removeComp = document.getElementById('remover-finalizados');
+const itens = document.querySelectorAll('.item');
+// adiciona itens
 
-botao.addEventListener('click', () => {
+botaoCria.addEventListener('click', () => {
   const item = document.createElement('li');
   item.innerText = inputTarefa.value;
   lista.appendChild(item);
-  console.log('deu certo');
   inputTarefa.value = '';
+  item.className = 'item';
+  // adiciona evento de click já na criação da Li.
+  item.addEventListener('click', () => {
+    item.className = 'item completed';
+    item.add
+    item.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
+  item.addEventListener('dblclick', () => {
+    item.className = 'item';
+  });
 });
 
-// botão de apagar!!!!!!!!!11111111111111111111111
+// apagar tudo.
+apagaTudo.addEventListener('click', () => {
+  while (lista.firstChild) {
+    lista.removeChild(lista.firstChild);
+  }
+});
+
+// remover itens finalizados.
+
+removeComp.addEventListener('click', () => {
+  for (let i = 0; i <= marcacao.length; i += 1) {
+    marcacao[i].remove();
+  }
+});
