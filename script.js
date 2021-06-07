aa358678a5d1a00242527597cb97d485fbc2dd8b
ivandosss <ivanndosssilva9389@gmail.com>
2021-06-07 20:22:22 -0300

    modificaçoes

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
-const enterBotao = document.getElementById('criar-tarefa');
-const input = document.getElementById('texto-tarefa');
+const botao = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
-let item = document.getElementsByTagName('li');
+const input = document.getElementById('texto-tarefa');
 
-function inputList() {
-  return input.value.length;
-}
-
-function criarlist() {
-  let li = document.createElement('li');
-
-  li.appendChild(document.createTextNode(input.value));
-  ol.appendChild(li);
+botao.addEventListener('click', () => {
+  let elementList = document.createElement('li');
+  ol.appendChild(elementList);
+  elementList.innerText = input.value;
   input.value = '';
+  elementList.addEventListener('click', (e) => {
+    for (let filho of ol.children){
+      filho.style.backgroundColor = ''; 
+    }
+    e.target.style.backgroundColor = 'rgb(128, 128, 128)'; 
+   
+  }); 
+});
 
-  function criarHover() {
-    li.classList.toggle('listHover');
-  }
 
-  li.addEventListener('click', criarHover);
-
-  let dbtn = document.createElement('button');
-  dbtn.appendChild(document.createTextNode('Apagar'));
-  li.appendChild(dbtn);
-  dbtn.addEventListener('click', deletListItem);
-
-  function deletListItem() {
-    li.classList.add('delete');
-  }
-}
-
-enterBotao.addEventListener('click', addListAfterClick);
-
-function addListAfterClick() {
-  if (inputList() > 0) {
-    criarlist();
-  }
-}
