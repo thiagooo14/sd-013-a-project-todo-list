c727c301a5864c6983f075c3135d552c77333fac
Ramon Vitor <vitorramon@hotmail.com>
2021-06-07 16:56:07 -0300

    Questão 7

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const lista = document.getElementById('lista-tarefas');
 const inputText = document.getElementById('texto-tarefa')
 
 btnAdd.addEventListener('click', function(){
-  const addList = document.createElement('li');
-  addList.innerText = inputText.value;
-  lista.appendChild(addList);
+  const itemList = document.createElement('li');
+  itemList.innerText = inputText.value;
+  lista.appendChild(itemList);
   inputText.value = "";
+
+  itemList.addEventListener('click', function(){
+    itemList.style.backgroundColor = 'rgb(128,128,128)';
+  })
+
+  itemList.addEventListener('dblclick', function(){
+    itemList.style.textDecoration = "line-through";
+  })
 })
