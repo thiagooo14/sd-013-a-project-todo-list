764b5d6ae97d25c83684c0b7f9f4949ff9621b24
Adriana Barreto <adri92barreto123@gmail.com>
2021-06-07 17:44:23 -0300

    Executa segunda requesito e altera o primeiro

---
+++ script.js
@@ -0,0 +1,21 @@
+// QUESTÃO 1
+function adicionaHeader () {
+  let novaHeader = document.createElement('header');
+  let novoH1 = document.createElement('h1');
+  novoH1.innerText = "Minha Lista de Tarefas"
+  novaHeader.appendChild(novoH1);
+  document.body.appendChild(novaHeader);
+}
+adicionaHeader()
+
+// QUESTÃO 2
+function adicionaP () {
+  let novaP = document.createElement('p');
+  novaP.innerText = "Clique duas vezes em um item para marcá-lo como completo"
+  novaP.setAttribute("id", "funcionamento");
+  let elementoPai = document.querySelector("header");
+  elementoPai.appendChild(novaP);
+}
+adicionaP()
+
+//QUESTÃO 3
