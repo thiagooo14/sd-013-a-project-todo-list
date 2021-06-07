7bb910458ed2db32f4a8d4c905b3c571ead9aaee
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-06-07 18:05:49 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 function addNewTask() {
-  let getInputField = document.querySelector('#texto-tarefa');
-  let addInputButton = document.querySelector('#criar-tarefa');
-  let getTaskList = document.querySelector('#lista-tarefas');
-      
+  const getInputField = document.querySelector('#texto-tarefa');
+  const addInputButton = document.querySelector('#criar-tarefa');
+  const getTaskList = document.querySelector('#lista-tarefas');
+
   addInputButton.addEventListener('click', function() {
     if (getInputField.value.length > 0) {
       let newLi = document.createElement('li');
       newLi.innerText = getInputField.value;
+      newLi.setAttribute("class", "li");
+      newLi.onclick = addColor;
+      newLi.ondblclick = taskCompleted;
       getTaskList.appendChild(newLi);
       getInputField.value = '';
     } else {
-       alert('Error: Digite ao menos 1 caractere.')
+        alert('Error: Digite ao menos 1 caractere.');
     }
-  })
+  });
 }
 
-window.onload=function(){
-    addNewTask();   
+function addColor(event) {
+  const getList = document.getElementsByClassName('li');
+  for (let index = 0; index < getList.length; index += 1) {
+    getList[index].classList.remove("selected");   
+  }
+  event.target.classList.add('selected');
 }
+
+function taskCompleted(event) { 
+  if (event.target.classList.contains('completed')){
+    event.target.classList.remove('completed');
+    console.log('erro')
+  }
+  else {
+  event.target.classList.add('completed');
+  console.log('erro')
+  }
+}
+
+window.onload = function() {
+  addNewTask();  
+}    
 
