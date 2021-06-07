197b13fb928fcd9b318cc148dcfd1bebd8a6698c
Cassyel <cassyel1967@gmail.com>
2021-06-07 16:52:52 -0300

    cumpre requisitos 7, 9. E formatação Lint, CSS e JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttonAdd = document.getElementById('criar-tarefa');// Constante para resgatar o botão "Adicionar"
 const buttonDeleteAll = document.getElementById('apaga-tudo');
+const ol = document.getElementById('lista-tarefas');
 
 // Adiciona som de erro e placeholder quando o valor do input esta em branco e o usuário envia a tarefa
 function soundError() {
@@ -20,14 +21,11 @@ function button1() {
   const task = document.getElementById('texto-tarefa');
   const input = document.getElementById('texto-tarefa').value;
   const li = document.createElement('li');
-  const ol = document.querySelector('#lista-tarefas');
-
   if (input !== '') {
     li.innerHTML = input;
     ol.appendChild(li);
-    li.className = 'color';
+    ol.className = 'color';
     li.id = 'li';
-    ol.className = 'color color2';
     task.value = '';
     soundAccept();
     document.getElementsByTagName('input')[0].placeholder = 'DIGITE SUA TAREFA';
@@ -35,7 +33,6 @@ function button1() {
     soundError();
   }
 }
-buttonAdd.addEventListener('click', button1);
 
 //  Aciona botão 'Enviar" ao apertar Enter
 function enterKey(e) {
@@ -43,12 +40,30 @@ function enterKey(e) {
     buttonAdd.click();
   }
 }
-document.addEventListener('keypress', enterKey);
 
 // Remove todas as tarefas quando clicar no botão "Apagar Tudo"
 function deleteAll() {
-  const ol = document.getElementsByTagName('ol')[0];
   ol.innerHTML = '';
-  ol.classList.remove('color2');
+  ol.classList.remove('color');
 }
+
+// Altera cor de fundo da li ao clicar no item
+function addSelected(e) {
+  e.target.classList.add('selected');
+}
+
+// Altera o estilo do item ao clicar duas vezes
+function complete(e) {
+  if (e.target.classList.contains('completed') === false) {
+    e.target.classList.add('completed');
+  } else if (e.target.classList.contains('completed')) {
+    e.target.classList.remove('completed');
+  }
+}
+
 buttonDeleteAll.addEventListener('click', deleteAll);
+buttonAdd.addEventListener('click', button1);
+document.addEventListener('keypress', enterKey);
+ol.addEventListener('click', addSelected);
+ol.addEventListener('dblclick', complete);
+ol.addEventListener('dblclick', complete);
