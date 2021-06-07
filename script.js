430ebe9fd36b73553c5a7c72c29fdf3364743a43
Wesley Maia <maia.weol@gmail.com>
2021-06-06 21:17:45 -0300

    Requisito 8- Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-//================================================================
+// ================================================================
 const clickB = document.getElementById('criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
 const input = document.getElementById('texto-tarefa');
 
 function tarefa(evento) {
   const res = document.getElementById('texto-tarefa').value;
-  listItem = document.createElement('li')
+  const listItem = document.createElement('li');
   listItem.innerText = res;
-  lista.appendChild(listItem)
+  lista.appendChild(listItem);
   input.value = '';
 }
-
 clickB.addEventListener('click', tarefa);
 
-//================================================================
+// ================================================================
 function selectLi(evento) {
-  console.log('ok')
-  evento.target.style.backgroundColor = 'rgb(128, 128, 128)'
-} 
+  //console.log('ok');
+  const listGray = document.getElementsByClassName('gray');
 
+  if (listGray.length > 0) {
+    for (let index = 0; index < listGray.length; index += 1){
+      listGray[index].classList.remove('gray');
+    }
+    evento.target.classList.add('gray');
+  } else {
+    evento.target.classList.add('gray');
+  }
+  lista.style.backgroundColor = 'white';
+}
 lista.addEventListener('click', selectLi);
+
+// ================================================================
+
+//console.log(document.body)
