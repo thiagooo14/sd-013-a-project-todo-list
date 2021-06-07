750b1c2cf534cbd6455b6e7e2f07ee8f6c20a658
thomasravache <thomasravache31@gmail.com>
2021-06-07 17:26:45 -0300

    Requisito 09 --> Finalizado --> Clicar duas vezes em um item, faz com que ele seja riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const toDoList = document.querySelector('#lista-tarefas');
 const btnCreateItem = document.querySelector('#criar-tarefa');
 const textTask = document.querySelector('#texto-tarefa');
 
+function clearLastSelected() {
+  for (let index = 0; index < toDoList.childElementCount; index += 1) {
+    toDoList.children[index].classList.remove('selected');
+  }
+}
+
+//Botão que quando clicado insere novos itens
 btnCreateItem.addEventListener('click', function() {
   console.log('botão de adicionar item foi clicado.');
   let item = document.createElement('li');
@@ -12,19 +19,23 @@ btnCreateItem.addEventListener('click', function() {
   textTask.focus();
 });
 
-function clearSelectedItem() {
-  for (let index = 0; index < toDoList.childElementCount; index += 1) {
-    toDoList.children[index].className = 'list-item';
-    toDoList.children[index].style.backgroundColor = 'white';
-  }
-}
-
+//Listener que seleciona o item da lista
 document.addEventListener('click', function(event) {
   if (event.target.classList.contains('list-item')) {
-    clearSelectedItem();
     console.log('item da lista foi clicado.');
-    event.target.className = 'list-item selected';
-    event.target.style.backgroundColor = 'rgb(128,128,128)';
+    clearLastSelected();
+    event.target.classList.add('selected');
+  }
+});
+
+//Listener que ao ter 2 cliques marca como completa a tarefa
+document.addEventListener('dblclick', function(event) {
+  if(event.target.classList.contains('list-item')) {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
   }
 });
 
