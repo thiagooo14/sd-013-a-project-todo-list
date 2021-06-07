03f4179767ee23c1af2e9aa601465ed8abf24a2d
Micael Santos <micaelmaicon@hotmail.com>
2021-06-07 11:51:00 -0300

    Finish requirements: 5, 6

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 // Iniciando o projeto 
-document. querySelector('main');
+const getOl = document.getElementById('lista-tarefas');
+const getInputTask = document.getElementById('texto-tarefa');
+const btnCreateTask = document.getElementById('criar-tarefa');
+
+// Limpar inputs
+function limparInput(value) {
+  value.value = '';
+}
+
+// Cria as tasks como lis.
+function createTasks(valueText) {
+  const createLi = document.createElement('li');
+  createLi.innerHTML = valueText;
+  getOl.appendChild(createLi);
+}
+
+// Pegar tarefa
+function getTask() {
+  const receiveValue = getInputTask.value;
+  console.log(getInputTask.value);
+  createTasks(receiveValue);
+  limparInput(getInputTask);
+}
+
+// Evento que ao clicar `criar tarefa` chama função pegar texto, inserir na ol
+btnCreateTask.addEventListener('click', getTask);
