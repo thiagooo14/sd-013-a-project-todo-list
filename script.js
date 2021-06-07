1112d3a0ca45e66979f3fd4ac3d06ef5bfac9ed5
Emídio Rebouças <emidioreb@gmail.com>
2021-06-06 21:57:57 -0300

    requirement 7 and 8 done

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-let button = document.querySelector('#criar-tarefa')
+let button = document.querySelector('#criar-tarefa');
 button.addEventListener("click",addText);
 
 function addText() {
   let textTarefa = document.getElementById('texto-tarefa').value;
-  let lista  = document.getElementById('lista-tarefas').innerHTML;
-  lista = lista +"<li>"+textTarefa+"</li>";
+  let list  = document.getElementById('lista-tarefas').innerHTML;
+  list = list +"<li>"+textTarefa+"</li>";
   
   
-  document.getElementById("lista-tarefas").innerHTML = lista;
-  document.getElementById('texto-tarefa').value = ''
+  document.getElementById("lista-tarefas").innerHTML = list;
+  document.getElementById('texto-tarefa').value = '';
 }
 
+// Desenvolvido com a ajuda do estudante Trybe Thiago Carboneri
+function selectedClass() {
+const getOl = document.querySelector('#lista-tarefas');
+  getOl.addEventListener('click', addColor);
+  function addColor(event){
+    const getLi = document.getElementsByTagName('li');
+    for (let index = 0; index < getLi.length; index += 1) {
+      getLi[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  };
+}
+selectedClass();
