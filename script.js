74e8407d95af4de2100dc7d68b075cf2c70ab935
turtore <brunosap@gmail.com>
2021-06-07 15:17:59 -0300

    atualizado até exercicio 11

--- script.js
+++ script.js
@@ -2,48 +2,50 @@ let botaoApaga = document.querySelector("#apaga-tudo");
 
 let botaoCriar = document.querySelector("#criar-tarefa");
 
+let botaoLimpaFinalizados = document.querySelector("#remover-finalizados");
+
 let listaOrdenada = document.querySelector("#lista-tarefas");
 
-
-//bloco do requerimento 7
-//bloco do requerimento 7
-// function carregaArrayLista() {
-//     let listaArray = document.querySelectorAll('li');
-//     for (let i = 0; i > listaArray.length ; i += 1 ) {
-//         listaArray[i].addEventListener("dblclick", riscaElemento);
-//         listaArray[i].addEventListener("click", mudaCorFundo);
-//     }
-//     return listaArray;
-// }
-
 function mudaCorFundo(event) {
-    //let temporario = event.target.className
-    //window.alert(temporario);
-    event.target.className = "selected";
+    let temporario = event.target;
+    document.querySelector('#selected').style.backgroundColor = 'white';
+    document.querySelector('#selected').removeAttribute('id');
+    temporario.style.backgroundColor = 'rgb(128, 128, 128)';
+    temporario.id = 'selected';
 }
 
-function riscaElemento() {
-    window.alert('riscaelemento');
+function riscaElemento(event) {
+    let temporario = event.target;
+    temporario.classList.toggle('riscado');
+}
+
+function removeFinalizados(){
+    let listaTemporaria = document.querySelectorAll('.riscado');
+    for (let i = 0; i < listaTemporaria.length; i += 1){
+        listaTemporaria[i].remove();
+    }
+    document.querySelector('.comum').id = 'selected'
 }
 
     //fim bloco requerimento 7
     //fim bloco requerimento 7
     
-    function addTarefa(){
-        let campoTexto = document.querySelector("#texto-tarefa").value;
-        let lugarNaLista = document.querySelector("#lista-tarefas");
-        let li = document.createElement('li');
-        li.innerHTML = campoTexto;
-        li.className = 'comum';
-        lugarNaLista.appendChild(li);
-        document.querySelector("#texto-tarefa").value = null;
-    }
+function addTarefa(){
+    let campoTexto = document.querySelector("#texto-tarefa").value;
+    let lugarNaLista = document.querySelector("#lista-tarefas");
+    let li = document.createElement('li');
+    li.innerHTML = campoTexto;
+    li.className = 'comum';
+    lugarNaLista.appendChild(li);
+    document.querySelector("#texto-tarefa").value = null;
+}
     
-    function apagaTudo() {
-        window.location.reload();
-    }
+function apagaTudo() {
+    window.location.reload();
+}
     
     botaoApaga.addEventListener("click",apagaTudo);
     botaoCriar.addEventListener("click", addTarefa);
-    // botaoCriar.addEventListener("click", carregaArrayLista)
+    botaoLimpaFinalizados.addEventListener("click", removeFinalizados)
     listaOrdenada.addEventListener("click", mudaCorFundo);
+    listaOrdenada.addEventListener("dblclick", riscaElemento);
