23658c7164aaa9686fce50b268f0a6becf58efac
Wesley Maia <maia.weol@gmail.com>
2021-06-07 18:02:49 -0300

    Requisito 11- Adicione um botão com id=remover-finalizados

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const clickB = document.getElementById('criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
 const input = document.getElementById('texto-tarefa');
 
-function tarefa(evento) {
+function tarefa() {
   const res = document.getElementById('texto-tarefa').value;
   const listItem = document.createElement('li');
   listItem.innerText = res;
@@ -14,7 +14,6 @@ clickB.addEventListener('click', tarefa);
 
 // ================================================================
 function selectLi(evento) {
-  //console.log('ok');
   const listGray = document.getElementsByClassName('gray');
 
   if (listGray.length > 0) {
@@ -31,7 +30,7 @@ lista.addEventListener('click', selectLi);
 
 // ================================================================
 function completo(evento) {
-  const listCompleted = document.getElementsByClassName('completed');
+  // const listCompleted = document.getElementsByClassName('completed');
   const teste = evento.target.classList.value;
 
   if (teste === 'completed gray') {
@@ -39,34 +38,48 @@ function completo(evento) {
   } else {
     evento.target.classList.add('completed');
   }
-  console.log(document.body)
 }
 lista.addEventListener('dblclick', completo);
 
 // ================================================================
-function createButton() {
+function createButtonClean() {
   const recuperaContainer = document.querySelector('.container');
   const cleanButton = document.createElement('button');
-  const buttonID = 'apaga-tudo';
   const listLi = document.getElementsByTagName('li');
+  const buttonID = 'apaga-tudo';
 
   cleanButton.innerHTML = 'Apaga';
   cleanButton.id = buttonID;
   recuperaContainer.appendChild(cleanButton);
 
-  cleanButton.addEventListener('click', function () {
-    //console.log(listLi.length);
-
+  cleanButton.addEventListener('click', function() {
     for (let index = listLi.length - 1; index > -1; index -= 1) {
       console.log(listLi);
-      //lista.removeChild(lista.childNodes[index]);
       lista.removeChild(listLi[index]);
     }
-
   });
-
-  //console.log(document.body)
 }
-createButton();
+createButtonClean();
 
-console.log(document.body)
+// ================================================================
+function createButtonRemove() {
+  const recuperaContainer = document.querySelector('.container');
+  const removeButton = document.createElement('button');
+  const listCompleted = document.getElementsByClassName('completed')
+  const buttonID = 'remover-finalizados';
+
+  removeButton.innerHTML = 'Remover';
+  removeButton.id = buttonID;
+  recuperaContainer.appendChild(removeButton);
+
+  removeButton.addEventListener('click', function() {
+    if(listCompleted.length > 0) {
+      for (let index = listCompleted.length -1; index > -1; index -= 1){
+        listCompleted[index].remove();
+      }
+    }
+  })
+}
+createButtonRemove();
+
+//console.log(document.body);
