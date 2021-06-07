345c89afc2ff5ca8b096e5223ab12ea92b7f1b7a
Guilherme Couto da Silva <gui.couto90@yahoo.com.br>
2021-06-07 18:04:41 -0300

    Conclusao dos requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+//Variaveis
+var listaTarefa = document.getElementById('lista-tarefas');
+var lis = document.getElementsByTagName('li');
+var completadas = document.getElementsByClassName('completed');
+
+//Botao 'Adicionar'
 let button = document.createElement('button');
 button.id = 'criar-tarefa';
 posicaoBotao1 = document.querySelector('#section-text');
 button.innerHTML = "Adicionar";
 posicaoBotao1.appendChild(button);
 
+//Botao 'Apagar todas as tarefas'
 let button2 = document.createElement('button');
 button2.id = 'apaga-tudo';
 posicaoBotao2 = document.querySelector('#section-text');
 button2.innerHTML = "Apagar todas as tarefas";
 posicaoBotao2.appendChild(button2);
 
+//Botao 'Remover tarefas finalizadas'
+let button3 = document.createElement('button');
+button3.id = 'remover-finalizados';
+posicaoBotao3 = document.querySelector('#section-text');
+button3.innerHTML = "Remover tarefas finalizadas";
+posicaoBotao3.appendChild(button3);
+
+// Funcao para adicionar tarefas
 function adicionarLista() {    
     let listaTarefa = document.getElementById('lista-tarefas');
     button.addEventListener('click', function() {
@@ -21,8 +36,7 @@ function adicionarLista() {
     })
 }
 
-let listaTarefa = document.querySelector('#lista-tarefas');
-
+// Funcao para adicionar cor de fundo cinza em um item clicado
 function corDeFundo(event) {
   let elementoClicado = event.target;
   var itensTarefa;
@@ -37,10 +51,9 @@ function corDeFundo(event) {
    elementoClicado2.classList.add('corCinza');
 }
 
+// Funcao para riscar tarefas completas
 function linhaRiscada(event) {
     let elementoClicado = event.target;
-    var riscadoTarefas;
-    elementoClicado.classList.add('listaClicado');
        if (elementoClicado.classList.contains('completed')) {
            elementoClicado.classList.remove('completed');
     } else {
@@ -48,10 +61,18 @@ function linhaRiscada(event) {
     }
 }
 
+// Funcao para apagar todas as tarefas
 function apagarTodasTarefas() {
-    
+    listaTarefa.textContent = ' ';
 }
-   
+
+//Funcao para apagar tarefas finalizadas
+function tarefasFinalizadas() {
+    listaTarefa.querySelectorAll('.completed').forEach(n => n.remove());
+}
+
 listaTarefa.addEventListener('click', corDeFundo);
 listaTarefa.addEventListener('dblclick', linhaRiscada);
 adicionarLista();
+button2.addEventListener('click', apagarTodasTarefas);
+button3.addEventListener('click', tarefasFinalizadas);
