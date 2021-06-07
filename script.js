5794fd2f13b9c819ddb45a5861115290abd45e7a
Emídio Rebouças <emidioreb@gmail.com>
2021-06-07 20:06:18 -0300

    requirement 11 done

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function addText() {
 }
 
 //Desenvolvido com a ajuda do estudante Trybe Felipe Neves;
+//Alterar cor de fundo
 const listDesk = document.querySelector('#lista-tarefas');
   listDesk.addEventListener('click', addColor);
   function addColor(event){
@@ -21,7 +22,7 @@ const listDesk = document.querySelector('#lista-tarefas');
     }
     event.target.classList.add('gray');
   };
-
+// Clicar duas vezes em um item, faz com que ele seja riscado, 
   listDesk.addEventListener('dblclick', addRisk);
   function addRisk(event){
     const item = document.getElementsByTagName('li');
@@ -37,3 +38,17 @@ while (listDesk.firstChild) {
 listDesk.removeChild(listDesk.firstChild);
 }
 }
+
+//Remover finalizados
+let buttonRemoveFin = document.getElementById('remover-finalizados');
+buttonRemoveFin.addEventListener('click',removeFinalizados);
+function removeFinalizados(){
+const risk = document.querySelectorAll(".completed");;
+const listDesk = document.getElementById('lista-tarefas');
+  for (let index = 0; index < risk.length; index += 1) {
+      listDesk.removeChild(risk[index]);
+    }
+  }
+
+
+
