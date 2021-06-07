8d9064abc95deba2705ca5ce3c5d3bc1fac02074
suco <francalucas937@gmail.com>
2021-06-07 18:08:44 -0300

    desafio 7/8 concluidos

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 function createOl() {
-	const paiOl = document.querySelector('#OrganizaOl');
-	const ordenedList = document.createElement('ol');
-	ordenedList.id = 'lista-tarefas';
-	paiOl.appendChild(ordenedList);
+  const paiOl = document.querySelector('#OrganizaOl');
+  const ordenedList = document.createElement('ol');
+  ordenedList.id = 'lista-tarefas';
+  paiOl.appendChild(ordenedList);
 }
 
 createOl();
 
 const paiLi = document.getElementsByTagName('ol')[0];
 function createOlItem() {
-	if (document.getElementById('texto-tarefa').value === '') {
-		alert('item inválido');
-	} else {
-		const input = document.getElementById('texto-tarefa').value;
-		const listItem = document.createElement('li');
-		listItem.innerHTML = input;
-		document.getElementById('texto-tarefa').value = '';
-		paiLi.appendChild(listItem);
-	}
+  const input1 = document.getElementById('texto-tarefa').value;
+  if (input1.value === '') {
+    alert('item inválido');
+  } else {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = input1;
+    document.getElementById('texto-tarefa').value = '';
+    paiLi.appendChild(listItem);
+  }
 }
 
 // onclick="document.getElementById('texto-tarefa').value = ''"
+function verifyLi() {
+  const getLis = document.getElementsByTagName('li');
+  for (let indice = 0; indice < getLis.length; indice += 1) {
+    getLis[indice].addEventListener('click', (e) => {
+      for (let i = 0; i < getLis.length; i += 1) {
+        getLis[i].classList = '';
+        e.target.classList = 'selected';
+      }
+    });
+  }
+}
 
 const button = document.getElementById('criar-tarefa');
 button.addEventListener('click', createOlItem);
-
-let getLis = document.getElementsByTagName('li');
-for (let i = 0; i < getLis.length; i += 1) {
-  getLis[i].addEventListener('click' , clickLi)
-}
+button.addEventListener('click', verifyLi);
