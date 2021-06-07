eedeb5b7044c215713343260c8ffaf4248090624
Luiz Casimiro <luizcasimi@gmail.com>
2021-06-07 15:30:57 -0300

    solve 1,2,3,4

---
+++ script.js
@@ -0,0 +1,43 @@
+// ADD TITLE -> DESAFIO 1
+
+// Cria uma variável 'titulo' para armazenar o texto 'Minha Lista de Tarefas':
+
+const titulo = document.querySelector('#titulo');
+titulo.innerHTML = 'Minha Lista de Tarefas';
+
+// -------------------------------------------------------
+
+// ADD FUNCIONAMENTO -> DESAFIO 2
+
+// Cria uma variável 'funcionamento' para armazenar o texto 'Clique duas vezes em um item para marcá-lo como completo':
+
+const funcionamento = document.querySelector('#funcionamento');
+funcionamento.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+
+// -------------------------------------------------------
+
+//ADD INPUT -> DESAFIO 3
+
+// Ver HTML (#texto-tarefa).
+
+// -------------------------------------------------------
+
+// ADD LISTA ORDENADA -> DESAFIO 4
+
+// Ver HTML (#lista-tarefas).
+
+// -------------------------------------------------------
+
+// ADD BUTTON -> DESAFIO 
+
+// Ver HTML (#criar-tarefa).
+
+
+
+
+// -------------------------------------------------------
+
+
+
+
+
