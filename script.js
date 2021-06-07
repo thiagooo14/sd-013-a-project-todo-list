b75d54e085a7b411f8376e8bb45bad77830a43a2
ivandosss <ivanndosssilva9389@gmail.com>
2021-06-07 05:23:32 -0300

    Commit Inicial

---
+++ script.js
@@ -0,0 +1,46 @@
+let enterBotao = document.getElementById("criar-tarefa");
+let input = document.getElementById("texto-tarefa");
+let ol = document.getElementById("lista-tarefa");
+let item = document.getElementsByTagName("li");
+
+function inputList () {
+  return input.value.length;
+}
+
+function criarlist () {
+  let li = document.createElement("li");
+
+  li.appendChild(document.createTextNode(input.value));
+  ol.appendChild(li);
+  input.value = "";
+
+  function criarHover () {
+    li.classList.toggle("listHover");
+  }
+
+  li.addEventListener("dblclick", criarHover);
+
+  let dbtn = document.createElement("button");
+  dbtn.appendChild(document.createTextNode("Apagar"));
+  li.appendChild(dbtn);
+  dbtn.addEventListener("click", deletListItem);
+
+  function deletListItem () {
+    li.classList.add("delete");
+  }
+}
+
+enterBotao.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+
+function addListAfterClick () {
+  if (inputList() > 0 ) {
+    criarlist();
+  }
+}
+
+function addListAfterKeypress () {
+  if (inputList() > 0 && event.which === 13) {
+    criarlist();
+  }
+}
