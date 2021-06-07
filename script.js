a257a411472661ce7996577f901e29d5a4f1367e
Gustavo Pia <piagja@gmail.com>
2021-06-07 15:52:57 -0300

    Atendeu ao requisito 10

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+/* eslint-disable quotes */
 /* eslint-disable no-use-before-define */
 window.onload = function load() {
   createTask();
 };
 
-const buttonCreateTask = document.querySelector('#criar-tarefa');
-const inputCreateTask = document.querySelector('#texto-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 
 const createTask = () => {
+  const buttonCreateTask = document.querySelector('#criar-tarefa');
+  const inputCreateTask = document.querySelector('#texto-tarefa');
   buttonCreateTask.addEventListener('click', () => {
     const inputValue = inputCreateTask.value;
     if (inputValue === '' || inputValue === null || inputValue === undefined) {
@@ -19,34 +20,46 @@ const createTask = () => {
 
     highlightTask();
     completedTask();
+    eraseTasks();
   });
 };
 
 const highlightTask = () => {
-  const lis = document.querySelectorAll('.task-item');
-  const ol = document.querySelector('#lista-tarefas');
+  const lis = document.querySelectorAll(".task-item");
+  const ol = document.querySelector("#lista-tarefas");
   const gray = "rgba(128, 128, 128, 1)";
 
   ol.addEventListener("click", (event) => {
-    lis.forEach(li => {
+    lis.forEach((item) => {
+      const li = item;
       li.style.backgroundColor = '';
     });
-
-    event.target.style.backgroundColor = gray;
+    const e = event.target;
+    e.style.backgroundColor = gray;
   });
-}
+};
 
 const completedTask = () => {
   const lis = document.querySelectorAll('.task-item');
-  const ol = document.querySelector('#lista-tarefas');
 
-  lis.forEach(item => {
+  lis.forEach((item) => {
     item.addEventListener("dblclick", (event) => {
       if (!event.target.classList.contains("completed")) {
-        event.target.classList.add("completed")
+        event.target.classList.add("completed");
       } else {
-        event.target.classList.remove("completed")
+        event.target.classList.remove("completed");
       }
-    })
-  })
-}
+    });
+  });
+};
+
+const eraseTasks = () => {
+  const button = document.querySelector("#apaga-tudo");
+  const lis = document.querySelectorAll(".task-item");
+
+  button.addEventListener("click", () => {
+    lis.forEach((item) => {
+      item.remove();
+    });
+  });
+};
