a7fd8e434aceffed7e1251b5249558475d189f8d
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2021-06-07 17:06:47 -0300

    progredindo no todolist

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+function riscar (e){
+    
+}
 
 
-window.onload
+
+
+
 
 
 function adicionar (){
@@ -8,6 +13,21 @@ function adicionar (){
     let botao = document.querySelector('#criar-tarefa');
     let adicionaItemL = document.querySelector('#lista-tarefas');
     let item = document.createElement('li');
+    item.style.border = '1px solid black';
+    item.classList.add('none');
+    item.addEventListener('click',function(e){
+      let todosItem = document.querySelectorAll('li');
+      for (let index = 0; index < todosItem.length; index +=1){
+          todosItem[index].classList.remove('selectedClass');
+          e.target.classList.add('selectedClass');
+      }
+       item.addEventListener('dblclick',function(event){
+          
+            
+            e.target.classList.toggle('completed');
+           
+       }) 
+    });
     if (valorInput !== ""){
             adicionaItemL.appendChild(item).innerHTML = valorInput;
             document.querySelector('input').value = null;
@@ -19,23 +39,21 @@ function adicionar (){
 }
 adicionar();
 
-function itemSelecionado(e) {
-    e.target.style.backgroundColor = 'gray';
-        
+
+      
+
     
-    
+      
+     
 
 
-  
-    
-}
-
-let lis = document.querySelectorAll('li');
-    for (let index = 0; index < lis.length; index++) {
-        lis[index].addEventListener('click', itemSelecionado);
-        
-    }
-    
-
-
-
+      function tarefaFinalizada() {
+        const lis = document.querySelectorAll('li');
+        for (let index = 0; index < lis.length; index++) {
+            lis[index].addEventListener('dbclick', function(event){
+                event.target.classList.add('completed');
+            })
+            
+        }
+      }
+      tarefaFinalizada();
