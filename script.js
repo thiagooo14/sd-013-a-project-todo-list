cef3117f9ad557e25260eaa33e6c4d137175e375
Isabella Antunes <bella.antunes2@gmail.com>
2021-06-07 19:29:55 -0300

    Resolução dos exercícios 7 e 8

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function addingNewTask() {
 }
 
 addingNewTask();
+
+function addClass(event) {
+  const getList = document.getElementsByTagName('li');
+  for (let index = 0; index < getList.length; index += 1) {
+    getList[index].classList.remove('selected');
+  }
+  const evento = event.target;
+  evento.classList.add('selected');
+}
+
+function addingBackground() {
+  const getOl = document.querySelector('#lista-tarefas');
+  getOl.addEventListener('click', addClass);
+}
+
+addingBackground();
