ab2c95e5f20ee1b4ee79703b7f465c8f37fef183
thomasravache <thomasravache31@gmail.com>
2021-06-07 16:13:19 -0300

    Requisito 07 Finalizado --> Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ btnCreateItem.addEventListener('click', function() {
   console.log('botão de adicionar item foi clicado.');
   let item = document.createElement('li');
   let createdItem = toDoList.appendChild(item);
+  createdItem.className = 'list-item';
   createdItem.innerHTML = textTask.value;
   textTask.value = '';
   textTask.focus();
+});
+
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('list-item')) {
+    console.log('item da lista foi clicado.');
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+  }
 });
 
 window.onload = function() {
