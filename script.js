0e78e49e3b948edddd999ae88e8f29c95c3c870b
thomasravache <thomasravache31@gmail.com>
2021-06-07 16:36:43 -0300

    Requisito 08 --> Finalizado --> Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo

--- script.js
+++ script.js
@@ -12,9 +12,18 @@ btnCreateItem.addEventListener('click', function() {
   textTask.focus();
 });
 
+function clearSelectedItem() {
+  for (let index = 0; index < toDoList.childElementCount; index += 1) {
+    toDoList.children[index].className = 'list-item';
+    toDoList.children[index].style.backgroundColor = 'white';
+  }
+}
+
 document.addEventListener('click', function(event) {
   if (event.target.classList.contains('list-item')) {
+    clearSelectedItem();
     console.log('item da lista foi clicado.');
+    event.target.className = 'list-item selected';
     event.target.style.backgroundColor = 'rgb(128,128,128)';
   }
 });
