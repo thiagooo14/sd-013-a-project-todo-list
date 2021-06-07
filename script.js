9c62c1a8d6a082986590f37056939a5d1f4ac9ed
Wesley Maia <maia.weol@gmail.com>
2021-06-07 09:52:28 -0300

    Requisito 10- Adicione um botão com id=apaga-tudo que quando clicado deve apagar todos os itens da lista

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function selectLi(evento) {
   const listGray = document.getElementsByClassName('gray');
 
   if (listGray.length > 0) {
-    for(let index = 0; index < listGray.length; index += 1) {
+    for (let index = 0; index < listGray.length; index += 1) {
       listGray[index].classList.remove('gray');
     }
     evento.target.classList.add('gray');
@@ -33,16 +33,40 @@ lista.addEventListener('click', selectLi);
 function completo(evento) {
   const listCompleted = document.getElementsByClassName('completed');
   const teste = evento.target.classList.value;
-  
+
   if (teste === 'completed gray') {
     evento.target.classList.remove('completed');
   } else {
     evento.target.classList.add('completed');
   }
-  //console.log(document.body)
+  console.log(document.body)
 }
 lista.addEventListener('dblclick', completo);
 
+// ================================================================
+function createButton() {
+  const recuperaContainer = document.querySelector('.container');
+  const cleanButton = document.createElement('button');
+  const buttonID = 'apaga-tudo';
+  const listLi = document.getElementsByTagName('li');
 
+  cleanButton.innerHTML = 'Apaga';
+  cleanButton.id = buttonID;
+  recuperaContainer.appendChild(cleanButton);
 
-// console.log(document.body)
+  cleanButton.addEventListener('click', function () {
+    //console.log(listLi.length);
+
+    for (let index = listLi.length - 1; index > -1; index -= 1) {
+      console.log(listLi);
+      //lista.removeChild(lista.childNodes[index]);
+      lista.removeChild(listLi[index]);
+    }
+
+  });
+
+  //console.log(document.body)
+}
+createButton();
+
+console.log(document.body)
