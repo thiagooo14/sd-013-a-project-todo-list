d011c511e3b1db0cff1af19eb9c94ab804189e59
Cassyel <cassyel1967@gmail.com>
2021-06-07 10:24:31 -0300

    Adiciona estilizações para a página e Formatação Lint no CSS e JavaScript

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
-const snd = new Audio('/assets/Enviar.mp3'); // Constante para adicionar Audio ao botão 'Adicionar'
-const paiLi = document.getElementById('lista-tarefas'); // Constante para resgatar elemento pai da 'li'
 const buttonAdd = document.getElementById('criar-tarefa');// Constante para resgatar o botão "Adicionar"
 const buttonDeleteAll = document.getElementById('apaga-tudo');
 
-//  Cria a LI com o valor digitado pelo usuário
-buttonAdd.addEventListener('click', () => {
+// Adiciona som de erro e placeholder quando o valor do input esta em branco e o usuário envia a tarefa
+function soundError() {
+  const snd2 = new Audio('/assets/input2.mp3');
+  snd2.play();
+  const input = document.getElementsByTagName('input')[0];
+  input.placeholder = ('TAREFA EM BRANCO, POR FAVOR ESCREVA SUA TAREFA AQUI!');
+}
+
+// Adiciona som quando o usuário envia a tarefa
+function soundAccept() {
+  const snd = new Audio('/assets/input.mp3');
+  snd.play();
+}
+
+// Cria a tarefa com o valor digitado pelo usuário
+function button1() {
   const task = document.getElementById('texto-tarefa');
   const input = document.getElementById('texto-tarefa').value;
   const li = document.createElement('li');
   const ol = document.querySelector('#lista-tarefas');
+
   if (input !== '') {
     li.innerHTML = input;
     ol.appendChild(li);
     li.className = 'color';
-    ol.className = 'color2 color';
+    li.id = 'li';
+    ol.className = 'color color2';
     task.value = '';
+    soundAccept();
+    document.getElementsByTagName('input')[0].placeholder = 'DIGITE SUA TAREFA';
+  } else {
+    soundError();
   }
-});
+}
+buttonAdd.addEventListener('click', button1);
 
 //  Aciona botão 'Enviar" ao apertar Enter
-document.addEventListener('keypress', function (e) {
+function enterKey(e) {
   if (e.key === 'Enter') {
     buttonAdd.click();
   }
-});
+}
+document.addEventListener('keypress', enterKey);
+
+// Remove todas as tarefas quando clicar no botão "Apagar Tudo"
+function deleteAll() {
+  const ol = document.getElementsByTagName('ol')[0];
+  ol.innerHTML = '';
+  ol.classList.remove('color2');
+}
+buttonDeleteAll.addEventListener('click', deleteAll);
