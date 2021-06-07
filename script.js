d432785e06d90b8e7ebdce88e0855b9fd3e83d23
Seu nomecd <canaldogildosantos@gmail.com>
2021-06-07 01:39:35 -0300

    resole exercicios 2,3,4,5

--- script.js
+++ script.js
@@ -15,3 +15,42 @@ function desafio2() {
     header.appendChild(p)
 }
 desafio2()
+
+function desafio3() {
+    let input = document.createElement("input");
+    input.setAttribute("id", "texto-tarefa");
+    input.setAttribute("type", "input");  // chave, valor
+    let div = document.createElement("div");
+    div.appendChild(input);
+    let body = document.querySelector("body");
+    body.appendChild(div);
+}
+desafio3()
+
+function desafio4(){
+    let ol = document.createElement("ol");
+    ol.setAttribute("id", "lista-tarefas");
+    let body = document.querySelector("body");
+    body.appendChild(ol);
+}
+desafio4()
+
+function desafio5() {
+    let button = document.createElement("button");
+    button.setAttribute("id", "criar-tarefa");
+    button.innerHTML = "Adicionar"
+    let div = document.querySelector("div")
+    let textoTarefa = document.querySelector("#texto-tarefa")
+     div.appendChild(button);
+     let ol = document.querySelector("#lista-tarefas")
+
+     button.addEventListener("click", (() => {
+     let texto = textoTarefa.value    // value = o valor que é adicionado no input.
+     let li = document.createElement("li") // sincrono, não precisa do if. mas poderia ter o if
+     li.innerText = texto
+     ol.appendChild(li)      
+     textoTarefa.value = "";
+     
+     }))
+}
+desafio5()
