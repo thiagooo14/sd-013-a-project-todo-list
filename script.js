c1fd47f75b323a1f9ad1be2f0083811804550f47
Gustavo Pia <piagja@gmail.com>
2021-06-08 09:54:26 -0300

    Refatorou o codigo

--- script.js
+++ script.js
@@ -1,44 +1,38 @@
 /* eslint-disable quotes */
 /* eslint-disable no-use-before-define */
-window.onload = function load() {
-  createTask();
-  saveTasks();
-};
-
 const taskList = document.querySelector('#lista-tarefas');
+const taskItem = ".task-item";
 
 const createTask = () => {
   const buttonCreateTask = document.querySelector('#criar-tarefa');
   const inputCreateTask = document.querySelector('#texto-tarefa');
   buttonCreateTask.addEventListener('click', () => {
     const inputValue = inputCreateTask.value;
-    if (inputValue === '' || inputValue === null || inputValue === undefined) {
+    if (!inputValue) {
       console.log('Valor inválido');
     } else {
       taskList.innerHTML += `<li class="task-item">${inputValue}</li>`;
       inputCreateTask.value = '';
+      inputCreateTask.focus();
     }
 
-    eraseTasks();
     completedTask();
-    highlightTask();
-    eraseSelected();
   });
 };
 
 const highlightTask = () => {
-  const lis = document.querySelectorAll(".task-item");
   const ol = document.querySelector("#lista-tarefas");
-  const gray = "rgba(128, 128, 128, 1)";
+  const gray = "gray-bgcolor";
 
   ol.addEventListener("click", (event) => {
+    const lis = document.querySelectorAll(taskItem);
     lis.forEach((item) => {
       const li = item;
-      li.style.backgroundColor = "";
+      li.classList.remove(gray);
     });
 
     const e = event.target;
-    e.style.backgroundColor = gray;
+    e.classList.add(gray);
   });
 };
 
@@ -58,20 +52,20 @@ const completedTask = () => {
 
 const eraseTasks = () => {
   const button = document.querySelector("#apaga-tudo");
-  const lis = document.querySelectorAll(".task-item");
 
   button.addEventListener("click", () => {
+    const lis = document.querySelectorAll(taskItem);
     lis.forEach((item) => {
       item.remove();
     });
   });
 };
 
-const eraseSelected = () => {
-  const lis = document.querySelectorAll(".task-item");
+const eraseCompleted = () => {
   const btn = document.querySelector("#remover-finalizados");
 
   btn.addEventListener("click", () => {
+    const lis = document.querySelectorAll(taskItem);
     lis.forEach((item) => {
       if (item.classList.contains("completed")) {
         item.remove();
@@ -89,4 +83,66 @@ const saveTasks = () => {
   });
 
   taskList.innerHTML = myStorage.getItem("task-list");
+  completedTask();
 };
+
+const moveUp = () => {
+  // marretei MUITO a cabeça, mas passando pelas breakouts, alguém falou sobre
+  // elementSibling e fui logo pesquisar e cheguei numa POSSIVEL solução
+  // eu vi uma explicação massa aqui: https://www.javascripttutorial.net/javascript-dom/javascript-siblings/
+  const moveUpBtn = document.querySelector("#mover-cima");
+
+  moveUpBtn.addEventListener("click", () => {
+    const selected = document.querySelector(".gray-bgcolor");
+    if (selected !== null) {
+      const previous = selected.previousElementSibling;
+      // é aqui que ele sobe, inserindo ANTES (before)
+      if (selected !== taskList.firstChild) {
+        taskList.insertBefore(selected, previous);
+      }
+    }
+  });
+};
+
+const moveDown = () => {
+  // a diferença pro moveDown() do moveUp() é justamente o sibling, MAS
+  // aqui é o PRÓXIMO (after) e não ANTES (before) como acima. O resto
+  // é bem similar, muda o proximo elemento que o insertBefore() vai receber
+  // em vez de ser o primeiro filho, vai ser o ultimo filho
+  const moveDownBtn = document.querySelector("#mover-baixo");
+
+  moveDownBtn.addEventListener("click", () => {
+    const selected = document.querySelector(".gray-bgcolor");
+    if (selected !== null) {
+      const next = selected.nextElementSibling;
+      // é aqui que ele sobe, inserindo ANTES (before)
+      if (selected !== taskList.lastChild) {
+        taskList.insertBefore(selected, next.nextElementSibling);
+      }
+    }
+  });
+};
+
+const eraseSelected = () => {
+  const btn = document.querySelector("#remover-selecionado");
+
+  btn.addEventListener("click", () => {
+    const lis = document.querySelectorAll(taskItem);
+    lis.forEach((item) => {
+      if (item.classList.contains("gray-bgcolor")) {
+        item.remove();
+      }
+    });
+  });
+};
+
+saveTasks();
+createTask();
+eraseTasks();
+highlightTask();
+
+moveUp();
+moveDown();
+
+eraseSelected();
+eraseCompleted();
