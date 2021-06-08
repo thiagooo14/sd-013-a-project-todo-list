d9823f48bf89ad18c9bf7ed5e01f3f998a4ab7c1
Micael Santos <micaelmaicon@hotmail.com>
2021-06-07 23:20:15 -0300

    Finish requiremente 10

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-// Iniciando o projeto 
 const getOl = document.getElementById('lista-tarefas');
 const getInputTask = document.getElementById('texto-tarefa');
 const btnCreateTask = document.getElementById('criar-tarefa');
 
 // Remove bacground aplicado
-function removeClass(value) {
+function removeClass(value, classRemove) {
   for (let index = 0; index < value.length; index += 1) {
-    value[index].classList.remove('apply-bg');
+    value[index].classList.remove(classRemove);
+  }
+}
+
+// Função taxa a task concluida.
+function stripeTask(event) {
+  const verify = event.target.classList.contains('completed');
+  if (!verify) {
+    event.target.classList.add('completed');
+  } else {
+    event.target.classList.remove('completed');
   }
 }
 
 // adiciona background color 
-function addBgColor(event, sizeOl) {
+function addBgColor(event) {
   const getList = document.querySelectorAll('.task');
-  removeClass(getList);
+  removeClass(getList, 'apply-bg');
   event.target.classList.add('apply-bg');
 }
 
 // Limpar inputs
-function limparInput(value) {
-  value.value = '';
+function limparInput(valueInput) {
+  valueInput.value = '';
 }
 
 //  Aplica um evento click
 function eventClick(value) {
   for (let index = 0; index < value.length; index += 1) {
     value[index].addEventListener('click', addBgColor);
+    value[index].addEventListener('dblclick', stripeTask)
   }
 }
 
@@ -37,7 +47,6 @@ function createTasks(valueText) {
   getOl.appendChild(createLi);
   const getList = document.querySelectorAll('.task');
   eventClick(getList);
-  console.log(getList);
 }
 
 // Pegar tarefa
