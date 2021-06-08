85ddf418e9f986fb19a18a39cabf9b7a4ceb329b
Micael Santos <micaelmaicon@hotmail.com>
2021-06-07 22:18:32 -0300

    Finish requeriment 7 - Uuuuuuufa

--- script.js
+++ script.js
@@ -3,22 +3,46 @@ const getOl = document.getElementById('lista-tarefas');
 const getInputTask = document.getElementById('texto-tarefa');
 const btnCreateTask = document.getElementById('criar-tarefa');
 
+// Remove bacground aplicado
+function removeClass(value) {
+  for (let index = 0; index < value.length; index += 1) {
+    value[index].classList.remove('apply-bg');
+  }
+}
+
+// adiciona background color 
+function addBgColor(event, sizeOl) {
+  const getList = document.querySelectorAll('.task');
+  removeClass(getList);
+  event.target.classList.add('apply-bg');
+}
+
 // Limpar inputs
 function limparInput(value) {
   value.value = '';
 }
 
+//  Aplica um evento click
+function eventClick(value) {
+  for (let index = 0; index < value.length; index += 1) {
+    value[index].addEventListener('click', addBgColor);
+  }
+}
+
 // Cria as tasks como lis.
 function createTasks(valueText) {
   const createLi = document.createElement('li');
+  createLi.classList.add('task');
   createLi.innerHTML = valueText;
   getOl.appendChild(createLi);
+  const getList = document.querySelectorAll('.task');
+  eventClick(getList);
+  console.log(getList);
 }
 
 // Pegar tarefa
 function getTask() {
   const receiveValue = getInputTask.value;
-  console.log(getInputTask.value);
   createTasks(receiveValue);
   limparInput(getInputTask);
 }
