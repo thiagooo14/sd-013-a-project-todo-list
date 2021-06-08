514670db2956fd8ad3fb3e498c9146039ce02325
Ramon Vitor <vitorramon@hotmail.com>
2021-06-08 19:25:53 -0300

    Resolvida questão 10 e questão 9 ainda está incompleta

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btnAdd = document.getElementById('criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
 const inputText = document.getElementById('texto-tarefa');
 
+// Botão para adicionar:
 btnAdd.addEventListener('click', function(){
   const itemList = document.createElement('li');
   itemList.innerText = inputText.value;
@@ -18,20 +19,24 @@ btnAdd.addEventListener('click', function(){
       }
       liList[i].style.backgroundColor = 'rgb(128,128,128)';
     })
+  };
 
+  // Evento para marcar e desmarcar o item:
+  for (let i = 0; i < liList.length; i += 1) {
+    liList[i].addEventListener('dblclick', function(){
+        if (liList[i].style.textDecoration == 'none') {
+          liList[i].classList.add('completed');
+          liList[i].style.textDecoration = 'line-through';
+        } else if (liList[i].style.textDecoration = 'line-through') {
+          liList[i].classList.remove('completed');
+          liList[i].style.textDecoration = 'none';
+        }
+    })  
   }
 
-  // Evento para riscar o texto:
-   itemList.addEventListener('dblclick', function(){
-     if (itemList.style.textDecoration = 'none') {
-       itemList.style.textDecoration = 'line-through';
-     }
-     // Evento para desriscar o texto:
-     itemList.addEventListener('dblclick', function(){
-       if (itemList.style.textDecoration = 'line-through') {
-       itemList.style.textDecoration = 'none';
-      }
-     })  
+  // Botão para limpar lista:
+  const btnLimpa = document.getElementById('apaga-tudo');
+  btnLimpa.addEventListener('click', function() {
+    document.location.reload();
   })
 })
-
