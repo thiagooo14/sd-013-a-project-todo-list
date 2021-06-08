491044bacac1599ee829776a8f57a731b0218f7f
Jackson Santana <onjacksm@gmail.com>
2021-06-08 04:33:42 -0300

    Adiciona requisitos 7 ao 10

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ function criaTarefa () {
   entradaTarefa.value = "";
 }
 document.getElementById('criar-tarefa').addEventListener('click',criaTarefa);
+
+function marcaItem (event) {
+  for (let index = 0; index < tarefas.length; index += 1){
+    if (tarefas[index].style.backgroundColor === 'rgb(128, 128, 128)'){
+      tarefas[index].style.backgroundColor = 'white';
+    }
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+listaTarefas.addEventListener('click',marcaItem);
+
+function tarefaCompleta (event) {
+  if (event.target.classList.item(0) === 'completed') {
+      event.target.classList.remove('completed');
+  } else {
+      event.target.classList.add('completed');
+  }
+}
+listaTarefas.addEventListener('dblclick',tarefaCompleta);
+
+//Referência: https://developer.mozilla.org/pt-BR/docs/Web/API/Node/removeChild
+//Referência: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/while
+function apagaTudo () {
+  while (listaTarefas.firstChild) {
+    listaTarefas.removeChild(listaTarefas.firstChild);
+  }
+}
+document.getElementById('apaga-tudo').addEventListener('click',apagaTudo);
+
