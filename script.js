8d85151ddcf57a862e49184de6c7f2743332f952
Luiz Casimiro <luizcasimi@gmail.com>
2021-06-07 21:53:45 -0300

    solve 8

--- script.js
+++ script.js
@@ -31,15 +31,18 @@ funcionamento.innerHTML = 'Clique duas vezes em um item para marcá-lo como comp
 // ADD BOTÃO 'ADICIONAR' -> DESAFIOS 5 E 6
 
 // Cria botão 'Adicionar':
+
 const botaoAdicionar = document.querySelector('#criar-tarefa');
 botaoAdicionar.innerHTML = 'Adicionar';
 
 // Cria variáveis para armazenar texto de entrada + lista:
+
 const inputText = document.querySelector('#texto-tarefa');
 const theList = document.querySelector('#lista-tarefas');
 
 // Cria função para adicionar o texto digitado na lista:
-function Adicionar() {
+
+function adicionar() {
   const listRow = document.createElement('li');
   listRow.innerHTML = inputText.value;
   theList.appendChild(listRow);
@@ -49,20 +52,32 @@ function Adicionar() {
 }
 
 // Cria função que ouve o clique e chama a função 'Adicionar':
-function Clique() {
-  botaoAdicionar.addEventListener('click', Adicionar);
+
+function clique() {
+  botaoAdicionar.addEventListener('click', adicionar);
 }
-Clique();
+
+clique();
 
 // -------------------------------------------------------
 
-// COLORE ITEM DE CINZA COM CLIQUE -> DESAFIO 7
+// COLORE ITEM DE CINZA (NO REPEAT) -> DESAFIOS 7 E 8
 
-// Cria função para colorir o item de cinza quando clicado:
-function ColoreCinza(event) {
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+// Cria função que traz a cor cinza ao background do item clicado:
+
+function trazOCinza(event) {
+  if (document.querySelector('.corCinza') === null) {
+    event.target.classList.add('corCinza');
+    document.querySelector('.corCinza').style.backgroundColor = 'rgb(128, 128, 128)';
+  } else {
+    document.querySelector('.corCinza').style.backgroundColor = 'white';
+    document.querySelector('.corCinza').classList.remove('corCinza');
+
+    event.target.classList.add('corCinza');
+    document.querySelector('.corCinza').style.backgroundColor = 'rgb(128, 128, 128)';
+  }
 }
-// Cria função que ouve o clique e chama a função 'ColoreCinza':
-theList.addEventListener('click', ColoreCinza);
+
+theList.addEventListener('click', trazOCinza);
 
 // -------------------------------------------------------
