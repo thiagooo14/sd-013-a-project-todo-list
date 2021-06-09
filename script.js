616b654e2920be39820f49aae12dd722588d56ef
thomasravache <thomasravache31@gmail.com>
2021-06-09 13:45:52 -0300

    Requisito 13 --> Finalizado --> Adicionar botões de movimento para cima e para baixo.

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const textTask = document.querySelector('#texto-tarefa');
 const btnClearAll = document.querySelector('#apaga-tudo');
 const btnClearCompleted = document.querySelector('#remover-finalizados');
 const btnSaveTasks = document.querySelector('#salvar-tarefas');
-
+const btnMoveUp = document.querySelector('#mover-cima');
+const btnMoveDown = document.querySelector('#mover-baixo');
 
 function clearLastSelected() {
   for (let index = 0; index < toDoList.childElementCount; index += 1) {
@@ -13,8 +14,8 @@ function clearLastSelected() {
 }
 
 function loadTasks() {
-  let tasks = JSON.parse(localStorage.getItem('tasks'));
-  let classes = JSON.parse(localStorage.getItem('classes'))
+  let tasks = JSON.parse(localStorage.getItem('taskContents'));
+  let classes = JSON.parse(localStorage.getItem('taskClasses'))
 
   for (let index in tasks) {
     let item = document.createElement('li');
@@ -26,47 +27,105 @@ function loadTasks() {
 
 //Botão que quando clicado insere novos itens
 btnCreateItem.addEventListener('click', function() {
-  console.log('botão de adicionar item foi clicado.');
-  let item = document.createElement('li');
-  let createdItem = toDoList.appendChild(item);
-  createdItem.className = 'list-item';
-  createdItem.innerHTML = textTask.value;
-  textTask.value = '';
-  textTask.focus();
+  if (textTask.value == '') {
+    alert('Digite o nome da tarefa');
+    textTask.focus();
+  } else {
+    console.log('botão de adicionar item foi clicado.');
+    let item = document.createElement('li');
+    let createdItem = toDoList.appendChild(item);
+    createdItem.className = 'list-item';
+    createdItem.innerHTML = textTask.value;
+    textTask.value = '';
+    textTask.focus();
+  }
 });
 
 //Botão que elimina as tarefas que já foram completas
 btnClearCompleted.addEventListener('click', function() {
-  for (let index = 0; index < toDoList.childElementCount; index += 1) {
-    if(toDoList.children[index].classList.contains('completed')) {
-      toDoList.removeChild(toDoList.children[index]);
-      index = 0;
+  let completedTasks = document.querySelectorAll('.completed');
+
+  for (let index = 0; index < completedTasks.length; index += 1) {
+    if (completedTasks[index].classList.contains('completed')) {
+      completedTasks[index].remove();
     }
   }
 });
 
 //Botão que quando clicado remove todos os itens da lista
 btnClearAll.addEventListener('click', function() {
-  let listLength = toDoList.childElementCount;
+  let listItem = document.querySelectorAll('.list-item');
 
-  for (let index = 0; index < listLength; index += 1) {
-    toDoList.removeChild(toDoList.children[0]);
+  for (let index = 0; index < listItem.length; index += 1) {
+    listItem[index].remove();
   }
-  // localStorage.removeItem('tasks');
 });
 
 btnSaveTasks.addEventListener('click', function() {
-  let tasks = [];
-  let classes = []
+  let taskContents = [];
+  let taskClasses = [];
+  let tasks = document.querySelectorAll('#lista-tarefas li');
 
-  for (let index = 0; index < toDoList.childElementCount; index += 1) {
-    tasks.push(toDoList.children[index].textContent);
-    classes.push(toDoList.children[index].className);
+  // for (let index = 0; index < toDoList.childElementCount; index += 1) {
+  //   taskContents.push(toDoList.children[index].textContent);
+  //   taskClasses.push(toDoList.children[index].className);
+  // }
+
+  for (let index = 0; index < tasks.length; index += 1) {
+    taskContents.push(tasks[index].textContent);
+    taskClasses.push(tasks[index].className);
   }
 
-  localStorage.setItem('tasks',JSON.stringify(tasks));
-  localStorage.setItem('classes',JSON.stringify(classes));
+  localStorage.setItem('taskContents',JSON.stringify(taskContents));
+  localStorage.setItem('taskClasses',JSON.stringify(taskClasses));
 });
+
+btnMoveUp.addEventListener('click', function() {
+  let listItem = document.querySelectorAll('.list-item');
+
+  for (let index = 0; index < listItem.length; index += 1) {
+    let isSelectedItem = listItem[index].classList.contains('selected');
+    let isValidMovement = (index - 1 >= 0);
+
+    if (isSelectedItem && isValidMovement) {
+      let selectedItem = listItem[index];
+      let previousElement = listItem[index - 1];
+      let auxiliar;
+
+      auxiliar = previousElement.textContent;
+      previousElement.innerHTML = selectedItem.textContent;
+      selectedItem.innerHTML = auxiliar;
+
+      auxiliar = previousElement.className;
+      previousElement.className = selectedItem.className;
+      selectedItem.className = auxiliar;
+    }
+  }
+});
+
+btnMoveDown.addEventListener('click', function() {
+  let listItem = document.querySelectorAll('.list-item');
+
+  for (let index = listItem.length-1; index >= 0; index -= 1) {
+    let isSelectedItem = listItem[index].classList.contains('selected');
+    let isValidMovement = (index + 1 < listItem.length);
+
+    if (isSelectedItem && isValidMovement) {
+      let selectedItem = listItem[index];
+      let nextElement = listItem[index + 1];
+      let auxiliar;
+
+      auxiliar = nextElement.textContent;
+      nextElement.innerHTML = selectedItem.textContent;
+      selectedItem.innerHTML = auxiliar;
+
+      auxiliar = nextElement.className;
+      nextElement.className = selectedItem.className;
+      selectedItem.className = auxiliar;
+    }
+  }
+});
+
 
 //Listener que seleciona o item da lista
 document.addEventListener('click', function(event) {
@@ -82,8 +141,10 @@ document.addEventListener('dblclick', function(event) {
   if(event.target.classList.contains('list-item')) {
     if (event.target.classList.contains('completed')) {
       event.target.classList.remove('completed');
+      event.target.classList.remove('selected');
     } else {
       event.target.classList.add('completed');
+      event.target.classList.remove('selected');
     }
   }
 });
