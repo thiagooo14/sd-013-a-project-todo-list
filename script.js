23b023e373f4c3ea95bdb8cc332c05dcbb7d84ba
Pedro Muniz <pedro.muniz@aluno.uece.br>
2021-06-09 19:49:13 -0300

    att4

--- script.js
+++ script.js
@@ -7,17 +7,3 @@ function addTar(){
   document.getElementById("lista-tarefas").appendChild(li);
   document.getElementById("texto-tarefa").value = "";
 }
-
-/* function applyGray(){
-  let y = document.getElementsByTagName("li")
-  let pixels = document.querySelector('#lista-tarefa');
-
-  pixels.addEventListener('click', function(event){
-    if (y.length > 0){
-      let color = y[0].style.backgroundColor;
-      event.target.style.backgroundColor = color;
-    }
-  });
-}; */
-
-applyGray()
