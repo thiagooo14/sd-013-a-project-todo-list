a453292d3263e67d44babd523b19f0923629dbe1
ivandosss <ivanndosssilva9389@gmail.com>
2021-06-09 14:54:12 -0300

    Botao apaga tudo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const botao = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
 const input = document.getElementById('texto-tarefa');
+const btnApagaTudo = document.getElementById('apaga-tudo');
 
 botao.addEventListener('click', () => {
   let elementList = document.createElement('li');
@@ -8,16 +9,15 @@ botao.addEventListener('click', () => {
   elementList.innerText = input.value;
   input.value = '';
   elementList.addEventListener('click', (e) => {
-    for (let filho of ol.children){
-      filho.style.backgroundColor = ''; 
+    for (let filho of ol.children) {
+      filho.style.backgroundColor = '';
     }
-    e.target.style.backgroundColor = 'rgb(128, 128, 128)'; 
-    
-  }); 
-  elementList.addEventListener('dblclick', () =>{
+    e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
+  elementList.addEventListener('dblclick', () => {
     elementList.classList.toggle('completed');
-
+  });
+  btnApagaTudo.addEventListener('click', () => {
+    document.querySelector('li').remove(elementList);
   });
 });
-
-
