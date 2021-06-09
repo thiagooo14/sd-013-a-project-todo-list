0d6ccab1b08820e6aa50d0f72f76a35b4c788ea4
Marcelo Cunha <marcelo.cunha1@outlook.com>
2021-06-09 08:27:52 -0300

    resolved issue seven

--- script.js
+++ script.js
@@ -5,23 +5,36 @@ window.onload = function(){
   var buttonInputTesk = document.getElementById("criar-tarefa");
   var itensOl = document.getElementById("lista-tarefas");
 
-// função para adicionar tesks
+  // função para adicionar tesks
   function addTask() {
-    if(textInput.value.length !== 0) { // verifica se o texto é diferente de 0 / vazio
-      let newLi = document.createElement("li"); // cria uma li 
-      itensOl.appendChild(newLi); // coloca li como filho da Ol
-      newLi.innerText = textInput.value; // transferi o valor do input em um novo item da lista
-      textInput.value = ""; // limpa input depois da adição
+    if(textInput.value.length !== 0) { // verifica se o texto é diferente de 0 / vazio.
+      let newLi = document.createElement("li"); // cria uma li .
+      itensOl.appendChild(newLi); // coloca li como filho da Ol.
+      newLi.innerText = textInput.value; // transferi o valor do input em um novo item da lista.
+      textInput.value = ""; // limpa input depois da adição.
+    }
+    else
+      alert ("É precisa digitar uma Tarefa ^^"); // caso o input estiver fazio, irá dar esse alert.
+  }
+
+  buttonInputTesk.addEventListener("click", addTask); // interação para adicionar a tesk na lista.via button.
+
+
+  // deixando fundo cinza 
+  let allTegsLi = document.getElementsByTagName ("li");
+
+  function setGreyColor(eventColorGrey) {
+    let liEventSelected = eventColorGrey.target; // adicionando target na li para criar evento
+    // for in passeado em: https://www.w3schools.com/jsref/jsref_forin.asp
+    for (i in allTegsLi) {
+      if (liEventSelected === allTegsLi[i] && !(liEventSelected.classList.contains("liSelected"))) {
+        allTegsLi[i].classList.add("liSelected");
+      }
+      else if (liEventSelected === allTegsLi[i] && liEventSelected.classList.contains("liSelected")) {
+        allTegsLi[i].classList.remove("liSelected");
+      }
     }
   }
-buttonInputTesk.addEventListener("click", addTask);
 
-
-
-
-
-
-
-
-
+  itensOl.addEventListener("click", setGreyColor);
 }
