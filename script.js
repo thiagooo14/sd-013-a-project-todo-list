96bdf2886085ef7823a1f9c8e9be12fea454e181
Wallacy Francis <wallacy_francis@hotmail.com>
2021-06-10 11:17:57 -0300

    Realizado exercícios 8 e 9

--- script.js
+++ script.js
@@ -2,12 +2,39 @@ const btnInput = document.getElementById('criar-tarefa');
 const taskText = document.getElementById('texto-tarefa');
 const taskList = document.getElementsByTagName('ol')[0];
 
+
 btnInput.onclick = () => {
   const taskItem = document.createElement('li');
   taskItem.innerText = taskText.value;
   taskList.appendChild(taskItem);
+  const listItem = document.getElementsByTagName('li');
+  for (let index = 0; index < listItem.length; index++) {
+
+    listItem[index].addEventListener('click', () => {
+
+    for (let j = 0; j < listItem.length; j += 1) {
+      if ((listItem[j].className === 'select') || (listItem[j].className === 'completed select')) {
+      listItem[j].classList.remove('select');
+      } 
+    };
+
+    listItem[index].classList.add('select');
+  
+    listItem[index].addEventListener('dblclick', () => {
+      if (listItem[index].className === 'select') {
+        listItem[index].classList.add('completed');
+        listItem[index].classList.remove('select');
+      } else if (listItem[index].className === 'completed select') {
+        listItem[index].classList.remove('completed');
+        listItem[index].classList.remove('select');
+      }
+      
+    });
+
+    }); 
+  }
   taskText.value = '';
 };
 
-/* const taskLi = document.querySelectorAll('li'); */
-console.log(localStorage);
+/*  */
+
