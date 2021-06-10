cba08dc6d140ccccd26b947f0b9e7bb1c68e508d
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-06-10 05:06:08 -0300

    Resolve o requisito 2

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function adicionaTitulo() {
   header.appendChild(h1);
 }
 adicionaTitulo();
+
+// Requisito 2 - Adicione abaixo do título um pequeno e discreto parágrafo com id="funcionamento" e com o texto "Clique duas vezes em um item para marcá-lo como completo"
+function addParagraph() {
+  const h1 = document.getElementsByTagName('h1')[0];
+  const paragraph = document.createElement('p');
+  paragraph.id = 'funcionamento';
+  paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+  h1.appendChild(paragraph);
+}
+addParagraph();
