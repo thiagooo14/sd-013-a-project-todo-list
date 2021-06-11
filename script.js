c1234be7b5c8b025a7daf4f99f6d8efaedf767b0
Marcelo Pantoja <pantoja.marcelo@hotmail.com>
2021-06-10 21:20:01 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ window.onload = function() {
         const contentTask = document.getElementById('texto-tarefa');
         const contentList = document.getElementById('lista-tarefas');
         const listItem = document.createElement('li');
+        const taskNumbers = document.querySelectorAll('li');
+        listItem.id = taskNumbers.length + 1;
         if (contentTask.value === '' || contentTask.value.indexOf(' ') === 0) {
             alert('Não Preenchido');
             contentTask.focus();
@@ -18,8 +20,33 @@ window.onload = function() {
     function clickSelect() {
         const clickButton = document.getElementById('criar-tarefa');
         clickButton.addEventListener('click', taskList);
-        console.log('oiiiiie');
+        clickButton.addEventListener('keypress', function (e) {
+            if (e.key === 'Enter') {
+                console.log('Enter');
+            }
+        });
     }
     clickSelect();
+
+    function paintTask() {
+        const listItens = document.getElementById('lista-tarefas');
+        listItens.addEventListener('click', function (c) {
+            painting(c.target.id)
+        });
+    }
+    paintTask();
+
+    function painting(idList) {
+        const item = document.querySelectorAll('li');
+        for (let i = 0; i < item.length; i += 1) {
+            if (item[i].id === idList) {
+              document.getElementById(item[i].id).classList.add('itens-for-color');
+            }else {
+               document.getElementById(item[i].id).classList.remove('itens-for-color'); 
+            };
+        }
+    }
+
+
 
 }
