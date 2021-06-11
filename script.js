4378c2ad2c035bc6df4e24215f4eed8df61c8104
junglejf <jfcchagas@id.uff.br>
2021-06-11 01:13:23 -0300

    falta somente resolver o quesito do local storage e web storage

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const addButton = document.getElementById('criar-tarefa');
 const task = document.querySelector('#texto-tarefa');
-const taskList = document.querySelector('#lista-tarefas');
+let taskList = document.querySelector('#lista-tarefas');
 let lastMarked = null;
 const cleanButton = document.getElementById('apaga-tudo');
 const cleanCheckedButton = document.getElementById('remover-finalizados');
 const removeSelectedButton = document.getElementById('remover-selecionado');
+const moveUpButton = document.getElementById('mover-cima');
+const moveDownButton = document.getElementById('mover-baixo');
+
 // tamanho do texto para marcar somente área escrita
 function getTextWidth(text, font) {
-  console.log(text.length);
   const canvas = document.createElement('canvas');
   const context = canvas.getContext('2d');
 
@@ -15,11 +17,29 @@ function getTextWidth(text, font) {
 
   const pxSize = (Math.round(context.measureText(text).width * 1.8)).toString();
   return pxSize.concat('px');
-}
+};
+// funcão que troca a posicão dos elementos do dom
+function swampElem(paiElem,newElem,oldElem) {
+  paiElem.insertBefore(newElem,oldElem);
+};
+
+// mover elemento da lista de tarefas para cima
+const onButtonMoveDown = function() {
+  if (taskList.lastElementChild !== lastMarked) {
+    swampElem(taskList, lastMarked.nextElementSibling, lastMarked); 
+  }
+};
+
+// mover elemento da lista de tarefas para baixo
+const onButtonMoveUp = function() {
+  if (taskList.firstElementChild !== lastMarked) {
+    swampElem(taskList, lastMarked.previousElementSibling, lastMarked.nextElementSibling);
+  }
+};
 
 const onButtonClick = function () {
   if (task.value.length > 0) {
-    const newTask = document.createElement('li');
+    let newTask = document.createElement('li');
     newTask.innerHTML = task.value;
     newTask.style.width = getTextWidth(task.value);
     newTask.style.margin = '10px';
@@ -50,20 +70,22 @@ const cleanList = function () {
 
 const cleanFinished = function () {
   var riskedItens = taskList.getElementsByClassName('completed');
-    while(riskedItens[0]){
+    while (riskedItens[0]) {
       riskedItens[0].parentNode.removeChild(riskedItens[0]);
 
   }
-}
+};
 
 const selected = function () {
     lastMarked.parentNode.removeChild(lastMarked);
-}
+};
 
 // Listeners
 addButton.addEventListener('click', onButtonClick);
 taskList.addEventListener('click', onItem);
 taskList.addEventListener('dblclick', checkedItem);
 cleanButton.addEventListener('click', cleanList);
-cleanCheckedButton.addEventListener('click',cleanFinished);
-removeSelectedButton.addEventListener('click',selected);
+cleanCheckedButton.addEventListener('click', cleanFinished);
+removeSelectedButton.addEventListener('click', selected);
+moveDownButton.addEventListener('click', onButtonMoveDown);
+moveUpButton.addEventListener('click', onButtonMoveUp);
