c29583518750d231cd51954f6f5642069bd9c74a
suco <francalucas937@gmail.com>
2021-06-11 18:22:44 -0300

    requisito 10 concluido

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ function createOl() {
 }
 
 createOl();
-
+// esta parte é responsável por pegar o nome digitado no imput e adiconar a lista;
 const paiLi = document.getElementsByTagName('ol')[0];
 function createOlItem() {
   const input1 = document.getElementById('texto-tarefa').value;
   const listItem = document.createElement('li');
-  listItem.innerHTML = input1;
-  document.getElementById('texto-tarefa').value = '';
-  paiLi.appendChild(listItem);
+  if (input1 === '' || input1 === ' ') {
+    alert('item Inválido');
+  } else {
+    listItem.className = 'style-font';
+    listItem.innerHTML = input1;
+    document.getElementById('texto-tarefa').value = '';
+    paiLi.appendChild(listItem);
+  }
 }
 
 function classeCompleted(e) {
@@ -37,6 +42,12 @@ function verifyLi() {
   }
 }
 
+const buttonClear = document.getElementById('apaga-tudo');
+function apagaTudo() {
+  paiLi.innerHTML = '';
+}
+
+buttonClear.addEventListener('click', apagaTudo);
 const button = document.getElementById('criar-tarefa');
 button.addEventListener('click', createOlItem);
 button.addEventListener('click', verifyLi);
