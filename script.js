4738f2b6db3c49dc1cdfd1b4d4d71368f7b37ca8
Antonio Martins <anmmartyns@gmail.com>
2021-06-11 00:42:11 -0300

    Requisitos 9 à 11

--- script.js
+++ script.js
@@ -1,36 +1,69 @@
 // Requisitos 5 e 6
 
 const toDoList = document.getElementById("lista-tarefas");
-const button = document.getElementById("criar-tarefa");
+const btnNewToDo = document.getElementById("criar-tarefa");
 const input = document.getElementById("texto-tarefa");
 
 function newToDo() {
-    const newLi = document.createElement("li");
-    newLi.classList.add("toDoItens")
-    newLi.innerText = input.value;
-    toDoList.appendChild(newLi);
-    input.value = null;
-    // Adicionados para os requisitos 7 e 8
-    newLi.addEventListener("click", toDoSelection);
-    // newLi.addEventListener("dblclick", )
+  const newLi = document.createElement("li");
+  newLi.classList.add("toDoItens");
+  newLi.innerText = input.value;
+  toDoList.appendChild(newLi);
+  input.value = null;
+  // Adicionados para os requisitos 7 à 9
+  newLi.addEventListener("click", toDoSelection);
+  newLi.addEventListener("dblclick", toDoCompleted);
 }
 
-button.addEventListener("click", newToDo);
+btnNewToDo.addEventListener("click", newToDo);
 input.addEventListener("keypress", function(event) {
-    if (event.keyCode == 13) {
-        button.click();
-    }
+  if (event.keyCode == 13) {
+    btnNewToDo.click();
+  }
 })
 
-// Requisito 7
+// Requisitos 7 e 8
 
 function toDoSelection(event) {
-    const toDoItens = document.getElementsByClassName("toDoItens");
-    for (let index = 0; index < toDoItens.length; index += 1) {
-        if (toDoItens[index].classList.contains("selected")) {
-            toDoItens[index].classList.remove("selected");
-        }
+  const toDoItens = document.getElementsByClassName("toDoItens");
+  for (let index = 0; index < toDoItens.length; index += 1) {
+    if (toDoItens[index].classList.contains("selected")) {
+      toDoItens[index].classList.remove("selected");
     }
-    event.target.classList.add("selected");
+  }
+  event.target.classList.add("selected");
 }
 
+// Requisito 9
+
+function toDoCompleted(event) {
+  const toDoItens = document.getElementsByClassName("toDoItens");
+  if (event.target.classList.contains("completed")) {
+    event.target.classList.remove("completed");
+  } else {
+    event.target.classList.add("completed");
+  }
+}
+
+// Requisito 10
+
+const btnClearAll = document.getElementById("apaga-tudo");
+
+function clearAll() {
+  toDoList.innerHTML = null;
+}
+
+btnClearAll.addEventListener("click", clearAll);
+
+// Requisito 11
+
+const btnClearCompleted = document.getElementById("remover-finalizados");
+
+function clearCompleted() {
+  const toDoCompleted = document.querySelectorAll(".completed");
+  for (let index = 0; index < toDoCompleted.length; index += 1) {
+    toDoCompleted[index].remove();
+  }
+}
+
+btnClearCompleted.addEventListener("click", clearCompleted);
