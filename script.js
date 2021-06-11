b5f0d4651c102a73320a74d9f31ab6c82411a3a7
Pedro Muniz <pedro.muniz@aluno.uece.br>
2021-06-11 17:38:28 -0300

    att6

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function addTar(){
 }
 
 function addSelected (event){
-  let x = event.target;
+  let x = event.target
   let element = document.getElementsByTagName("li")
 
   for (let i = 0; i < element.length; i++){
@@ -18,3 +18,32 @@ function addSelected (event){
 
   x.classList.add('selected')
 } 
+
+function riscaOp (event){
+  let x = event.target
+
+  if (x.classList.contains("completed") === true){
+    x.classList.remove("completed")
+  } else {
+    x.classList.add("completed")
+  }
+}
+
+function clear(){
+  let x = document.getElementById("lista-tarefas")
+
+  while (x.hasChildNodes()) {
+    x.removeChild(x.firstChild)
+  }
+}
+
+function clearDone(){
+  let x = document.getElementsByClassName("completed");
+
+  while(x.length > 0){
+    x[0].parentNode.removeChild(x[0]);
+  }
+}
+
+document.getElementById("apaga-tudo").addEventListener("click", clear)
+document.getElementById("remover-finalizados").addEventListener("click", clearDone)
