f96f9a33c8ad5ee4aeba9fc3b147881703363750
junglejf <jfcchagas@id.uff.br>
2021-06-11 03:12:05 -0300

    projeto finalizado, falta eliminar os problemas do Lynt

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const removeSelectedButton = document.getElementById('remover-selecionado');
 const moveUpButton = document.getElementById('mover-cima');
 const moveDownButton = document.getElementById('mover-baixo');
 
+function saveList() {
+  localStorage.setItem('taskList', taskList.innerHTML);
+}
 // tamanho do texto para marcar somente área escrita
 function getTextWidth(text, font) {
   const canvas = document.createElement('canvas');
@@ -22,21 +25,25 @@ function getTextWidth(text, font) {
 function swampElem(paiElem,newElem,oldElem) {
   paiElem.insertBefore(newElem,oldElem);
 };
-
+// recupera a lista de tarefas da sessão anterior
+const loadList = function() {
+  taskList.innerHTML = localStorage.getItem('taskList');
+};
 // mover elemento da lista de tarefas para cima
 const onButtonMoveDown = function() {
   if (lastMarked && taskList.lastElementChild !== lastMarked) {
     swampElem(taskList, lastMarked.nextElementSibling, lastMarked); 
+    saveList();
   }
 };
-
 // mover elemento da lista de tarefas para baixo
 const onButtonMoveUp = function() {
   if (lastMarked && taskList.firstElementChild !== lastMarked) {
     swampElem(taskList, lastMarked, lastMarked.previousElementSibling);
+    saveList();
   }
 };
-
+// adiciona a nova tarefa inserida no input box
 const onButtonClick = function () {
   if (task.value.length > 0) {
     let newTask = document.createElement('li');
@@ -45,9 +52,10 @@ const onButtonClick = function () {
     newTask.style.margin = '10px';
     taskList.appendChild(newTask);
     task.value = '';
+    saveList();
   }
 };
-
+// marca o item selecionado com a cor cinza
 const onItem = function (e) {
   if (e.target.tagName === 'LI') {
     if (lastMarked != null) {
@@ -55,31 +63,35 @@ const onItem = function (e) {
     }
     e.target.style.backgroundColor = 'rgb(128, 128, 128)';
     lastMarked = e.target;
+    saveList();
   }
 };
-
+// com o duplo clique gera um riscado no item
 const checkedItem = function (e) {
   if (e.target.tagName === 'LI') {
     e.target.classList.toggle('completed');
+    saveList();
   }
 };
-
+// deleta a lista
 const cleanList = function () {
   taskList.innerHTML = '';
+  saveList();
 };
-
+// limpa os elementos que já foram finalizados
 const cleanFinished = function () {
-  var riskedItens = taskList.getElementsByClassName('completed');
-    while (riskedItens[0]) {
-      riskedItens[0].parentNode.removeChild(riskedItens[0]);
-
+  let riskedItens = taskList.getElementsByClassName('completed');
+  while (riskedItens[0]) {
+    riskedItens[0].parentNode.removeChild(riskedItens[0]);
   }
+  saveList();
 };
-
+// remove o último item marcado
 const selected = function () {
-    if(lastMarked !== null) {
-      lastMarked.parentNode.removeChild(lastMarked);
-    }
+  if(lastMarked !== null) {
+    lastMarked.parentNode.removeChild(lastMarked);
+    saveList();
+  }
 };
 
 // Listeners
@@ -91,3 +103,4 @@ cleanCheckedButton.addEventListener('click', cleanFinished);
 removeSelectedButton.addEventListener('click', selected);
 moveDownButton.addEventListener('click', onButtonMoveDown);
 moveUpButton.addEventListener('click', onButtonMoveUp);
+document.addEventListener('DOMContentLoaded', loadList);
