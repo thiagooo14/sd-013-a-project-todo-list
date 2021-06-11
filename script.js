7185b0f9fdba95d928ae3a3d78c3b51e273a802f
Walace Nascimento <wlacens@hotmail.com>
2021-06-11 15:12:15 -0300

    Resolve o requisito 7 e8

--- script.js
+++ script.js
@@ -12,11 +12,28 @@ function createLi() {
   const tarefas = document.getElementById('lista-tarefas');
   const inputTarefa = document.getElementById('texto-tarefa');
   const btn = document.getElementById('criar-tarefa');
-  btn.addEventListener('click', function() {
+  btn.addEventListener('click', (event) => {
     const lista = document.createElement('li');
+    lista.setAttribute('id', 'lista-li');
     lista.innerText = inputTarefa.value;
     tarefas.appendChild(lista);
     inputTarefa.value = '';
   });
 }
 createLi();
+
+// Alterar a cor de fundo do item da lista quando clicado
+// Não permite selecionar mais de um elemento da lista ao mesmo tempo
+function mudaCorDoFundo() {
+  const listOl = document.querySelector('ol');
+  const listLi = document.getElementsByTagName('li');
+  listOl.addEventListener('click', (event) => {
+    for (let index = 0; index < listLi.length; index += 1) {
+      const li = listLi[index];
+      li.style.backgroundColor = 'rgb(128, 128, 128)';
+      li.style.backgroundColor = '';
+    }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
+}
+mudaCorDoFundo();
