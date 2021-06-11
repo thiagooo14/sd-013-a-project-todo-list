be58cef9eb4231b392eba5c1e9f01b707d768a9c
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-06-11 01:09:01 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const input = document.getElementById("texto-tarefa");
 const button = document.getElementById("criar-tarefa");
 const lista = document.getElementById("lista-tarefas");
+const item = document.getElementsByClassName('lista');
 
 //Adicionando uma li 
 function criarTarefa() {
   const element = input.value;
   const li = document.createElement('li');
+  li.addEventListener('click', colorBackground);
   li.innerText = element;
   lista.appendChild(li);
   input.value = null;
+  
 };
-
 button.addEventListener("click", criarTarefa);
+
+
+
+
+//Colorir background da li quando clicado
+function colorBackground(event) {
+  for(let i = 0; i < item.length; i += 1){
+    item[i].classList.remove('selected');
+  }
+    event.target.classList.add('selected'); 
+}
