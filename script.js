477ef5e0b17306fc63df9eb9ada90c1b4a1ab512
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-06-11 08:36:05 -0300

    Corrige dos requisitos 1,2,3 e 4

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
-// Requisito 1 - Adicione à sua lista o título "Minha Lista de Tarefas" em uma tag
-function adicionaTitulo() {
-  const titulo = document.getElementsByTagName('body')[0];
-  const header = document.createElement('header');
-  header.className = 'boxTitle';
-  titulo.appendChild(header);
-  const h1 = document.createElement('h1');
-  h1.innerHTML = 'Minha Lista de Tarefas';
-  header.appendChild(h1);
-}
-adicionaTitulo();
+// // Requisito 1 - Adicione à sua lista o título "Minha Lista de Tarefas" em uma tag
+// function adicionaTitulo() {
+//   const titulo = document.getElementsByTagName('body')[0];
+//   const header = document.createElement('header');
+//   header.className = 'boxTitle';
+//   titulo.appendChild(header);
+//   const h1 = document.createElement('h1');
+//   h1.innerHTML = 'Minha Lista de Tarefas';
+//   header.appendChild(h1);
+// }
+// adicionaTitulo();
 
-// Requisito 2 - Adicione abaixo do título um pequeno e discreto parágrafo com id="funcionamento" e com o texto "Clique duas vezes em um item para marcá-lo como completo"
-function addParagraph() {
-  const h1 = document.getElementsByTagName('h1')[0];
-  const paragraph = document.createElement('p');
-  paragraph.id = 'funcionamento';
-  paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
-  h1.appendChild(paragraph);
-}
-addParagraph();
+// // Requisito 2 - Adicione abaixo do título um pequeno e discreto parágrafo com id="funcionamento" e com o texto "Clique duas vezes em um item para marcá-lo como completo"
+// function addParagraph() {
+//   const h1 = document.getElementsByTagName('h1')[0];
+//   const paragraph = document.createElement('p');
+//   paragraph.id = 'funcionamento';
+//   paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+//   h1.appendChild(paragraph);
+// }
+// addParagraph();
 
-// Requisito 3 - Adicione um input com o id="texto-tarefa" onde a pessoa usuária poderá digitar o nome do item que deseja adicionar à lista
-function addInput() {
-  const header = document.getElementsByTagName('header')[0];
-  const saida = document.createElement('input');
-  saida.id = 'texto-tarefa';
-  header.appendChild(saida);
-}
-addInput();
+// // Requisito 3 - Adicione um input com o id="texto-tarefa" onde a pessoa usuária poderá digitar o nome do item que deseja adicionar à lista
+// function addInput() {
+//   const header = document.getElementsByTagName('header')[0];
+//   const saida = document.createElement('input');
+//   saida.id = 'texto-tarefa';
+//   header.appendChild(saida);
+// }
+// addInput();
 
-// Requisito 4 - Adicione uma lista ordenada de tarefas com o id="lista-tarefas"
-function addListOrder() {
-  const getSaida = document.getElementsByTagName('input')[0];
-  const listOrder = document.createElement('ol');
-  listOrder.id = 'lista-tarefas';
-  getSaida.appendChild(listOrder);
-}
-addListOrder();
+// // Requisito 4 - Adicione uma lista ordenada de tarefas com o id="lista-tarefas"
+// function addListOrder() {
+//   const getSaida = document.getElementsByTagName('input')[0];
+//   const listOrder = document.createElement('ol');
+//   listOrder.id = 'lista-tarefas';
+//   getSaida.appendChild(listOrder);
+// }
+// addListOrder();
+
+// // Requsito 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
+// function addButton() {
+//   // const onlyInput = document.getElementsByTagName('input')[0];
+//   // const newDiv = document.createElement('div');
+//   // onlyInput.appendChild(newDiv);
+//   // const div = document.getElementsByTagName('div')[0];
+//   const getOl = document.querySelector('#lista-tarefas');
+//   const div1 = document.createElement('div');
+//   getOl.appendChild(div1);
+//   const adicionaBotao = document.createElement('button');
+//   adicionaBotao.id = 'criar-tarefa';
+//   adicionaBotao.type = 'Submit';
+//   adicionaBotao.innerHTML = 'Adicionar';
+//   div1.appendChild(adicionaBotao);
+// }
+// addButton();
+
+
