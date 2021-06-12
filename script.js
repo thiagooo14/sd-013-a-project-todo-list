2f53324d88a940268d34bce398a2107147d1ee75
Fabíola Rodrigues Moutinho <fabiolamoutinho@hotmail.com>
2021-06-12 12:32:30 -0400

    Requisito 8

--- script.js
+++ script.js
@@ -15,14 +15,35 @@ function createList() {
     listItem.appendChild(task); // colocando as tarefas digitadas como filhas de li
     list.appendChild(listItem); // colocando as li's como filhas do elemento ol
     enter.value = ''; //para o campo de input ficar em branco após a tarefa ser registrada
+
+
     
-    //função para colocar o fundo cinza
-    function cor(){
-        listItem.style.backgroundColor ="rgb(128, 128, 128)";
-    }
-    listItem.addEventListener('click', cor);
+        function mudaCor(novaCor) {
+            const tasks = document.getElementsByTagName('li');
+            for(let index = 0; index < tasks.length; index += 1){
+                let listItem = tasks[index];
+                listItem.style.backgroundColor = null;
+            }
+      
+            novaCor.target.style.backgroundColor='rgb(128, 128, 128)';
+        }
+        listItem.addEventListener('click', mudaCor)
+
+
+
+
+       
+    
+
 }
 button.addEventListener('click', createList);
+
+
+
+
+
+
+
 
 
 
