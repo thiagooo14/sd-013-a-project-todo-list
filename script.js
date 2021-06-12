5a26545071b7abc0fe25374308028839a08213c8
Fabíola Rodrigues Moutinho <fabiolamoutinho@hotmail.com>
2021-06-11 21:26:15 -0400

    Requisito 7

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let button = document.getElementById('criar-tarefa'); // recuperando o elemento 
 let list = document.getElementById('lista-tarefas'); // recuperando e elemento lista
 
 
+
+
 //função para criar a lista de tarefas
 
 function createList() {
@@ -13,8 +15,15 @@ function createList() {
     listItem.appendChild(task); // colocando as tarefas digitadas como filhas de li
     list.appendChild(listItem); // colocando as li's como filhas do elemento ol
     enter.value = ''; //para o campo de input ficar em branco após a tarefa ser registrada
+    
+    //função para colocar o fundo cinza
+    function cor(){
+        listItem.style.backgroundColor ="rgb(128, 128, 128)";
+    }
+    listItem.addEventListener('click', cor);
 }
 button.addEventListener('click', createList);
+
 
 
 
