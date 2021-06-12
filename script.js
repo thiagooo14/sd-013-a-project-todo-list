c34e13b54dee9b9b64c584d2bb0d51b17b72cb72
Emanoel Mattos <emanoelmattos13@gmail.com>
2021-06-12 13:16:37 -0300

    adicionado listas

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 let buttonList = document.getElementById('criar-tarefa');
 let inputList = document.getElementById('texto-tarefa');
 let paiLista = document.getElementById('lista-tarefas');
+let paintingList = document.getElementsByClassName('liCriada');
 
 function createList() {
+  if (inputList.value === "") {
+    return null
+  }
   let listaSon = document.createElement('li');
   listaSon.innerHTML = inputList.value;
-  listaSon.classList = 'ilCriada'
-  paiLista.appendChild(listaSon)
+  listaSon.className = 'liCriada';
+  paiLista.appendChild(listaSon);
   inputList.value = '';
 } 
   buttonList.addEventListener('click', createList);
+
+const changeListColor = () => {
+  let paintedList = document.getElementsByClassName('painted')
+  for (let i = 0; i < paintingList.length; i += 1) {
+    paintingList.classList.remove('painted');
+    paintingList.target.className ='painted'
+    paintedList[i].style.backgroundColor = rgb(128, 128, 128);
+  }
+}
+
+paintingList.addEventListener('click', changeListColor)
