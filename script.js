17001c860d18ef26a1e1b3d83fe74c10f7193a7d
Bruno <brunoalencarlima@outlook.com.br>
2021-06-11 21:06:23 -0300

    exercicio 7 e 8

--- script.js
+++ script.js
@@ -4,18 +4,40 @@ window.onload = function() {
         const contentTask = document.getElementById('texto-tarefa');
         const contentList = document.getElementById('lista-tarefas');
         const listItem = document.createElement('li');
-        if (contentTask.length === 0) {
+        const liExistentes = document.querySelectorAll('li');
+        listItem.id = liExistentes.length + 1; 
+        if (contentTask.value.indexOf(' ') === 0 || contentTask.value === '') {
            alert('Campo não preenchido');
-           return false;
+           contentTask.focus();
         }else {
           listItem.textContent = contentTask.value;
            contentList.appendChild(listItem);
            contentTask.value = '';
+           contentTask.focus();
         }
      }
-     function clickSelect() {
+    function clickSelect() {
          const clickBoton = document.getElementById('criar-tarefa');
          clickBoton.addEventListener('click', taskList); 
      }
-     clickSelect();
+    clickSelect();
+    function corNoClick() {
+        const clickLista = document.getElementById('lista-tarefas');
+        clickLista.addEventListener('click', function (event) {
+            cliqueLista(event.target.id);              
+        })
+    }
+    corNoClick();
+    function cliqueLista(fundoCinza) {
+        let todasLis = document.querySelectorAll('li');
+            for (let i = 0; i < todasLis.length; i+=1) {   
+                if (todasLis[i].id === fundoCinza){ 
+                    const color = document.getElementById(todasLis[i].id)
+                    color.classList.add('corDaselecao'); 
+                }else{
+                    const color = document.getElementById(todasLis[i].id)
+                    color.classList.remove('corDaselecao');
+                }   
+            }
+    }
 }
