31c2fcbdd458fe729082732a9a36b213b591263a
Emanoel Mattos <emanoelmattos13@gmail.com>
2021-06-12 17:30:47 -0300

    adiciona os exercicios to do list

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let buttonList = document.getElementById('criar-tarefa');
 let inputList = document.getElementById('texto-tarefa');
 let paiLista = document.getElementById('lista-tarefas');
-let paintingList = document.getElementsByClassName('liCriada');
 
 function createList() {
   if (inputList.value === "") {
@@ -9,19 +8,78 @@ function createList() {
   }
   let listaSon = document.createElement('li');
   listaSon.innerHTML = inputList.value;
-  listaSon.className = 'liCriada';
+  listaSon.classList.add('liCriada')
   paiLista.appendChild(listaSon);
   inputList.value = '';
 } 
   buttonList.addEventListener('click', createList);
 
 const changeListColor = () => {
-  let paintedList = document.getElementsByClassName('painted')
-  for (let i = 0; i < paintingList.length; i += 1) {
-    paintingList.classList.remove('painted');
-    paintingList.target.className ='painted'
-    paintedList[i].style.backgroundColor = rgb(128, 128, 128);
-  }
+  const getLi = document.getElementsByTagName('li');
+  const paiLi = document.querySelector('ol');
+  paiLi.addEventListener('click' , (color) => {
+    for (let i = 0; i < getLi.length; i += 1) {
+      const lista = getLi[i];
+      lista.style.backgroundColor = "";
+    }
+    color.target.style.backgroundColor = "rgb(128, 128, 128)"
+  })
 }
+changeListColor();
 
-paintingList.addEventListener('click', changeListColor)
+const riskTask = () => {
+  const getLi = document.getElementsByTagName('li');
+  const paiLi = document.querySelector('ol');
+  paiLi.addEventListener('dblclick', (line) => {
+    if (line.target.classList.contains('completed')) {
+      line.target.classList.remove('completed');
+    } else {
+      line.target.classList.add('completed');
+    }
+  })
+}
+riskTask();
+
+const deleteLis = () => {
+  const apagador = document.getElementById('apaga-tudo');
+  const paiLi = document.querySelector('ol');
+    apagador.addEventListener('click', () => {
+      paiLi.innerHTML = null;
+    })
+}
+deleteLis();
+
+const deleteComp = () => {
+  const apagador = document.getElementById('remover-finalizados');
+  const paiLista = document.querySelector('ol');
+  apagador.addEventListener('click', () => {
+    const lista = document.querySelectorAll('.completed')
+    for (let i = 0; i < lista.length; i += 1) {
+      paiLista.removeChild(lista[i]);
+    }
+    
+  })
+}
+deleteComp();
+
+// const savePage = () => {
+//   const getSaveButton = document.querySelector('salvar-tarefas');
+
+//   getSaveButton.addEventListener('click', () => {
+//     const getList = document.querySelector('#lista-tarefas');
+//     localStorage.setItem('tasks', getList.innerHTML);
+//     })
+// }
+// savePage();
+
+// window.onload = () => {
+//   const getList = document.querySelector('#lista-tarefas');
+
+//   if(localStorage.getItem('tasks') !=   null) {
+//     getList.innerHTML += localStorage.getItem('task')
+//   }
+// }
+
+const upButton = () => {
+  const getUppButton = document.querySelector('mover-cima');
+}
