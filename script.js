c990ccf82417608afe5272575b59990f9769cf58
LuizFJP <luizfjportela@gmail.com>
2021-06-12 14:45:49 -0300

    requirement 07 and 08

--- script.js
+++ script.js
@@ -2,20 +2,32 @@
   let buttonAddTask = document.getElementById("criar-tarefa");
   let inputText = document.getElementById('texto-tarefa');
   let list = document.getElementById('lista-tarefas');
-
-  buttonAddTask.addEventListener('click', addTask);
-
+ 
   function addTask(text){
     text = inputText;
     let li = document.createElement('li');
     li.className = "list";
     li.innerText = inputText.value;
+    li.backgroundColor = 'white';
     list.appendChild(li);
     inputText.value = '';
 }
 
-  let topicList = document.querySelectorAll('.list');
+  buttonAddTask.addEventListener('click', addTask);
 
-  topicList.addEventListener('click', changeTopicColor);
+  //const topicList = document.querySelectorAll('.list');
+
+  function changeTopicColor(e){      
+  const topicList = document.querySelectorAll('.list');
+
+    for(let aa of topicList){        
+      
+     aa.classList.remove('selected');
+      console.log(aa);
+  }   e.target.classList.add('selected');
+  }  
+  
+  list.addEventListener('click',changeTopicColor);
+  
 
   
