969f26ef2a534dfb06129be2545624753efde2a8
Patrik Batista <pcr_batista@homail.com>
2021-06-12 13:16:23 -0300

    finalizando item 6

--- script.js
+++ script.js
@@ -9,6 +9,26 @@ const setInput = () => {
     const taskContent = inputList.value;
     setTask.innerText = taskContent;
     lista.appendChild(setTask);
-  })
-}
+
+    inputList.value = '';
+  });
+};
 setInput();
+
+const setInputEnter = () => {
+  const lista = document.querySelector('#lista-tarefas');
+  const inputList = document.querySelector('#texto-tarefa');
+
+  inputList.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13 && inputList.value.length > 0) {
+      const setTask = document.createElement('li');
+      setTask.classList.add('task-list');
+      const taskContent = inputList.value;
+      setTask.innerText = taskContent;
+      lista.appendChild(setTask);
+
+      inputList.value = '';
+    }
+  });
+};
+setInputEnter();
