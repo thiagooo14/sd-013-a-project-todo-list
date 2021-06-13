d1c5780fc65ac217f5910eabed6ba750914f2ecf
Fernando <nando_filipe@hotmail.com>
2021-06-13 01:49:29 -0300

    desafio 4

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-function criarLista(){
+function criarLista() {
+  //pega o botão para o evento de click
+
   let btnAdd = document.querySelector
-  
-    //pega o botão para o evento de click
-    ('#criar-tarefa');
-    btnAdd.addEventListener('click', function(){
-   
-    //pegar a ol
-    let lista = document.querySelector('#lista-tarefa');
-    
-    //criar a li
-    let li = document.createElement('li');
-    
-    // pegar o input(o valor do que vai ser escrito)
-    let texto = document.querySelector('#texto-tarefa');
-    li.innerHTML = texto.value
-    
-    //criar a lista no HTML
-    lista.appendChild(li);
+  ('#criar-tarefa');
+  btnAdd.addEventListener('click', function() {
+
+  //pegar a ol
+  let lista = document.querySelector('#lista-tarefas');
+
+  //criar a li
+  let li = document.createElement('li');
+
+  // pegar o input(o valor do que vai ser escrito)
+  let texto = document.querySelector('#texto-tarefa');
+  li.innerHTML = texto.value
+
+  //criar a lista no HTML
+  lista.appendChild(li);
+
+  texto.value = '';
   })
 }
+
 criarLista();
