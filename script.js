cc27991de997f13d8dffebeecab38b68337fa811
Leme <andresleme@gmail.com>
2021-06-13 18:23:01 -0300

    Refactored

--- script.js
+++ script.js
@@ -9,11 +9,29 @@ function clearTextoTarefa() {
   document.getElementById('texto-tarefa').value = '';
 }
 
+// Requirement 8
+function clearSelection() {
+  const liArray = lista.childNodes;
+  // Por que não funciona com for in?!?!?!
+  // for (let i in liArray) {
+  //   liArray[i].style.backgroundColor = 'white';
+  // }
+  for (let i = 0; i < liArray.length; i += 1) {
+    liArray[i].style.backgroundColor = 'white';
+  }
+}
+
 function addListener() {
-  li.addEventListener("click", (event) => { 
+  li.addEventListener('click', (event) => {
     clearSelection();
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  })
+  });
+}
+
+// Requirement 9
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
+function doubleClick() {
+  li.addEventListener('dblclick', (event) => event.target.classList.toggle('completed'));
 }
 
 // Requirements 5, 6, 7
@@ -24,22 +42,4 @@ function criarTarefa() {
   clearTextoTarefa();
   doubleClick();
   addListener();
-}
-
-// Requirement 8
-function clearSelection() {
-  let liArray = lista.childNodes;
-  // Por que não funciona com for in?!?!?!
-  // for (let i in liArray) {
-  //   liArray[i].style.backgroundColor = 'white';
-  // }
-  for (let i = 0; i < liArray.length; i+= 1) {
-    liArray[i].style.backgroundColor = 'white';
-  }
-}
-
-// Requirement 9
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
-function doubleClick() {
-  li.addEventListener("dblclick", (event) => event.target.classList.toggle('completed'));
 }
