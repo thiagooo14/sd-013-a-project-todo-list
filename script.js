d53fb0702f16b788ad3da14628eb9c671472dfa4
Filipe <filipecandido123@hotmail.com>
2021-06-14 20:56:54 -0300

    requisitos obrigatorios feitos e alguns bonus

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+//elementos
 const inputi = document.getElementById('texto-tarefa');
-const button = document.getElementById('criar-tarefa');
 const listaDeTarefas = document.getElementById('lista-tarefas');
+
+//buttons
+const button = document.getElementById('criar-tarefa');
+const buttonDelet = document.getElementById('apaga-tudo');
+const buttonRemoved = document.getElementById('remover-finalizados');
+const buttonSave = document.getElementById('salvar-tarefas');
+
+//Função que adiciona tarefas
+function adicionaTarefa() {
+  const CreateText = document.createElement('li');
+  listaDeTarefas.appendChild(CreateText);
+  CreateText.innerText = inputi.value;
+  inputi.value = '';
+  CreateText.className = 'item-list';
+}
+button.addEventListener('click', adicionaTarefa);
+inputi.addEventListener('keypress', (g) => {
+  if (g.key === 'Enter') {
+    adicionaTarefa();
+  }
+});
 
 // Referência Josué Lobo//
 listaDeTarefas.addEventListener('click', (event) => {
@@ -16,20 +37,7 @@ listaDeTarefas.addEventListener('click', (event) => {
   }
 });
 
-function adicionaTarefa() {
-  const CreateText = document.createElement('li');
-  listaDeTarefas.appendChild(CreateText);
-  CreateText.innerText = inputi.value;
-  inputi.value = '';
-  CreateText.className = 'item-list';
-}
-button.addEventListener('click', adicionaTarefa);
-inputi.addEventListener('keypress', (g) => {
-  if (g.key === 'Enter') {
-    adicionaTarefa();
-  }
-});
-
+//Função que rasura a linha
 function riscaLinha(evento) {
   const alvo = evento.target;
   if (alvo.classList.contains('completed')) {
@@ -39,3 +47,32 @@ function riscaLinha(evento) {
   }
 }
 listaDeTarefas.addEventListener('dblclick', riscaLinha);
+
+//Função que apaga lista
+function clearList() {
+  listaDeTarefas.innerHTML='';
+}
+buttonDelet.addEventListener('click', clearList);
+
+//Função removeConcluídos
+function removeDone() {
+  const concluido = document.querySelectorAll('.completed');
+  for (let i = 0; i < concluido.length; i += 1) {
+    listaDeTarefas.removeChild(concluido[i]);
+  }
+}
+buttonRemoved.addEventListener('click', removeDone)
+
+//Funções salva-tarefa
+function setLocalSave() {
+  const gravarPag = listaDeTarefas.innerHTML;
+  localStorage.setItem('save', gravarPag);
+}
+buttonSave.addEventListener('click', setLocalSave);
+
+function getLocalSave() {
+  const salvaElementos = localStorage.getItem('save');
+  if (salvaElementos !== null);
+  listaDeTarefas.innerHTML= salvaElementos;
+}
+getLocalSave()
