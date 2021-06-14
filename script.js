58deb7e65c684bf56a89e524f9716427ec3358ba
Edgar Melo <edgarmeloam@gmail.com>
2021-06-13 21:10:05 -0300

    adiciona funçao para limpar ultima selecao

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ const btnSaveTasks = document.querySelector('#salvar-tarefas');
 const btnMoveUp = document.querySelector('#mover-cima');
 const btnMoveDown = document.querySelector('#mover-baixo');
 const btnRemoveSelected = document.querySelector('#remover-selecionado');
+
+function clearLastSelected() {
+    for (let index = 0; index < toDoList.childElementCount; index += 1) {
+      toDoList.children[index].classList.remove('selected');
+    }
+  }
+
+  function loadTasks() {
+    let tasks = JSON.parse(localStorage.getItem('taskContents'));
+    let classes = JSON.parse(localStorage.getItem('taskClasses'))
+  
+    for (let index in tasks) {
+      let item = document.createElement('li');
+      let createdItem = toDoList.appendChild(item);
+      createdItem.className = classes[index];
+      createdItem.innerHTML = tasks[index];
+    }
+  }  
+
+  
