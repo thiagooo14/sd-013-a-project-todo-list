0da48990fa8609bca42a7479eab9007ee15300f4
Caê <eduardokae01@hotmail.com>
2021-06-13 21:26:33 -0300

    melhorando codigo

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const button = document.getElementById('criar-tarefa');
 const textAdd = document.getElementById('texto-tarefa');
 const list = document.getElementById('lista-tarefas');
+const botaoReset = document.querySelector('#apaga-tudo');
+const buttonRemoveFinish = document.getElementById('remover-finalizados');
+let classeCompletada = document.getElementsByClassName('completed');
+const botaoRemoveSelected = document.querySelector('#remover-selecionado');
+let classSelected = document.getElementsByClassName('selected');
 
 button.addEventListener('click', increase);
 
@@ -17,42 +22,27 @@ function increase() {
 
       for (let refresh = 0; refresh < itemLista.length; refresh += 1) {
         itemLista[refresh].style.backgroundColor = '#e9d8d8';
+        itemLista[refresh].classList.remove('selected');
       }
       event.target.style.backgroundColor = 'rgb(128,128,128)';
+      event.target.classList.add('selected');
     });
   }
 }
 
-const itemLista = document.getElementById('lista-tarefas');
-
-itemLista.addEventListener('dblclick', function (event) {
+list.addEventListener('dblclick', function (event) {
   event.target.classList.toggle('completed');
 });
-
-const botaoReset = document.querySelector('#apaga-tudo');
 
 botaoReset.addEventListener('click', function () {
   location.reload();
 });
 
-const botaResetFinished = document.getElementById('remover-finalizados');
-const classCompleted = document.getElementsByTagName('li');
-
-botaResetFinished.addEventListener('click', function () {
-  if (itemLista.childNodes.classList === 'completed') {
-    itemLista.removeChild(classCompleted);
-  }
-})
-
-const buttonRemoveFinish = document.getElementById('remover-finalizados');
-let classeCompletada = document.getElementsByClassName('completed');
-
 buttonRemoveFinish.addEventListener('click', function () {
   
   for (let index = 0; index < classeCompletada.length; index += 1) {
-    let pai = document.getElementById('lista-tarefas');
     while (classeCompletada.length > 0) {
-      pai.removeChild(classeCompletada[index]);
+      list.removeChild(classeCompletada[index]);
     }
   }
 })
