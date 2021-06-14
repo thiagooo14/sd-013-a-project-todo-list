f5aa981c5ea1c39f8e5977f9370bf7bc9b22d5d3
Fernando <nando_filipe@hotmail.com>
2021-06-13 23:52:03 -0300

    requisito 10

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 function criarLista() {
-  //pega o botão para o evento de click
-  let btnAdd = document.querySelector
-  ('#criar-tarefa');
+  let btnAdd = document.querySelector ('#criar-tarefa');
   btnAdd.addEventListener('click', function() {
-  //pegar a ol
   let lista = document.querySelector('#lista-tarefas');
-  //criar a li
   let li = document.createElement('li');
-  // pegar o input(o valor do que vai ser escrito)
   let texto = document.querySelector('#texto-tarefa');
-  li.innerHTML = texto.value
+  li.innerHTML = texto.value;
   texto.value = '';
-  //criar a lista no HTML
   lista.appendChild(li);
   });
 }
 
 criarLista();
 
-function backgroundChange(){
+function backgroundChange() {
   let listaOl = document.querySelector('#lista-tarefas');
-  let listaLi = document.getElementsByTagName('li')
+  let listaLi = document.getElementsByTagName('li');
 
-  listaOl.addEventListener('click', (evento) =>{
-    for(let i = 0; i < listaLi.length; i ++){
+  listaOl.addEventListener('click', (evento) => {
+    for (let i = 0; i < listaLi.length; i ++) {
       let li = listaLi[i];
       li.style.bagroundColor = 'rgb(128,128,128)';
       li.style.backgroundColor = '';
     }
     evento.target.style.backgroundColor = 'rgb(128,128,128)';
-
   })
 }
 backgroundChange();
 
+function apagarTudo() {
+  let btnApagar = document.querySelector('#apaga-tudo');
+  btnApagar.addEventListener('click', () => {
+    document.querySelector('ol').innerHTML = '';
+  })
+}
+apagarTudo();
