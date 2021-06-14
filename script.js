df4040e4f9a6994f65b475fa5028670444f05808
cristianovitorino <57003922+cristianovitorino@users.noreply.github.com>
2021-06-14 15:59:42 -0300

    Adiciona e conserta requisitos 6 e 7

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function addTask() {
     document.getElementById('texto-tarefa').value = '';
   }
 }
+//debugger
+function changeItemBG() {
+  let input = document.querySelector('ol').children;
+
+  for (let i = 0; i < input.length; i += 1) {
+    input[i].addEventListener('click', () => {
+      item = input[i];
+      item.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
+}
+
+//changeItemBG();
