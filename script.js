fd1be29c4f1be677b6e9a49b2cd4f795cde54364
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2021-06-14 00:51:36 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
+window.onload
+
 /* Variaveis */
 let inputAdd = document.getElementById("texto-tarefa");
 let btnAdd = document.getElementById("criar-tarefa");
 let listOrd = document.getElementById("lista-tarefas");
-
+let item = document.getElementsByClassName("item");
 /*criando itens apos clicar no botão adicionar*/
 
-btnAdd.addEventListener("click", criarItens)
+
 
 function criarItens () {
-  
+  btnAdd.addEventListener("click", () =>{
   let itens = document.createElement('li');
   listOrd.appendChild(itens);
-  itens.innerText=inputAdd.value
+  itens.classList = "item"
+  itens.innerText = inputAdd.value
   inputAdd.value = ""
+  listenerClick();
+  })
+}
+
+criarItens();
+
+ //Criando background ao clicar nos itens 
+
+function listenerClick () {
+  for (let i = 0 ; i < item.length; i++){
+    item[i].addEventListener("click", () => {
+      adicionaClass()});
+    }
+}
+
+function adicionaClass (){
+for (let i = 0 ; i < item.length; i++){
+  item[i].style.backgroundColor= "rgb(128, 128, 128)"
+  }
 }
