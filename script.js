3e53dfaa13b85a9359a42a17a5a47bd8578df32b
Caio-Borin <caioborin@gmail.com>
2021-06-16 15:33:41 -0300

    Exercicio 7 e 8

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
-const lista = document.querySelector('#lista-tarefas');
-const botaoCriarTarefa = document.querySelector('#criar-tarefa');
-const inputTarefa = document.querySelector('#texto-tarefa');
-
-function adicionaTarefa() {
-  botaoCriarTarefa.addEventListener('click', function () {
-    const item = document.createElement('li');
-    item.className = 'item';
-    item.innerText = inputTarefa.value;
-    lista.appendChild(item)
-    inputTarefa.value = '';
-  });
+function addTask() {
+  const valueInput = document.querySelector('#texto-tarefa');
+  const valueLi = document.createElement('li');
+  valueLi.innerHTML = valueInput.value;
+  valueLi.classList.add('tarefas');
+  document.getElementById('lista-tarefas').appendChild(valueLi);
+  valueInput.value = null;
 }
-adicionaTarefa();
+const tarefa = document.getElementById('criar-tarefa');
+tarefa.addEventListener('click', addTask);
 
-function selecionarTarefa() {
-  const tarefaTodas = document.querySelectorAll('.item');
-  for (let i in tarefaTodas) {
-    tarefaTodas[i].addEventListener('click', function(event) {
-      event.target.style.backgroundColor = 'rgb(155, 163, 111)';
-    })
+const olGrey = document.querySelector('#lista-tarefas');
+const liGrey = document.getElementsByTagName('li');
+olGrey.addEventListener('click', (event) => {
+  if (event.target.classList.contains('tarefas')) {
+    if (event.target.classList.contains('grey')) {
+      event.target.classList.remove('grey');
+    } else {
+      for (let i of liGrey) {
+        i.classList.remove('grey');
+      }
+      event.target.classList.add('grey');
+    }
+  }
+});
+
+olGrey.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('tarefas')) {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
+  }
+});
+
+function removeTask() {
+  const ol = document.querySelector('#lista-tarefas');
+  while (ol.hasChildNodes()) {
+    ol.removeChild(ol.firstChild);
   }
 }
+
+function finishRemove() {
+  const finish = document.querySelector('.completed');
+  const ol = document.querySelector('#lista-tarefas');
+  for (let index = 0; index < finish.length; index += 1) {
+    if (finish[index].classList.contains('completed')) {
+      ol.removeChild(finish[index]);
+    }
+  }
+}
+const buttonFinish = document.getElementById('remover-finalizados');
+buttonFinish.addEventListener('click', finishRemove);
