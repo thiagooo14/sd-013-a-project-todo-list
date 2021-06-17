530d3cfb1b0877866e66021456df597844c4f1f9
joaovitor-B <joao.vitorjv2000@gmail.com>
2021-06-17 10:27:55 -0300

    mais um requisito

---
+++ script.js
@@ -0,0 +1,4 @@
+const listaDTarefas = document.querySelector('#lista-tarefas');
+const textoTarefa = document.querySelector('#texto-tarefa');
+const criarTarefa = document.querySelector('#criar-tarefa');
+
