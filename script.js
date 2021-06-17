e97c218b1f328860624b92766f4de6269dc5abe0
CarlosLima <carlos.augustolimaf@gmail.com>
2021-06-17 04:33:15 -0300

    Adiciona o requisito 10

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const btnCriarTarefa = document.querySelector('#criar-tarefa');
 const inputCriarTarefa = document.querySelector('#texto-tarefa');
 const olTarefa = document.querySelector('#lista-tarefas');
+const btnApagarTudo = document.querySelector('#apaga-tudo');
+const btnRemoverFinalizados = document.querySelector('#remover-finalizados');
 
 btnCriarTarefa.addEventListener('click', function() {
   let liTarefa = document.createElement('li');
   olTarefa.appendChild(liTarefa);
+  liTarefa.classList.add('li-style')
 
   liTarefa.innerHTML = inputCriarTarefa.value;
   inputCriarTarefa.value = '';
@@ -18,4 +21,24 @@ olTarefa.addEventListener('click', function(event) {
   }
 
   event.target.classList.add('select');
+});
+
+olTarefa.addEventListener('dblclick', function(event) {
+    event.target.classList.toggle('completed');
+});
+
+btnApagarTudo.addEventListener('click', function(){
+  olTarefa.innerHTML = '';
+});
+
+btnRemoverFinalizados.addEventListener('click', function(){
+  const liIrmaos = olTarefa.children;
+  console.log(liIrmaos);
+
+  for (const i of liIrmaos) {
+    if (i.classList.contains('completed') === true) {
+      liIrmaos.parentNode.removeChild(i)
+    }
+  }
+  
 });
