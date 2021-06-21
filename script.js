69d825dd282d419d92c95feab09c19e14083b1b1
joaovitor-B <joao.vitorjv2000@gmail.com>
2021-06-21 11:07:45 -0300

    correcoes das id's dos botoes

--- script.js
+++
@@ -1,4 +0,0 @@
-const listaDTarefas = document.querySelector('#lista-tarefas');
-const textoTarefa = document.querySelector('#texto-tarefa');
-const criarTarefa = document.querySelector('#criar-tarefa');
-
