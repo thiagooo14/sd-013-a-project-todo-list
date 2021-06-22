b412dfd9fa96eb6ef4656bc56f343909435fb88c
Gean Gonçalves <gean.a.l.c@gmail.com>
2021-06-22 09:50:39 -0400

    Requisito 10

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const criarTarefaBt = document.getElementById('criar-tarefa');
-
+const deletarTarefasBt = document.getElementById('apaga-tudo');
 function novaTarefa() {
   const input = document.querySelector('input').value;
   const tarefa = document.createElement('li');
   tarefa.innerHTML = input;
+  tarefa.classList.add('tarefa');
   document.querySelector('ol').appendChild(tarefa);
   tarefa.addEventListener('click', selectedItem);
   tarefa.addEventListener('dblclick', markItem);
@@ -21,4 +22,12 @@ function markItem(item) {
   item.target.classList.add('completed');
 }
 
+function deleteAll() {
+  document.querySelector('ol').remove();
+  const lista = document.createElement('ol');
+  lista.id = 'lista-tarefas';
+  document.querySelector('.lista-tarefas').appendChild(lista);
+}
+
 criarTarefaBt.addEventListener('click', novaTarefa);
+deletarTarefasBt.addEventListener('click', deleteAll);
