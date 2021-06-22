c3e4b6fa09b367aa0c43f2944f0bc8f0d52fa387
Gean Gonçalves <gean.a.l.c@gmail.com>
2021-06-22 09:36:34 -0400

    Requisito 6 e 7

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ function novaTarefa() {
   const tarefa = document.createElement('li');
   tarefa.innerHTML = input;
   document.querySelector('ol').appendChild(tarefa);
+  tarefa.addEventListener('click', corSelecao);
+  document.querySelector('input').value = "";
+}
+
+function corSelecao(item) {
+  const selectedAtual = document.querySelector('.selected');
+  const selected = item.target;
+  selected.classList.add('selected');
+  selectedAtual.classList.remove('selected');
 }
 
 criarTarefaBt.addEventListener('click', novaTarefa);
