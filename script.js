24482aaa8388fefd7c3ce651e69b89f806c646e7
Gean Gonçalves <gean.a.l.c@gmail.com>
2021-06-22 09:42:06 -0400

    Requisito 8 e 9

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ function novaTarefa() {
   const tarefa = document.createElement('li');
   tarefa.innerHTML = input;
   document.querySelector('ol').appendChild(tarefa);
-  tarefa.addEventListener('click', corSelecao);
+  tarefa.addEventListener('click', selectedItem);
+  tarefa.addEventListener('dblclick', markItem);
   document.querySelector('input').value = "";
 }
 
-function corSelecao(item) {
+function selectedItem(item) {
   const selectedAtual = document.querySelector('.selected');
   const selected = item.target;
   selected.classList.add('selected');
   selectedAtual.classList.remove('selected');
+}
+
+function markItem(item) {
+  item.target.classList.add('completed');
 }
 
 criarTarefaBt.addEventListener('click', novaTarefa);
