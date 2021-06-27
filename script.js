67f87aa3526bdc424b1f3aa88693bd0418467979
Eduardomak <eduardomak92@gmail.com>
2021-06-27 02:12:25 -0300

    alguns requisitos, bug no código no requisito 8, parou de funcionar(?)

---
+++ script.js
@@ -0,0 +1,64 @@
+const captureOl = document.getElementById('lista-tarefas');
+
+function addNewItem () {
+  const btn = document.getElementById('criar-tarefa');
+  const list = document.getElementById('lista-tarefas')
+  btn.addEventListener('click', () => {
+    const newLi = document.createElement('li')
+    const newText = list.appendChild(newLi);
+    newText.className = 'Item'
+    const inputText = document.getElementById('texto-tarefa');
+    newText.innerHTML = `${inputText.value}`;
+    inputText.value = '';
+  });
+}
+addNewItem();
+
+function markItem () {
+  captureOl.addEventListener('click', (event) => {
+    const captureLi = event.target;
+    console.log(captureLi)
+    if (captureLi.classList.contains('Selected')) {
+      captureLi.classList.remove('Selected')
+    }
+    captureLi.classList.add('Selected');
+  });
+}
+markItem();
+
+function riskItem () {
+  captureOl.addEventListener('dblclick', (event) => {
+    const captureLi = event.target;
+    if(captureLi.classList.contains('completed') ) {
+      captureLi.classList.remove('completed');
+    } else
+    captureLi.classList.add('completed');
+  });
+}
+riskItem();
+
+function clearAll () {
+  const btnclear = document.getElementById('apaga-tudo');
+  btnclear.addEventListener('click', () => {
+    captureOl.innerHTML = '';
+  });
+}
+clearAll();
+
+function clearCompleted() {
+  const btnclearSome = document.getElementById('remover-finalizados');
+  btnclearSome.addEventListener('click', () => {
+    const completedTasks = document.querySelectorAll('.completed');
+    for (let index = 0; index < completedTasks.length; index += 1) {
+      completedTasks[index].remove();
+    }
+  })
+}
+clearCompleted();
+
+function moveUp() {
+  const btnUp = document.getElementById('mover-cima');
+  btnUp.addEventListener('click', () => {
+  });
+}
+moveUp();
