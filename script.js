029e9414d7eba640090e5c16d7a50b9d7bc77eb9
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2021-06-27 22:40:19 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -22,12 +22,23 @@ function createNewTask() {
     inputField.value = null;
   })
 }
+// Executa a função.
 createNewTask();
 
-// Ao clicar em um item da lista deve alterar a cor de fundo do item.
+// Ao clicar em um item da lista, altera a cor de fundo do item.
 function selectedTask() {
+  // Adiciona um EventListener a constante taskList (ol) que, ao clicar,
+  // verificar a existência de li com classe "selected" e, se for o caso, remove
+  // então, adiciona uma classe (selected) à li clicada.
   taskList.addEventListener('click', (event) => {
+    const allTask = document.querySelectorAll('li');
+    for (let index = 0; index < allTask.length; index +=1) {
+      if (allTask[index].classList.contains('selected')) {
+        allTask[index].classList.remove('selected');
+      }
+    }
     event.target.classList.add('selected');
   })
 }
+//Executa a função.
 selectedTask();
