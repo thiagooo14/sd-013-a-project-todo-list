698ca4b1006c4c5098cfc23f20073e6a8494e159
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2021-06-27 22:05:36 -0300

    Requisitos 5 e 6

---
+++ script.js
@@ -0,0 +1,25 @@
+// Pega o campo input.
+const inputField = document.getElementById('texto-tarefa');
+
+// Pega o botão criar tarefa.
+const buttonCreateTask = document.getElementById('criar-tarefa');
+
+// Pega a lista de tarefas.
+const taskList = document.getElementById('lista-tarefas');
+
+// Cria uma nova tarefa.
+function createNewTask() {
+  buttonCreateTask.addEventListener('click', () => {
+    // Cria uma nova li.
+    let newTask = document.createElement('li');
+    // Pega o texto do campo input.
+    const textInput = inputField.value;
+    // Coloca o texto do input dentro da li
+    newTask.innerHTML = textInput;
+    // Adiciona a nova li como filha da ol
+    taskList.appendChild(newTask);
+    // Apaga o contúdo do campo input.
+    inputField.value = null;
+  })
+}
+createNewTask();
