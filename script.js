92fe2537aacd725cf7dd92063abe028545e041be
CarlosLima <carlos.augustolimaf@gmail.com>
2021-06-29 11:17:33 -0300

    Adiciona todos os requisitos e o lint ajustado

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const ol = document.querySelector('#lista-tarefas');
+
 const requisito4 = () => {
   const input = document.getElementById('texto-tarefa');
   const button = document.getElementById('criar-tarefa');
-  const ol = document.getElementById('lista-tarefas');
+
   button.addEventListener('click', () => {
     const li = document.createElement('li');
     li.setAttribute('class', 'tarefa');
@@ -12,15 +14,16 @@ const requisito4 = () => {
 };
 
 const requisito7 = () => {
-  const liS = document.getElementsByClassName('tarefa');
-  const ol = document.getElementById('lista-tarefas');
+  const liIrmaos = document.getElementsByTagName('li');
+
   ol.addEventListener('click', (event) => {
-    for (let i = 0; i < liS.length; i += 1) {
-      const li = liS[i];
+    for (let i = 0; i < liIrmaos.length; i += 1) {
+      const li = liIrmaos[i];
       li.classList.remove('selected');
     }
     event.target.classList.add('selected');
   });
+
   ol.addEventListener('dblclick', (event) => {
     event.target.classList.toggle('completed');
   });
@@ -28,7 +31,7 @@ const requisito7 = () => {
 
 const requisito10 = () => {
   const buttonErase = document.getElementById('apaga-tudo');
-  const ol = document.getElementById('lista-tarefas');
+
   buttonErase.addEventListener('click', () => {
     ol.innerHTML = '';
   });
@@ -36,6 +39,7 @@ const requisito10 = () => {
 
 const requisito11 = () => {
   const buttonErase = document.getElementById('remover-finalizados');
+
   buttonErase.addEventListener('click', () => {
     const completeds = document.querySelectorAll('.completed');
     for (let i = 0; i < completeds.length; i += 1) {
@@ -47,6 +51,7 @@ const requisito11 = () => {
 
 const requisito14 = () => {
   const buttonErase = document.getElementById('remover-selecionado');
+
   buttonErase.addEventListener('click', () => {
     const selecteds = document.querySelectorAll('.selected');
     for (let i = 0; i < selecteds.length; i += 1) {
@@ -57,14 +62,30 @@ const requisito14 = () => {
 };
 
 const requisito12 = () => {
-  const ol = document.getElementById('lista-tarefas');
   const saveButton = document.getElementById('salvar-tarefas');
+
   saveButton.addEventListener('click', () => {
     localStorage.setItem('lista', ol.innerHTML);
   });
-  if (localStorage.getItem('lista') !== null) {
-    ol.innerHTML = localStorage.getItem('lista');
-  }
+
+  ol.innerHTML = localStorage.getItem('lista');
+};
+
+const requisito13 = () => {
+  const cima = document.querySelector('#mover-cima');
+  const baixo = document.querySelector('#mover-baixo');
+  cima.addEventListener('click', () => {
+    const selected = document.querySelector('.selected');
+    if (selected !== null && selected.previousElementSibling !== null) {
+      ol.insertBefore(selected, selected.previousSibling);
+    }
+  });
+  baixo.addEventListener('click', () => {
+    const selected = document.querySelector('.selected');
+    if (selected !== null && selected.nextElementSibling !== null) {
+      ol.insertBefore(selected.nextSibling, selected);
+    }
+  });
 };
 
 window.onload = () => {
@@ -73,48 +94,6 @@ window.onload = () => {
   requisito10();
   requisito11();
   requisito14();
+  requisito13();
   requisito12();
 };
-
-// const btnCriarTarefa = document.querySelector('#criar-tarefa');
-// const inputCriarTarefa = document.querySelector('#texto-tarefa');
-// const olTarefa = document.querySelector('#lista-tarefas');
-// const btnApagarTudo = document.querySelector('#apaga-tudo');
-// const btnRemoverFinalizados = document.querySelector('#remover-finalizados');
-
-// btnCriarTarefa.addEventListener('click', function() {
-//   let liTarefa = document.createElement('li');
-//   olTarefa.appendChild(liTarefa);
-//   liTarefa.classList.add('li-style')
-
-//   liTarefa.innerHTML = inputCriarTarefa.value;
-//   inputCriarTarefa.value = '';
-// });
-
-// olTarefa.addEventListener('click', function(event) {
-//   const liIrmaos = olTarefa.childNodes;
-
-//   for (const i of liIrmaos) {
-//     i.classList.remove('select');
-//   }
-
-//   event.target.classList.add('select');
-// });
-
-// olTarefa.addEventListener('dblclick', function(event) {
-//     event.target.classList.toggle('completed');
-// });
-
-// btnApagarTudo.addEventListener('click', function(){
-//   olTarefa.innerHTML = '';
-// });
-
-// btnRemoverFinalizados.addEventListener('click', function(){
-//   const liIrmaos = document.querySelectorAll('.li-style');
-
-//   for (const i of liIrmaos) {
-//     if (i.classList.contains('completed') === true) {
-//         i.remove()
-//     }
-//   }
-// });
